82fbda9a3f057901ddc4d0ea655014f6f3f9b76c
Chad Nedzlek <chadnedz@microsoft.com>
2018-12-06 16:03:11 -0800

    Join-Path doesn't work with multiple arguments

--- src/Maestro/update-database.ps1
+++ src/Maestro/update-database.ps1
@@ -19,11 +19,11 @@ function removeDllExtension {
 function Get-EfDllPath {
   $dotnetLocation = Split-Path -Parent (Get-Command dotnet).Path
   $sdkVersion = dotnet --version
-  $sdkToolsPath = Join-Path $dotnetLocation sdk $sdkVersion DotnetTools
+  $sdkToolsPath = "$dotnetLocation\sdk\$sdkVersion\DotnetTools"
   $dotnetEfToolPath = Join-Path $sdkToolsPath dotnet-ef
   $dotnetEfVersion = ls $dotnetEfToolPath | select -ExpandProperty Name | sort -Descending | select -First 1
 
-  return (Join-Path $dotnetEfToolPath $dotnetEfVersion tools netcoreapp2.1 any tools netcoreapp2.0 any ef.dll)
+  return "$dotnetEfToolPath\$dotnetEfVersion\tools\netcoreapp2.1\any\tools\netcoreapp2.0\any\ef.dll"
 }
 
 $migrationsNamespace = removeDllExtension $migrationsDll
