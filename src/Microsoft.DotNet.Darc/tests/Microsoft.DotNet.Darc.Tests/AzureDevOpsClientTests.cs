33142e81b3450cf6caea6dbe862c8eba48469077
Matt Mitchell <mmitche@microsoft.com>
2018-12-19 09:59:50 -0800

    Switch to theories

--- src/Microsoft.DotNet.Darc/tests/Microsoft.DotNet.Darc.Tests/AzureDevOpsClientTests.cs
+++ src/Microsoft.DotNet.Darc/tests/Microsoft.DotNet.Darc.Tests/AzureDevOpsClientTests.cs
@@ -11,76 +11,46 @@ namespace Microsoft.DotNet.Darc.Tests
 {
     public class AzureDevOpsClientTests
     {
-        [Fact]
-        private void ParseValidRepoUriTests()
+        [Theory]
+        [InlineData("https://dev.azure.com/dnceng/public/_git/foo", "dnceng", "public", "foo")]
+        [InlineData("https://dev.azure.com/dcn2eng/public-s/_git/foo-23bar", "dcn2eng", "public-s", "foo-23bar")]
+        [InlineData("https://dev.azure.com/foo/bar/_git/baz-bop/pullrequest/11?_a=overview", "foo", "bar", "baz-bop")]
+        private void ParseValidRepoUriTests(string inputUri, string expectedAccount, string expectedProject, string expectedRepo)
         {
-            Dictionary<string, (string account, string project, string repo)> validRepoUris =
-                new Dictionary<string, (string account, string project, string repo)>()
-            {
-                {  "https://dev.azure.com/dnceng/public/_git/foo", ("dnceng", "public", "foo") },
-                {  "https://dev.azure.com/dcn2eng/public-s/_git/foo-23bar", ("dcn2eng", "public-s", "foo-23bar") },
-                {  "https://dev.azure.com/foo/bar/_git/baz-bop/pullrequest/11?_a=overview", ("foo", "bar", "baz-bop") },
-            };
-
-            foreach (var validUri in validRepoUris)
-            {
-                (string account, string project, string repo) = AzureDevOpsClient.ParseRepoUri(validUri.Key);
-                Xunit.Assert.Equal(validUri.Value.account, account);
-                Xunit.Assert.Equal(validUri.Value.project, project);
-                Xunit.Assert.Equal(validUri.Value.repo, repo);
-            }
+            (string account, string project, string repo) = AzureDevOpsClient.ParseRepoUri(inputUri);
+            Xunit.Assert.Equal(expectedAccount, account);
+            Xunit.Assert.Equal(expectedProject, project);
+            Xunit.Assert.Equal(expectedRepo, repo);
         }
 
-        [Fact]
-        private void ParseInvalidRepoUriTests()
+        [Theory]
+        [InlineData("https://dnceng.visualstudio.com/public/_git/foo")]
+        [InlineData("https://dev.azure.com/dcn-eng/public-s/_git/foo-23bar")]
+        [InlineData("https://dnceng@dev.azure.com/dnceng/public/_git/foo-core")]
+        private void ParseInvalidRepoUriTests(string inputUri)
         {
-            List<string> invalidRepoUris = new List<string>()
-            {
-                "https://dnceng.visualstudio.com/public/_git/foo",
-                "https://dev.azure.com/dcn-eng/public-s/_git/foo-23bar",
-                "https://dnceng@dev.azure.com/dnceng/public/_git/foo-core"
-            };
-
-            foreach (var invalidUri in invalidRepoUris)
-            {
-                Xunit.Assert.Throws<ArgumentException>(() => AzureDevOpsClient.ParseRepoUri(invalidUri));
-            }
+            Xunit.Assert.Throws<ArgumentException>(() => AzureDevOpsClient.ParseRepoUri(inputUri));
         }
 
-        [Fact]
-        private void ParseValidPullRequestUriTests()
+        [Theory]
+        [InlineData("https://dev.azure.com/foo/bar/_apis/git/repositories/baz98-bop/pullRequests/112", "foo", "bar", "baz98-bop", 112)]
+        [InlineData("https://dev.azure.com/foo/bar/_apis/git/repositories/kidz-bop/pullRequests/1133?_a=files", "foo", "bar", "kidz-bop", 1133)]
+        [InlineData("https://dev.azure.com/foo/bar/_apis/git/repositories/baz-bop/pullRequests/141?_a=files&path=%2F.build%2Frestore.yaml", "foo", "bar", "baz-bop", 141)]
+        private void ParseValidPullRequestUriTests(string inputUri, string expectedAccount,
+            string expectedProject, string expectedRepo, int expectedId)
         {
-            Dictionary<string, (string account, string project, string repo, int id)> validRepoUris =
-                new Dictionary<string, (string account, string project, string repo, int id)>()
-            {
-                { "https://dev.azure.com/foo/bar/_apis/git/repositories/baz98-bop/pullRequests/112", ("foo", "bar", "baz98-bop", 112) },
-                { "https://dev.azure.com/foo/bar/_apis/git/repositories/kidz-bop/pullRequests/1133?_a=files", ("foo", "bar", "kidz-bop", 1133) },
-                { "https://dev.azure.com/foo/bar/_apis/git/repositories/baz-bop/pullRequests/141?_a=files&path=%2F.build%2Frestore.yaml", ("foo", "bar", "baz-bop", 141) },
-            };
-
-            foreach (var validUri in validRepoUris)
-            {
-                (string account, string project, string repo, int id) = AzureDevOpsClient.ParsePullRequestUri(validUri.Key);
-                Xunit.Assert.Equal(validUri.Value.account, account);
-                Xunit.Assert.Equal(validUri.Value.project, project);
-                Xunit.Assert.Equal(validUri.Value.repo, repo);
-                Xunit.Assert.Equal(validUri.Value.id, id);
-            }
+            (string account, string project, string repo, int id) = AzureDevOpsClient.ParsePullRequestUri(inputUri);
+            Xunit.Assert.Equal(expectedAccount, account);
+            Xunit.Assert.Equal(expectedProject, project);
+            Xunit.Assert.Equal(expectedRepo, repo);
+            Xunit.Assert.Equal(expectedId, id);
         }
 
-        [Fact]
-        private void ParseInvalidPullRequestUriTests()
+        [Theory]
+        [InlineData("https://dev.azure.com/foo/bar/_git/baz98-bop/pullRequests/112")]
+        private void ParseInvalidPullRequestUriTests(string inputUri)
         {
-            List<string> invalidPrUrls = new List<string>()
-            {
-                // Not expecting the non-api form of PR url
-                "https://dev.azure.com/foo/bar/_git/baz98-bop/pullRequests/112"
-            };
-
-            foreach (var invalidUri in invalidPrUrls)
-            {
-                Xunit.Assert.Throws<ArgumentException>(() => AzureDevOpsClient.ParsePullRequestUri(invalidUri));
-            }
+            Xunit.Assert.Throws<ArgumentException>(() => AzureDevOpsClient.ParsePullRequestUri(inputUri));
         }
     }
 }
