26342a530201e66d71db08f974c5aba495d5a775
Cesar Soares Lucas <disoares@microsoft.com>
2019-01-09 16:11:03 -0800

    Add concept of ReleasePipeline to BAR

--- src/Maestro/Maestro.Data/Models/ReleasePipeline.cs
+++ src/Maestro/Maestro.Data/Models/ReleasePipeline.cs
@@ -2,26 +2,19 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Maestro.Data.Models
 {
-    public class Channel
+    public class ReleasePipeline
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        [Required]
-        public string Name { get; set; }
+        public int PipelineIdentifier { get; set; }
 
-        [Required]
-        public string Classification { get; set; }
-
-        public List<BuildChannel> BuildChannels { get; set; }
-        public List<DefaultChannel> DefaultChannels { get; set; }
-        public List<ReleasePipeline> ReleasePipelines { get; set; }
+        public int ChannelId { get; set; }
     }
 }
