b5fba4bd83151aef2623c3f192df733898b3be1a
Alex Perovich <alperovi@microsoft.com>
2019-01-10 13:32:52 -0800

    Handle BOMs in file contents

--- src/Microsoft.DotNet.Darc/src/DarcLib/IGitRepoExtension.cs
+++ src/Microsoft.DotNet.Darc/src/DarcLib/IGitRepoExtension.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.IO;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,11 @@ namespace Microsoft.DotNet.DarcLib
             try
             {
                 byte[] content = Convert.FromBase64String(encodedContent);
-                return Encoding.UTF8.GetString(content);
+                using (var str = new MemoryStream(content, false))
+                using (var reader = new StreamReader(str))
+                {
+                    return reader.ReadToEnd();
+                }
             }
             catch (FormatException)
             {
