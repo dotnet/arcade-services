37ac6c8e8027a1bcaaf6be250e5bdb12b8fdd842
Matt Mitchell <mmitche@microsoft.com>
2019-01-30 11:13:15 -0800

    New dependency graph tests

--- src/Microsoft.DotNet.Darc/tests/Microsoft.DotNet.Darc.Tests/DependencyGraphTests.cs
+++ src/Microsoft.DotNet.Darc/tests/Microsoft.DotNet.Darc.Tests/DependencyGraphTests.cs
@@ -13,171 +13,20 @@ namespace Microsoft.DotNet.Darc.Tests
 {
     public class DependencyGraphTests
     {
-        private const string DefaultOutputFileName = "output.xml";
-
-        [Fact]
-        public void ValidateFullGraph()
+        [Theory]
+        [InlineData("core-sdk", "c57bde45936fc120c0124bf964f570c45bfd8f8b", true)]
+        [InlineData("core-sdk", "c57bde45936fc120c0124bf964f570c45bfd8f8b", false)]
+        [InlineData("core-sdk", "36e48aa03cb568c128f387a090ad5ece86933c1c", true)]
+        [InlineData("core-sdk", "36e48aa03cb568c128f387a090ad5ece86933c1c", false)]
+        [InlineData("corefx", "bc0cfc49d1a8c1681fb09603f3389ef65342d542", true)]
+        [InlineData("corefx", "bc0cfc49d1a8c1681fb09603f3389ef65342d542", false)]
+        public void ValidateGraph(string rootRepo, string rootCommit, bool includeToolset)
         {
-            DependencyDetail dependencyDetail = new DependencyDetail
-            {
-                Name = "Base.Asset",
-                RepoUri = "RepoA",
-                Commit = "shaA",
-                Version = "1.0.0",
-                Type = DependencyType.Product
-            };
-
             DependencyTestDriver.GetGraphAndCompare("DependencyGraph", async driver =>
             {
-                return await driver.GetDependencyGraph(dependencyDetail, true);
+                return await driver.GetDependencyGraph(rootRepo, rootCommit, includeToolset);
             },
-            GetExpectedDependencyGraphAsync,
-            dependencyDetail,
-            DefaultOutputFileName,
-            true);
-        }
-
-        [Fact]
-        public void ValidateIncompleteFullGraph()
-        {
-            DependencyDetail dependencyDetail = new DependencyDetail
-            {
-                Name = "Base.Asset",
-                RepoUri = "RepoA",
-                Commit = "shaA",
-                Version = "1.0.0",
-                Type = DependencyType.Product
-            };
-
-            DependencyTestDriver.GetGraphAndCompare("DependencyGraph", async driver =>
-            {
-                return await driver.GetDependencyGraph(dependencyDetail, true);
-            },
-            GetExpectedDependencyGraphAsync,
-            dependencyDetail,
-            "output1.xml",
-            false);
-        }
-
-        [Fact]
-        public void ValidateGraphFromChildNode()
-        {
-            DependencyDetail dependencyDetail = new DependencyDetail
-            {
-                Name = "Base.Asset",
-                RepoUri = "RepoB",
-                Commit = "shaB",
-                Version = "1.0.0",
-                Type = DependencyType.Product
-            };
-
-            DependencyTestDriver.GetGraphAndCompare("DependencyGraph", async driver =>
-            {
-                return await driver.GetDependencyGraph(dependencyDetail, true);
-            },
-            GetExpectedDependencyGraphAsync,
-            dependencyDetail,
-            DefaultOutputFileName,
-            true);
-        }
-
-        [Fact]
-        public void ValidateGraphWithNoChildNodes()
-        {
-            DependencyDetail dependencyDetail = new DependencyDetail
-            {
-                Name = "Base.Asset",
-                RepoUri = "RepoX",
-                Commit = "shaX",
-                Version = "1.0.0",
-                Type = DependencyType.Product
-            };
-
-            DependencyTestDriver.GetGraphAndCompare("DependencyGraph", async driver =>
-            {
-                return await driver.GetDependencyGraph(dependencyDetail, true);
-            },
-            GetExpectedDependencyGraphAsync,
-            dependencyDetail,
-            DefaultOutputFileName,
-            true);
-        }
-
-        [Fact]
-        public void ValidateLeafNode()
-        {
-            DependencyDetail dependencyDetail = new DependencyDetail
-            {
-                Name = "Base.Asset",
-                RepoUri = "RepoE",
-                Commit = "shaE1",
-                Version = "1.0.0",
-                Type = DependencyType.Product
-            };
-
-            DependencyTestDriver.GetGraphAndCompare("DependencyGraph", async driver =>
-            {
-                return await driver.GetDependencyGraph(dependencyDetail, true);
-            },
-            GetExpectedDependencyGraphAsync,
-            dependencyDetail,
-            DefaultOutputFileName,
-            true);
-        }
-
-        private async Task<DependencyGraph> GetExpectedDependencyGraphAsync(DependencyDetail rootDependency, string temporaryRepositoryPath, string outputFileName)
-        {
-            HashSet<DependencyDetail> flatGraph = new HashSet<DependencyDetail>(new DependencyDetailComparer()) { rootDependency };
-            DependencyGraphNode rootGraphNode = new DependencyGraphNode(rootDependency.RepoUri,
-                                                                    rootDependency.Commit,
-                                                                    new List<DependencyDetail>() { rootDependency });
-            Stack<DependencyGraphNode> nodesToVisit = new Stack<DependencyGraphNode>();
-
-            string outputFilePath = Path.Combine(
-                    temporaryRepositoryPath,
-                    rootDependency.RepoUri,
-                    rootDependency.Commit,
-                    outputFileName);
-
-            if (!File.Exists(outputFilePath))
-            {
-                return new DependencyGraph(rootGraphNode, new List<DependencyDetail>(), new List<DependencyGraphNode>(), new List<DependencyGraphNode>());
-            }
-
-            string output = await File.ReadAllTextAsync(outputFilePath);
-            XmlDocument document = new XmlDocument();
-            document.LoadXml(output);
-
-            XmlNode baseNode = document.SelectSingleNode("Dependencies");
-            Stack<XmlNode> xmlNodes = new Stack<XmlNode>();
-            xmlNodes.Push(baseNode);
-
-            nodesToVisit.Push(rootGraphNode);
-
-            while (nodesToVisit.Count > 0)
-            {
-                DependencyGraphNode node = nodesToVisit.Pop();
-                baseNode = xmlNodes.Pop();
-
-                foreach (XmlNode xmlNode in baseNode.ChildNodes)
-                {
-                    DependencyDetail dependencyDetail = new DependencyDetail
-                    {
-                        Commit = xmlNode.Attributes["Sha"].Value,
-                        Name = xmlNode.Attributes["Name"].Value,
-                        RepoUri = xmlNode.Attributes["Uri"].Value,
-                        Version = xmlNode.Attributes["Version"].Value
-                    };
-                    DependencyGraphNode dependencyGraphNode = new DependencyGraphNode(dependencyDetail.RepoUri, dependencyDetail.Commit, node.VisitedNodes);
-                    dependencyGraphNode.VisitedNodes.Add(node.Dependencies.RepoUri);
-                    node.Children.Add(dependencyGraphNode);
-                    nodesToVisit.Push(dependencyGraphNode);
-                    flatGraph.Add(dependencyGraphNode.Dependencies);
-                    xmlNodes.Push(xmlNode);
-                }
-            }
-
-            return new DependencyGraph(rootGraphNode, flatGraph);
+            Path.Combine(rootRepo, rootCommit, includeToolset ? "graph-with-toolset.xml" : "graph-without-toolset.xml"));
         }
     }
 }
