d832adb6e56a04e56939d9911ba0639188b60757
mmitche <mmitche@microsoft.com>
2019-02-07 15:15:17 -0800

    Add maestro bar client shell

---
+++ src/Maestro/SubscriptionActorService/MaestroBarClient.cs
@@ -0,0 +1,109 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.DotNet.DarcLib;
+using Microsoft.DotNet.Maestro.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubscriptionActorService
+{
+    /// <summary>
+    ///     A bar client interface for use by DarcLib which talks directly
+    ///     to the database for diamond dependency resolution.  Only a few features are required.
+    /// </summary>
+    internal class MaestroBarClient : IBarClient
+    {
+        #region Unneeded APIs
+
+        public Task AddDefaultChannelAsync(string repository, string branch, string channel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Channel> CreateChannelAsync(string name, string classification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Subscription> CreateSubscriptionAsync(string channelName, string sourceRepo, string targetRepo, string targetBranch, string updateFrequency, List<MergePolicy> mergePolicies)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Channel> DeleteChannelAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteDefaultChannelAsync(string repository, string branch, string channel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Subscription> DeleteSubscriptionAsync(Guid subscriptionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Channel> GetChannelAsync(string channel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Channel>> GetChannelsAsync(string classification = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DefaultChannel>> GetDefaultChannelsAsync(string repository = null, string branch = null, string channel = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Build> GetLatestBuildAsync(string repoUri, int channelId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Subscription> GetSubscriptionAsync(Guid subscriptionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Subscription>> GetSubscriptionsAsync(string sourceRepo = null, string targetRepo = null, int? channelId = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Subscription> TriggerSubscriptionAsync(Guid subscriptionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Subscription> UpdateSubscriptionAsync(Guid subscriptionId, SubscriptionUpdate subscription)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        public Task<IEnumerable<Asset>> GetAssetsAsync(string name = null, string version = null, int? buildId = null, bool? nonShipping = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Build> GetBuildAsync(int buildId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Build>> GetBuildsAsync(string repoUri, string commit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
