eb7ab362b39ddb6966f9b81433d27a2262125298
Alex Perovich <alperovi@microsoft.com>
2019-03-12 18:09:37 -0700

    More tests and remove angular favicon.ico

--- src/Maestro/maestro-angular/src/helpers.spec.ts
+++ src/Maestro/maestro-angular/src/helpers.spec.ts
@@ -23,8 +23,42 @@ describe("helpers", () => {
       const result = topologicalSort(nodes, getChildrenFn(nodes), getKey);
       expect(result).toEqual(expectedList);
     }
+
+    let nodeId: number;
+    beforeEach(() => {
+      nodeId = 1;
+    });
+
+    function node(...children: Node[]) {
+      return {
+        id: nodeId++,
+        children: children.map(c => c.id),
+      };
+    }
+
     it("should return empty if given empty", () => {
       runTest([], []);
+    });
+
+    it("should return a single node given a single node", () => {
+      const a = node();
+      runTest([a], [a]);
+    });
+
+    it("should sort 2 nodes properly", () => {
+      const a = node();
+      const b = node(a);
+      runTest([b, a], [a, b]);
+    });
+
+    it("should sort many nodes properly", () => {
+      const a = node();
+      const b = node(a);
+      const c = node(b, a);
+      const d = node(a, b, c);
+      const e = node(a, d);
+      const f = node(a, e);
+      runTest([f, e, b, d, a, c], [a, b, c, d, e, f]);
     });
   });
 });
