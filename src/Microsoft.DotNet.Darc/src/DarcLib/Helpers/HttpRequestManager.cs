10189738a30ffcf0f5d926fa5f2722fc7ea65f98
juanam <juanam@microsoft.com>
2019-03-19 16:12:10 -0700

    Fixes here and there

--- src/Microsoft.DotNet.Darc/src/DarcLib/Helpers/HttpRequestManager.cs
+++ src/Microsoft.DotNet.Darc/src/DarcLib/Helpers/HttpRequestManager.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -54,6 +55,17 @@ namespace Microsoft.DotNet.DarcLib
                         }
 
                         HttpResponseMessage response = await _client.SendAsync(message);
+
+                        if (response.StatusCode == HttpStatusCode.NotFound)
+                        {
+                            if (_logFailure)
+                            {
+                                _logger.LogError("A 404 (Not Found) was returned. We'll set the retries amount to 0.");
+                            }
+
+                            retriesRemaining = 0;
+                        }
+
                         response.EnsureSuccessStatusCode();
                         return response;
                     }
