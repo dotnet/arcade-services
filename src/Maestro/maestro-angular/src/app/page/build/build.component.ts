6c59ffbb9b75577b2b009bfe9f12fa0ab8a909ae
Alex Perovich <alperovi@microsoft.com>
2019-04-05 12:52:13 -0700

    Update build page to support "latest" buildid
    - If buildId == "latest" the page will reload every 5 minutes to the current latest
    - if the latest hasn't changed don't reload the page

--- src/Maestro/maestro-angular/src/app/page/build/build.component.ts
+++ src/Maestro/maestro-angular/src/app/page/build/build.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit, OnChanges } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { prettyRepository } from "src/app/util/names";
-import { map, shareReplay } from 'rxjs/operators';
+import { map, shareReplay, switchMap, filter, distinctUntilChanged, tap } from 'rxjs/operators';
 import { isAfter, compareAsc, parseISO } from "date-fns";
 
 import { BuildGraph, Build } from 'src/maestro-client/models';
 import { MaestroService } from 'src/maestro-client';
-import { Observable } from 'rxjs';
+import { Observable, of, timer } from 'rxjs';
 import { BuildStatusService } from 'src/app/services/build-status.service';
 import { BuildStatus } from 'src/app/model/build-status';
 import { statefulSwitchMap, StatefulResult, statefulPipe } from 'src/stateful';
 import { getCommitLink, getBuildLink } from 'src/helpers';
+import { Loading } from 'src/stateful/helpers';
 
 interface AzDevBuildInfo {
   isMostRecent: boolean;
@@ -33,24 +34,105 @@ export class BuildComponent implements OnInit, OnChanges {
 
   public includeToolsets: boolean = false;
 
+  static buildCache: Record<number, Build> = {};
+  private getBuild(buildId: number): Observable<StatefulResult<Build>> {
+    return of(buildId).pipe(
+      statefulSwitchMap(id => {
+        if (id in BuildComponent.buildCache) {
+          return of(BuildComponent.buildCache[buildId]);
+        }
+        return this.maestro.builds.getBuildAsync({id}).pipe(
+          tap(build => BuildComponent.buildCache[id] = build),
+        );
+      }),
+    );
+  }
+
+  // reload the data every 5 minutes
+  static reloadInterval = 1000 * 60 * 5;
+
+  private getLatestBuildId(channelId: number, repository: string): Observable<StatefulResult<number>> {
+    let emittedLoading = false;
+    return timer(0, BuildComponent.reloadInterval).pipe(
+      statefulSwitchMap(() => {
+        return this.maestro.builds.getLatestAsync({
+          channelId,
+          repository,
+        }).pipe(
+          map(build => {
+            // dump the build in the cache so we don't hit the server again for it
+            BuildComponent.buildCache[build.id] = build;
+            return build.id;
+          }),
+        );
+      }),
+      filter(r => {
+        if (!(r instanceof Loading)) {
+          return true;
+        }
+        // emit only the first "Loading" instance so refreshes don't cause the loading spinner to show up
+        if (!emittedLoading)  {
+          emittedLoading = true;
+          return true;
+        }
+        return false;
+      }),
+      statefulPipe(
+        distinctUntilChanged(), // don't re-emit the same buildid
+      ),
+      tap(b => console.log("Latest: ", b)),
+    );
+  }
+
   public ngOnInit() {
     const buildId$ = this.route.paramMap.pipe(
-      map(params => +(params.get("buildId") as string)),
-    );
-    this.graph$ = buildId$.pipe(
-      statefulSwitchMap(buildId => {
-        return this.maestro.builds.getBuildGraphAsync({id: buildId});
+      map(params => {
+        const buildId = params.get("buildId");
+        const channelId = params.get("channelId");
+        const repository = params.get("repository");
+        if (buildId == null) {
+          throw new Error("buildId was null");
+        }
+        if (channelId == null) {
+          throw new Error("channelId was null");
+        }
+        if (repository == null) {
+          throw new Error("repository was null");
+        }
+        return {buildId, channelId, repository};
+      }),
+      tap(v => console.log("Params: ", v)),
+      switchMap(params => {
+        if (params.buildId == "latest") {
+          return this.getLatestBuildId(+params.channelId, params.repository);
+        }
+        else {
+          return of(params.buildId);
+        }
       }),
       shareReplay(1),
     );
     this.build$ = buildId$.pipe(
-      statefulSwitchMap(buildId => {
-        return this.maestro.builds.getBuildAsync({id: buildId});
-      }),
+      statefulPipe(
+        switchMap(id => this.getBuild(id)),
+      ),
+    );
+    this.graph$ = buildId$.pipe(
+      statefulPipe(
+        statefulSwitchMap((id) => {
+          return this.maestro.builds.getBuildGraphAsync({id: id});
+        }),
+      ),
     );
 
     this.azDevBuildInfo$ = this.build$.pipe(
       statefulPipe(
+        switchMap(b => {
+          return timer(0, BuildComponent.reloadInterval).pipe(
+            map(() => b),
+          );
+        }),
+        tap(() => console.log("getting azdev info")),
         statefulSwitchMap(b => this.getBuildInfo(b)),
       ),
     );
