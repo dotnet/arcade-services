83237be3b3a4206608b0ac7a2abdb7a7dc4fd1f3
Ricardo Arenas <riarenas@microsoft.com>
2019-04-18 10:10:48 -0700

    First shot at refactoring the test scripts (#344)

--- src/Maestro/tests/Scenarios/default-channels.ps1
+++ src/Maestro/tests/Scenarios/default-channels.ps1
@@ -42,8 +42,6 @@ try {
     try { Darc-Delete-Channel $testChannel2Name } catch {}
     Darc-Add-Channel $testChannel1Name "test"
     Darc-Add-Channel $testChannel2Name "test"
-    $channelsToDelete += $testChannel1Name
-    $channelsToDelete += $testChannel2Name
 
     # Adding default channels'
     Write-Host "Creating default channels"
@@ -51,14 +49,12 @@ try {
     try { Darc-Delete-Default-Channel $testChannel2Name $repoUri $branchName } catch {}
     Darc-Add-Default-Channel $testChannel1Name $repoUri $branchName
     Darc-Add-Default-Channel $testChannel2Name $repoUri $branchName
-    $defaultChannelsToDelete += @{ channel = $testChannel1Name; repo = $repoUri; branch = $branchName }
-    $defaultChannelsToDelete += @{ channel = $testChannel2Name; repo = $repoUri; branch = $branchName }
 
     Write-Host "Set up build for intake into target repository"
     
     # Create a build for the source repo
     $buildId = New-Build -repository $repoUri -branch $branchName -commit $commit -buildNumber $buildNumber -assets $assets
-    
+
     # Look up the build and ensure that the channels were added
 
     $buildInfo = Get-Build $buildId
