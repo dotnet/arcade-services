29ec9873505c843dc962c1812523506f09dcf49d
mmitche <mmitche@microsoft.com>
2019-05-23 10:28:06 -0700

    Add command get-asset to get information on assets Simple lookup for assets, useful for answering random questions

---
+++ src/Microsoft.DotNet.Darc/src/Darc/Options/GetAssetCommandLineOptions.cs
@@ -0,0 +1,32 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CommandLine;
+using Microsoft.DotNet.Darc.Operations;
+using Microsoft.DotNet.DarcLib;
+using System.Collections.Generic;
+
+namespace Microsoft.DotNet.Darc.Options
+{
+    [Verb("get-asset", HelpText = "Get information about an asset.")]
+    internal class GetAssetCommandLineOptions : CommandLineOptions
+    {
+        [Option("name", Required = true, HelpText = "Name of asset to look up")]
+        public string Name { get; set; }
+
+        [Option("version", HelpText = "Look up specific version of an asset.")]
+        public string Version { get; set; }
+
+        [Option("channel", HelpText = "Look up the asset produced from builds applied to this channel.")]
+        public string Channel { get; set; }
+
+        [Option("max-lookup", Default = 10, HelpText = "Maximum number of builds to look up.")]
+        public int MaxBuildLookups { get; set; }
+
+        public override Operation GetOperation()
+        {
+            return new GetAssetOperation(this);
+        }
+    }
+}
