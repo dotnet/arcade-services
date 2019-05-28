199b227fd92a1ce97df2ef2cb01e02737b02c129
Matt Mitchell <mmitche@microsoft.com>
2019-05-28 13:25:58 -0700

    Add missing header

--- src/Microsoft.DotNet.Darc/src/DarcLib/HealthMetrics/SubscriptionHealthMetric.cs
+++ src/Microsoft.DotNet.Darc/src/DarcLib/HealthMetrics/SubscriptionHealthMetric.cs
@@ -1,10 +1,13 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.DotNet.Maestro.Client.Models;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Microsoft.DotNet.Maestro.Client.Models;
 
 namespace Microsoft.DotNet.DarcLib.HealthMetrics
 {
