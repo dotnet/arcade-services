695b2a8529e30259f879721fdcdcc0b0274cae1e
Ricardo Arenas <riarenas@microsoft.com>
2019-06-06 08:36:24 -0700

    Remove extra space in PR title now that we insert a space before every Repo name (#452)
    * remove extra space now that we insert a space before every repo name
    
    * Also remove the extra space from batched scenario tests

--- src/Maestro/tests/Scenarios/common.ps1
+++ src/Maestro/tests/Scenarios/common.ps1
@@ -521,7 +521,7 @@ function Check-NonBatched-AzDO-PullRequest($sourceRepoName, $targetRepoName, $ta
 }
 
 function Check-Batched-AzDO-PullRequest($sourceRepoCount, $targetRepoName, $targetBranch, $expectedDependencies) {
-    $expectedPRTitle = "[$targetBranch] Update dependencies from  $sourceRepoCount repositories"
+    $expectedPRTitle = "[$targetBranch] Update dependencies from $sourceRepoCount repositories"
     return Check-AzDO-PullRequest $expectedPRTitle $targetRepoName $targetBranch $expectedDependencies $false
 }
 
@@ -722,7 +722,7 @@ function Check-NonBatched-Github-PullRequest($sourceRepoName, $targetRepoName, $
 }
 
 function Check-Batched-Github-PullRequest($sourceRepoCount, $targetRepoName, $targetBranch, $expectedDependencies) {
-    $expectedPRTitle = "[$targetBranch] Update dependencies from  $sourceRepoCount repositories"
+    $expectedPRTitle = "[$targetBranch] Update dependencies from $sourceRepoCount repositories"
     return Check-Github-PullRequest $expectedPRTitle $targetRepoName $targetBranch $expectedDependencies $false
 }
 
