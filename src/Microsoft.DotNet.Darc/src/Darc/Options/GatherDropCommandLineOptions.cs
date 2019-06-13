905f906f94384b56dc99f656259ce2aa074755d3
Divino César <disoares@microsoft.com>
2019-06-12 19:15:17 -0700

    Add `-latest` option to gather-drop (#459)
    * Add `-lates` option to gather-drop
    
    * Some fixes after more tests.

--- src/Microsoft.DotNet.Darc/src/Darc/Options/GatherDropCommandLineOptions.cs
+++ src/Microsoft.DotNet.Darc/src/Darc/Options/GatherDropCommandLineOptions.cs
@@ -64,6 +64,10 @@ namespace Microsoft.DotNet.Darc.Options
         [Option("skip-existing", HelpText = "Skip files that already exist at the destination.")]
         public bool SkipExisting { get; set; }
 
+        [Option("latest-location", HelpText = "Download assets from their latest known location.")]
+        public bool LatestLocation { get; set; }
+
+
         public override Operation GetOperation()
         {
             return new GatherDropOperation(this);
