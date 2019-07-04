6f33c560e38a9c053ff59ceb46bfa48de45d0379
Cesar Soares Lucas <disoares@microsoft.com>
2019-07-04 14:12:17 -0700

    Parse / recognize azure storage proxy feeds

--- src/Microsoft.DotNet.Darc/src/DarcLib/Helpers/GitFileManager.cs
+++ src/Microsoft.DotNet.Darc/src/DarcLib/Helpers/GitFileManager.cs
@@ -25,7 +25,12 @@ namespace Microsoft.DotNet.DarcLib
         // Matches package feeds like
         // https://dnceng.pkgs.visualstudio.com/public/_packaging/darc-pub-arcade-fd8184c3fcde81eb27ca4c061c6e171f418d753f-1
         private const string MaestroManagedFeedPattern =
-            @"https://(\w+).pkgs.visualstudio.com/(public/){0,1}_packaging/darc-(int|pub)-(.+?)-([A-Fa-f0-9]{7,40})-?(\d*)/nuget/v\d+/index.json";
+            @"https://(?<organization>\w+).pkgs.visualstudio.com/(public/){0,1}_packaging/darc-(?<type>(int|pub))-(?<repository>.+?)-(?<sha>[A-Fa-f0-9]{7,40})-?(?<subversion>\d*)/nuget/v\d+/index.json";
+
+        // Matches package feeds like
+        // https://dotnet-feed-internal.azurewebsites.net/container/dotnet-core-internal/sig/dsdfasdfasdf234234s/se/2020-02-02/darc-int-dotnet-arcade-services-babababababe-08/nuget/v3/index.json
+        private const string AzureStorageProxyFeedPattern =
+            @"https://([a-z-]+).azurewebsites.net/container/([^/]+)/sig/\w+/se/([0-9]{4}-[0-9]{2}-[0-9]{2})/darc-(?<type>int)-(?<repository>.+?)-(?<sha>[A-Fa-f0-9]{7,40})-?(?<subversion>\d*)/nuget/v\d+/index.json";
 
         public GitFileManager(IGitRepo gitRepo, ILogger logger)
         {
@@ -252,7 +257,8 @@ namespace Microsoft.DotNet.DarcLib
 
         private bool IsMaestroManagedFeed(string feed)
         {
-            return Regex.IsMatch(feed, MaestroManagedFeedPattern);
+            return Regex.IsMatch(feed, MaestroManagedFeedPattern) || 
+                Regex.IsMatch(feed, AzureStorageProxyFeedPattern);
         }
 
         private XmlDocument UpdatePackageSources(XmlDocument nugetConfig, HashSet<string> maestroManagedFeeds)
@@ -941,7 +947,7 @@ namespace Microsoft.DotNet.DarcLib
 
             foreach (string feed in feeds)
             {
-                var parsedFeed = ParseMaestroManagedFeed(feed);
+                var parsedFeed = ParseFeed(feed);
 
                 string key = $"darc-{parsedFeed.type}-{parsedFeed.repoName}-{parsedFeed.sha.Substring(0, 7)}";
                 if (!string.IsNullOrEmpty(parsedFeed.subVersion))
@@ -953,22 +959,27 @@ namespace Microsoft.DotNet.DarcLib
             return sources;
         }
 
-        private (string org, string repoName, string type, string sha, string subVersion) ParseMaestroManagedFeed(string feed)
+        private (string org, string repoName, string type, string sha, string subVersion) ParseFeed(string feed)
         {
             var match = Regex.Match(feed, MaestroManagedFeedPattern);
+
+            match = match.Success ? 
+                match : 
+                Regex.Match(feed, AzureStorageProxyFeedPattern);
+
             if (match.Success)
             {
-                string org = match.Groups[1].Value;
-                string repo = match.Groups[4].Value;
-                string type = match.Groups[3].Value;
-                string sha = match.Groups[5].Value;
-                string subVersion = match.Groups[6].Value;
+                string org = match.Groups["organization"].Value;
+                string repo = match.Groups["repository"].Value;
+                string type = match.Groups["type"].Value;
+                string sha = match.Groups["sha"].Value;
+                string subVersion = match.Groups["subversion"].Value;
                 return (org, repo, type, sha, subVersion);
             }
             else
             {
-                _logger.LogError($"Unable to parse feed { feed } as a Maestro managed feed");
-                throw new ArgumentException($"feed { feed } is not a valid Maestro managed feed");
+                _logger.LogError($"Unable to parse feed '{ feed }' ");
+                throw new ArgumentException($"feed { feed } is not a valid Maestro managed feed or Azure Storage proxy feed.");
             }
         }
 
