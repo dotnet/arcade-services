4dcaf9b900c48b8291651023d34207180a1db580
juanam <juanam@microsoft.com>
2019-07-22 14:09:25 -0700

    Up timeout and add token caching

--- src/Maestro/Maestro.GitHub/GitHubTokenProvider.cs
+++ src/Maestro/Maestro.GitHub/GitHubTokenProvider.cs
@@ -7,6 +7,8 @@ using Microsoft.DotNet.DarcLib;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Octokit;
+using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -15,6 +17,7 @@ namespace Maestro.GitHub
     public class GitHubTokenProvider : IGitHubTokenProvider
     {
         private readonly IOptions<GitHubTokenProviderOptions> _options;
+        private readonly Dictionary<long, AccessToken> _tokenCache;
         public ILogger<GitHubTokenProvider> _logger;
 
         public GitHubTokenProvider(IOptions<GitHubTokenProviderOptions> options,
@@ -22,12 +25,18 @@ namespace Maestro.GitHub
         {
             _options = options;
             _logger = logger;
+            _tokenCache = new Dictionary<long, AccessToken>();
         }
 
         public GitHubTokenProviderOptions Options => _options.Value;
 
         public async Task<string> GetTokenForInstallation(long installationId)
         {
+            if (TryGetCachedToken(installationId, out string cachedToken))
+            {
+                return cachedToken;
+            }
+
             return await ExponentialRetry.RetryAsync(
                 async () =>
                 {
@@ -35,6 +44,7 @@ namespace Maestro.GitHub
                     var product = new ProductHeaderValue(Options.ApplicationName, Options.ApplicationVersion);
                     var appClient = new Octokit.GitHubClient(product) { Credentials = new Credentials(jwt, AuthenticationType.Bearer) };
                     AccessToken token = await appClient.GitHubApps.CreateInstallationToken(installationId);
+                    UpdateTokenCache(installationId, token);
                     return token.Token;
                 },
                 ex => _logger.LogError(ex, $"Failed to get a github token for installation id {installationId}, retrying"),
@@ -51,6 +61,40 @@ namespace Maestro.GitHub
                     ExpirationSeconds = 600
                 });
             return generator.CreateEncodedJwtToken();
+        }
+
+        private bool TryGetCachedToken(long installationId, out string cachedToken)
+        {
+            cachedToken = null;
+
+            if (!_tokenCache.ContainsKey(installationId))
+            {
+                return false;
+            }
+
+            AccessToken token = _tokenCache[installationId];
+
+            // If the cached token will expire in less than 15 minutes we won't use it and let GetTokenForInstallation generate a new one
+            // and update the cache
+            if (DateTimeOffset.Now.Subtract(token.ExpiresAt).TotalMinutes < 15)
+            {
+                return false;
+            }
+
+            cachedToken = token.Token;
+            return true;
+        }
+
+        private void UpdateTokenCache(long installationId, AccessToken accessToken)
+        {
+            if (_tokenCache.ContainsKey(installationId))
+            {
+                _tokenCache[installationId] = accessToken;
+            }
+            else
+            {
+                _tokenCache.Add(installationId, accessToken);
+            }
         }
     }
 }
