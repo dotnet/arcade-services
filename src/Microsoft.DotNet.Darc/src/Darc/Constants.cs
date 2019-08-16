296934c5fae4cc5785837cc0a53e6981f4d61467
Matt Mitchell <mmitche@microsoft.com>
2019-08-16 15:07:17 -0700

    Update src/Microsoft.DotNet.Darc/src/Darc/Constants.cs
    Co-Authored-By: Ricardo Arenas <riarenas@microsoft.com>

--- src/Microsoft.DotNet.Darc/src/Darc/Constants.cs
+++ src/Microsoft.DotNet.Darc/src/Darc/Constants.cs
@@ -16,7 +16,7 @@ namespace Microsoft.DotNet.Darc
         public const int MaxPopupTries = 3;
         public static string DarcDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".darc");
 
-        public const string AllCheckSuccessfulMergePolicyName = "AllChecksSuccessul";
+        public const string AllCheckSuccessfulMergePolicyName = "AllChecksSuccessful";
         public const string StandardMergePolicyName = "Standard";
         public const string NoExtraCommitsMergePolicyName = "NoExtraCommits";
         public const string NoRequestedChangesMergePolicyName = "NoRequestedChanges";
