5979e0d81b85d200f62a15a5845f831d12e90172
Lukas Lansky <lulansky@microsoft.com>
2019-10-30 13:23:27 +0100

    Tests for GetParsedCommand

--- src/Microsoft.DotNet.Darc/tests/Microsoft.DotNet.Darc.Tests/Helpers/UxManagerHelpers.cs
+++ src/Microsoft.DotNet.Darc/tests/Microsoft.DotNet.Darc.Tests/Helpers/UxManagerHelpers.cs
@@ -6,9 +6,30 @@ namespace Microsoft.DotNet.Darc.Tests.Helpers
     public class UxManagerHelpers
     {
         [Fact]
-        public void GetParsedCommandHandlesFoldersWithSpacesWell()
+        public void PathCodeLocation()
         {
-            UxManager.GetParsedCommand("");
+            var codeCommand = UxManager.GetParsedCommand("code --wait");
+
+            Assert.Equal("code", codeCommand.FileName);
+            Assert.Equal(" --wait", codeCommand.Arguments);
+        }
+
+        [Fact]
+        public void EscapedCodeLocation()
+        {
+            var codeCommand = UxManager.GetParsedCommand(@"'C:\Users\lulansky\AppData\Local\Programs\Microsoft VS Code\Code.exe' --wait");
+
+            Assert.Equal(@"C:\Users\lulansky\AppData\Local\Programs\Microsoft VS Code\Code.exe", codeCommand.FileName);
+            Assert.Equal(" --wait", codeCommand.Arguments);
+        }
+
+        [Fact]
+        public void EscapedCodeLocationWithoutArg()
+        {
+            var codeCommand = UxManager.GetParsedCommand(@"'C:\Users\lulansky\AppData\Local\Programs\Microsoft VS Code\Code.exe'");
+
+            Assert.Equal(@"C:\Users\lulansky\AppData\Local\Programs\Microsoft VS Code\Code.exe", codeCommand.FileName);
+            Assert.Equal("", codeCommand.Arguments);
         }
     }
 }
