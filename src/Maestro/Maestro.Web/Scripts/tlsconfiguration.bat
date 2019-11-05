695c48a43756b18bb613e8dc3082fc39a8bde0e8
Premek Vysoky <prvysoky@microsoft.com>
2019-11-05 16:10:25 +0100

    Fix paths

--- src/Maestro/Maestro.Web/Scripts/tlsconfiguration.bat
+++ src/Maestro/Maestro.Web/Scripts/tlsconfiguration.bat
@@ -4,5 +4,5 @@ SET PollingMode=PollingMode4min
 SET TimeSyncLogFile="%TEMP%\w32time.log"
 
 echo "--- Configuring TLS ---" >> "%LOGFILE%" 2>&1
-POWERSHELL -ExecutionPolicy Unrestricted -File ".\Set-TlsConfiguration.ps1" >> "%LOGFILE%" 2>&1
+POWERSHELL -ExecutionPolicy Unrestricted -File ".\Scripts\Set-TlsConfiguration.ps1" >> "%LOGFILE%" 2>&1
 if %errorlevel% neq 0 if %errorlevel% neq 3010 exit /b %errorlevel%
