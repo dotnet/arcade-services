d5db7ff6e9d5a4e41931c478abfca3628ff67747
Matt Mitchell <mmitche@microsoft.com>
2019-11-20 09:53:51 -0800

    Allow deletion of subscriptions based on filters. (#794)
    * Allow deletion of subscriptions based on filters.
    Deprecate delete-subscription and replace with delete-subscriptions (same parameter set supported, plus all the filters of get-subscriptions and trigger subscriptions). This will allow easier cleanup of large numbers of subscriptions.
    
    * Update src/Microsoft.DotNet.Darc/src/Darc/Operations/DeleteSubscriptionsOperation.cs
    
    Co-Authored-By: Ricardo Arenas <riarenas@microsoft.com>

--- src/Microsoft.DotNet.Darc/src/Darc/Options/DeleteSubscriptionCommandLineOptions.cs
+++ src/Microsoft.DotNet.Darc/src/Darc/Options/DeleteSubscriptionCommandLineOptions.cs
@@ -7,7 +7,7 @@ using Microsoft.DotNet.Darc.Operations;
 
 namespace Microsoft.DotNet.Darc.Options
 {
-    [Verb("delete-subscription", HelpText = "Remove a subscription.")]
+    [Verb("delete-subscription", Hidden = true, HelpText = "Please use delete-subscriptions.")]
     class DeleteSubscriptionCommandLineOptions : CommandLineOptions
     {
         [Option('i', "id", Required = true, HelpText = "ID of subscription to delete.")]
