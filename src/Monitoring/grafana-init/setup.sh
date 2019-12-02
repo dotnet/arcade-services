3617429443237c0c908319b49a147a146e38e94c
Chad Nedzlek <chadnedz@microsoft.com>
2019-12-02 14:52:11 -0800

    Casing matters

--- src/Monitoring/grafana-init/setup.sh
+++ src/Monitoring/grafana-init/setup.sh
@@ -116,7 +116,7 @@ Environment=GRAFANA_BIN=${GRAFANA_BIN}
 Environment=GF_SERVER_DOMAIN=${GRAFANA_DOMAIN}
 Environment=GF_SECURITY_SECRET_KEY=[vault(dotnet-grafana/grafana-aes-256-secret-key)]
 Environment=GF_AUTH_AUTH_GITHUB_CLIENT_ID=${GRAFANA_GITHUB_APP_ID}
-Environment=GF_AUTH_AUTH_GITHUB_CLIENT_SECRET=[vault(dotnet-grafana/dotnet-grafana-${Environment}-github-oauth-app-secret)]
+Environment=GF_AUTH_AUTH_GITHUB_CLIENT_SECRET=[vault(dotnet-grafana/dotnet-grafana-${ENVIRONMENT}-github-oauth-app-secret)]
 Environment=GF_EXTERNAL_IMAGE_STORAGE_AZURE_BLOB_ACCOUNT_KEY=[vault(dotnet-grafana/dotnetgrafana-storage-account-key)]
 EOT
 
