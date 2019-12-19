d5522a904bfcc8cfcff21c1767fc9c081df1b482
Chad Nedzlek <chadnedz@microsoft.com>
2019-12-18 17:55:24 -0800

    Microsoft.DotNet.Monitoring.Sdk

---
+++ src/Monitoring/Sdk/MonitoringImport.cs
@@ -0,0 +1,40 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading.Tasks;
+using Microsoft.Build.Framework;
+using Task = Microsoft.Build.Utilities.Task;
+
+namespace Microsoft.DotNet.Monitoring.Sdk
+{
+    public class MonitoringImport: Task
+    {
+        [Required]
+        public string Host { get; set; }
+
+        [Required]
+        public string AccessToken { get; set; }
+
+        [Required]
+        public string DashboardId { get; set; }
+        
+        [Required]
+        public string DashboardDirectory { get; set; }
+
+        [Required]
+        public string DataSourceDirectory{ get; set; }
+
+        public sealed override bool Execute()
+        {
+            return ExecuteAsync().GetAwaiter().GetResult();
+        }
+
+        private async Task<bool> ExecuteAsync()
+        {
+            Log.LogMessage(MessageImportance.Normal, "Importing grafana dashboard '{0}' form '{1}' to '{2}'", DashboardId, Host, DashboardDirectory);
+            Log.LogMessage(MessageImportance.Low, "Importing grafana data sources to '{0}'", DataSourceDirectory);
+            return true;
+        }
+    }
+}
---
+++ src/Monitoring/Sdk/MonitoringPublish.cs
@@ -0,0 +1,50 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading.Tasks;
+using Microsoft.Build.Framework;
+using BuildTask = Microsoft.Build.Utilities.Task;
+
+namespace Microsoft.DotNet.Monitoring.Sdk
+{
+    public class MonitoringPublish : BuildTask
+    {
+        [Required]
+        public string Host { get; set; }
+
+        [Required]
+        public string AccessToken { get; set; }
+
+        [Required]
+        public ITaskItem[] Dashboard { get; set; }
+
+        [Required]
+        public ITaskItem[] DataSource { get; set; }
+
+        public sealed override bool Execute()
+        {
+            return ExecuteAsync().GetAwaiter().GetResult();
+        }
+
+        private async Task<bool> ExecuteAsync()
+        {
+            Log.LogMessage(MessageImportance.Low, "Uploading grafana dashboards to '{0}'", Host);
+            
+            foreach (ITaskItem dataSource in DataSource)
+            {
+                string dataSourcePath = dataSource.ItemSpec;
+
+                Log.LogMessage(MessageImportance.Low, "Uploading data source '{0}'", dataSourcePath);
+            }
+            foreach (ITaskItem dashboard in Dashboard)
+            {
+                string dashboardPath = dashboard.ItemSpec;
+
+                Log.LogMessage(MessageImportance.Low, "Uploading dashboard '{0}'", dashboardPath);
+            }
+
+            return true;
+        }
+    }
+}
