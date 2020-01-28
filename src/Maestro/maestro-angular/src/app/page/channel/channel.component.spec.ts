67f625d8877b4f8267593fe5904f934311ac916b
Michelle McDaniel <michelm@microsoft.com>
2020-01-28 14:18:25 -0800

    Attempting to get the ui tests working

--- src/Maestro/maestro-angular/src/app/page/channel/channel.component.spec.ts
+++ src/Maestro/maestro-angular/src/app/page/channel/channel.component.spec.ts
@@ -1,7 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ChannelComponent } from './channel.component';
+import { RouterTestingModule } from '@angular/router/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+import { StatefulModule } from 'src/stateful';
+import { ChannelComponent } from './channel.component';
+
+import { MaestroService } from 'src/maestro-client';
+import { ChannelService} from 'src/app/services/channel.service';
 
 describe('ChannelComponent', () => {
   let component: ChannelComponent;
@@ -10,6 +15,22 @@ describe('ChannelComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ ChannelComponent ],
+      providers: [
+        {
+          provide: MaestroService,
+          useValue: {
+          },
+        },
+        {
+          provide: ChannelService,
+          useValue: {
+          },
+        }
+      ],
+      imports: [
+        RouterTestingModule,
+        StatefulModule,
+      ],
       schemas: [NO_ERRORS_SCHEMA],
     })
     .compileComponents();
