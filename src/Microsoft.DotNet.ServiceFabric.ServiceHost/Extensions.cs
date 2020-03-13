d9cfc8b1c425e8e04e74060a4306a3d941695c7b
Chad Nedzlek <ChadNedzlek@users.noreply.github.com>
2020-03-13 15:09:57 -0700

    Add section fetching overload to Configure (#1044)

--- src/Microsoft.DotNet.ServiceFabric.ServiceHost/Extensions.cs
+++ src/Microsoft.DotNet.ServiceFabric.ServiceHost/Extensions.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
@@ -29,13 +30,27 @@ namespace Microsoft.DotNet.ServiceFabric.ServiceHost
             cancellationToken.Register(s => ((TaskCompletionSource<bool>) s).SetResult(true), tcs);
             return tcs.Task;
         }
-
+        
         public static IServiceCollection Configure<TOptions>(
             this IServiceCollection services,
             Action<TOptions, IServiceProvider> configure) where TOptions : class
         {
             return services.AddSingleton<IConfigureOptions<TOptions>>(
                 provider => new ConfigureOptions<TOptions>(options => configure(options, provider)));
+        }
+
+        public static IServiceCollection Configure<TOptions>(
+            this IServiceCollection services,
+            string sectionName,
+            Action<TOptions, IConfiguration> configure) where TOptions : class
+        {
+            return services.AddSingleton<IConfigureOptions<TOptions>>(
+                provider => new ConfigureOptions<TOptions>(
+                    options => configure(options,
+                        provider.GetRequiredService<IConfiguration>().GetSection(sectionName)
+                    )
+                )
+            );
         }
     }
 }
