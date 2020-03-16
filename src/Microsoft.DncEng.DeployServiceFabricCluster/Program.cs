402c4c137df1c80d356fd803cd509e3d2bc5bcec
Alex Perovich <alperovi@microsoft.com>
2020-03-16 15:33:04 -0700

    PR Feedback

--- src/Microsoft.DncEng.DeployServiceFabricCluster/Program.cs
+++ src/Microsoft.DncEng.DeployServiceFabricCluster/Program.cs
@@ -8,28 +8,12 @@ using System.Threading.Tasks;
 using Microsoft.DncEng.Configuration.Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Mono.Options;
 
 namespace Microsoft.DncEng.DeployServiceFabricCluster
 {
-    class HostEnvironment : IHostEnvironment
-    {
-        public HostEnvironment(string environmentName, string contentRootPath)
-        {
-            EnvironmentName = environmentName;
-            ContentRootPath = contentRootPath;
-        }
-
-        public string EnvironmentName { get; set; }
-        public string ApplicationName { get; set; }
-        public string ContentRootPath { get; set; }
-        public IFileProvider ContentRootFileProvider { get; set; }
-    }
-
-    class Program
+    public static class Program
     {
         static async Task Main(string[] args)
         {
@@ -41,7 +25,7 @@ namespace Microsoft.DncEng.DeployServiceFabricCluster
                 {"e|environment=", e => environment = e},
             };
 
-            var remaining = options.Parse(args);
+            List<string> remaining = options.Parse(args);
             if (remaining.Any())
             {
                 Fatal(1, $"argument '{remaining[0]}' not recognized.");
@@ -55,11 +39,11 @@ namespace Microsoft.DncEng.DeployServiceFabricCluster
                 Fatal(3, $"config file '{configFile}' does not exist.");
             }
 
-            var configFileName = Path.GetFileNameWithoutExtension(configFile);
-            var configExtension = Path.GetExtension(configFile);
-            var configDir = Path.GetDirectoryName(configFile);
+            string? configFileName = Path.GetFileNameWithoutExtension(configFile);
+            string? configExtension = Path.GetExtension(configFile);
+            string? configDir = Path.GetDirectoryName(configFile);
 
-            var configuration = new ConfigurationBuilder()
+            IConfigurationRoot configuration = new ConfigurationBuilder()
                 .SetBasePath(Path.GetDirectoryName(configFile))
                 .AddDefaultJsonConfiguration(new HostEnvironment(environment, configDir),
                     configFileName + "{0}" + configExtension)
@@ -77,10 +61,10 @@ namespace Microsoft.DncEng.DeployServiceFabricCluster
                 builder.AddConsole();
             });
 
-            await using var provider = services.BuildServiceProvider();
+            await using ServiceProvider provider = services.BuildServiceProvider();
 
-            var loggerFactory = provider.GetRequiredService<ILoggerFactory>();
-            var logger = loggerFactory.CreateLogger("ClusterCreation");
+            ILoggerFactory loggerFactory = provider.GetRequiredService<ILoggerFactory>();
+            ILogger logger = loggerFactory.CreateLogger("ClusterCreation");
             var creator = new ServiceFabricClusterCreator(config, logger);
 
             var cts = new CancellationTokenSource();
@@ -100,66 +84,15 @@ namespace Microsoft.DncEng.DeployServiceFabricCluster
         [DoesNotReturn]
         private static void Fatal(int code, string errorMessage)
         {
-            Console.Error.WriteLine("fatal: " + errorMessage);
+            if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("BUILD_BUILDNUMBER")))
+            {
+                Console.Error.WriteLine("fatal: " + errorMessage);
+            }
+            else
+            {
+                Console.Error.WriteLine($"##vso[task.logissue type=error]{errorMessage}");
+            }
             Environment.Exit(code);
         }
-    }
-
-    public class ServiceFabricClusterConfiguration
-    {
-        public void Validate()
-        {
-            if (NodeTypes.All(nt => nt.Name != "Primary"))
-            {
-                throw new ArgumentException("Must have a single node type named 'Primary'");
-            }
-            NodeTypes = NodeTypes.OrderBy(nt => nt.Name == "Primary" ? 0 : 1).ToList();
-        }
-
-        public string Name { get; set; }
-
-        public string Location { get; set; }
-
-        public string ResourceGroup { get; set; }
-
-        public Guid SubscriptionId { get; set; }
-
-        public string AdminUsername { get; set; }
-        public string AdminPassword { get; set; }
-
-        public int TcpGatewayPort { get; set; } = 19000;
-        public int HttpGatewayPort { get; set; } = 19080;
-
-        public List<ServiceFabricNodeType> NodeTypes { get; set; }
-        public string CertificateCommonName { get; set; }
-        public string AdminClientCertificateCommonName { get; set; }
-        public string AdminClientCertificateIssuerThumbprint { get; set; }
-        public string CertificateSourceVaultId { get; set; }
-        public List<string> CertificateUrls { get; set; }
-    }
-
-    public class ServiceFabricNodeType
-    {
-        public string Name { get; set; }
-
-        public List<ServiceFabricNodeEndpoint> Endpoints { get; set; }
-        public int InstanceCount { get; set; }
-        public string UserAssignedIdentityId { get; set; }
-        public string Sku { get; set; }
-        public ServiceFabricNodeVmImage VmImage { get; set; }
-    }
-
-    public class ServiceFabricNodeVmImage
-    {
-        public string Publisher { get; set; }
-        public string Offer { get; set; }
-        public string Sku { get; set; }
-        public string Version { get; set; }
-    }
-
-    public class ServiceFabricNodeEndpoint
-    {
-        public int ExternalPort { get; set; }
-        public int InternalPort { get; set; }
     }
 }
