19ce482f8c4ba6918bbc39737b7cc7186bb0a0f7
Andy Zivkovic <zivkan@users.noreply.github.com>
2020-03-26 10:25:53 -0700

    Output build id as msbuild property in PushMetadataToBuildAssetRegistry (#1070)
    * Output build id as msbuild property in PushMetadataToBuildAssetRegistry
    
    * Use int rather than string

--- src/Maestro/Microsoft.DotNet.Maestro.Tasks/src/PushMetadataToBuildAssetRegistry.cs
+++ src/Maestro/Microsoft.DotNet.Maestro.Tasks/src/PushMetadataToBuildAssetRegistry.cs
@@ -39,6 +39,9 @@ namespace Microsoft.DotNet.Maestro.Tasks
 
         public string RepoRoot { get; set; }
 
+        [Output]
+        public int BuildId { get; set; }
+
         private const string SearchPattern = "*.xml";
         private readonly CancellationTokenSource _tokenSource = new CancellationTokenSource();
 
@@ -93,6 +96,7 @@ namespace Microsoft.DotNet.Maestro.Tasks
                     finalBuild.Dependencies = deps;
 
                     Client.Models.Build recordedBuild = await client.Builds.CreateAsync(finalBuild, cancellationToken);
+                    BuildId = recordedBuild.Id;
 
                     Log.LogMessage(MessageImportance.High, $"Metadata has been pushed. Build id in the Build Asset Registry is '{recordedBuild.Id}'");
 
