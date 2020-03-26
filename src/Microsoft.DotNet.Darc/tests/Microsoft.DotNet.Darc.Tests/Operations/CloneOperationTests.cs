fa0e8a8d9502e985c659ffa63a2a4f2966c77a08
Davis Goodin <dagood@microsoft.com>
2020-03-26 12:48:05 -0500

    Add ability to create "darc clone" unit tests

---
+++ src/Microsoft.DotNet.Darc/tests/Microsoft.DotNet.Darc.Tests/Operations/CloneOperationTests.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.DotNet.Darc.Operations;
+
+namespace Microsoft.DotNet.Darc.Tests.Operations
+{
+    public class CloneOperationTests
+    {
+        internal CloneOperation op = new CloneOperation(null);
+    }
+}
