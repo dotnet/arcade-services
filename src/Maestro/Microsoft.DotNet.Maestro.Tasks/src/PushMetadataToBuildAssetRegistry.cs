bec729e03c04b969fa0c38a26c075398da7c3ce9
Cesar Soares Lucas <disoares@microsoft.com>
2020-04-14 15:27:20 -0700

    Don't need this anymore

--- src/Maestro/Microsoft.DotNet.Maestro.Tasks/src/PushMetadataToBuildAssetRegistry.cs
+++ src/Maestro/Microsoft.DotNet.Maestro.Tasks/src/PushMetadataToBuildAssetRegistry.cs
@@ -13,7 +13,6 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
@@ -32,8 +31,6 @@ namespace Microsoft.DotNet.Maestro.Tasks
 
         [Required]
         public string MaestroApiEndpoint { get; set; }
-
-        public bool PublishUsingPipelines { get; set; } = false;
 
         private bool IsStableBuild { get; set; } = false;
 
