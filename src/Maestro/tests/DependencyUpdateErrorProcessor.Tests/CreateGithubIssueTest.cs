7262448b769c9ab086a2edc1d907adfa91529ef7
Epsitha Ananth <47157394+epananth@users.noreply.github.com>
2020-04-23 16:15:21 -0700

    Tests for dependency update error processor - Part One  (#1116)
    * Tests for dependency update error processor

---
+++ src/Maestro/tests/DependencyUpdateErrorProcessor.Tests/CreateGithubIssueTest.cs
@@ -0,0 +1,150 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Maestro.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Octokit;
+using Xunit;
+
+namespace DependencyUpdateErrorProcessor.Tests
+{
+    public class CreateGithubIssueTest : DependencyUpdateErrorProcessorTests
+    {
+        private const string SubscriptionId = "00000000-0000-0000-0000-000000000001";
+        private const string RepoUrl = "https://github.test/test-org-1/test-repo-1";
+        private const string BranchOne = "BranchOne";
+        private const string BranchTwo = "BranchTwo";
+        [Theory]
+        [InlineData("https://github.test/test-org-1/test-repo-1", "BranchOne", "ProcessPendingUpdatesAsync", "no arguments" , "2200/1/1" , false)]
+        [InlineData("https://github.test/test-org-1/test-repo-13", "BranchTwo", "UpdateAssetsAsync", "[\"00000000-0000-0000-0000-000000000001\",\"UpdateAssetsAsync\"]",  "2200/1/1", false)]
+        public async Task ShouldCreateIssue(string repoUrl, string branch , string method, string arguments , DateTime errorOccurredAt, bool success)
+        {
+            RepositoryBranchUpdateHistoryEntry repositoryBranchUpdate =
+                new RepositoryBranchUpdateHistoryEntry
+                {
+                    Repository = repoUrl,
+                    Branch = branch,
+                    Method = method,
+                    Timestamp = errorOccurredAt,
+                    Arguments = arguments,
+                    Success = success
+                };
+            Repository repository = new Repository();
+            GithubClient.Setup(x => x.Repository.Get(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(repository);
+            Mock<Maestro.Data.Models.Subscription> subscription = new Mock<Maestro.Data.Models.Subscription>();
+            subscription.Object.Id = Guid.Parse(SubscriptionId);
+            Context.Subscriptions.Add(subscription.Object);
+            Context.SaveChanges();
+            Mock<Octokit.Issue> issue = new Mock<Issue>();
+            //Shared mocks works for Xunit as it creates a separate file for each test, but for Nunit there will be a conflict. 
+            //We need to take care of this if we port to Nunit in future.
+            GithubClient.Setup(x => x.Issue.Create(It.IsAny<long>(),It.IsAny<NewIssue>())).ReturnsAsync(issue.Object);
+            Context.RepoBranchUpdateInMemory = new List<RepositoryBranchUpdateHistoryEntry>
+                {repositoryBranchUpdate};
+            DependencyUpdateErrorProcessor errorProcessor =
+                ActivatorUtilities.CreateInstance<DependencyUpdateErrorProcessor>(Scope.ServiceProvider,
+                    Context);
+            await errorProcessor.ProcessDependencyUpdateErrorsAsync();
+            GithubClient.Verify(x => x.Issue.Create(It.IsAny<long>(), It.IsAny<NewIssue>()), Times.Once);
+        }
+
+        /// <summary>
+        /// No issue should be created for the method SynchronizePullRequestAsync
+        /// No issue should be created if the subscriptionGuid is invalid for the UpdateAssetsAsync method
+        /// </summary>
+        /// <param name="repoUrl"></param>
+        /// <param name="branch"></param>
+        /// <param name="method"></param>
+        /// <param name="arguments"></param>
+        /// <param name="errorOccurredAt"></param>
+        /// <param name="success"></param>
+        /// <returns>Does not create any new issue.</returns>
+        [Theory]
+        [InlineData("https://github.test/test-org-1/test-repo-1", "38", "SynchronizePullRequestAsync", "no arguments", "2200/1/1", false)]
+        [InlineData("https://github.test/test-org-1/test-repo-13", "38", "UpdateAssetsAsync", "[\"0000000\",\"UpdateAssetsAsync\"]", "2200/1/1", false)]
+        public async Task ShouldNotCreateIssue(string repoUrl, string branch, string method, string arguments, DateTime errorOccurredAt, bool success)
+        {
+            RepositoryBranchUpdateHistoryEntry repositoryBranchUpdate =
+                new RepositoryBranchUpdateHistoryEntry
+                {
+                    Repository = repoUrl,
+                    Branch = branch,
+                    Method = method,
+                    Timestamp = errorOccurredAt,
+                    Arguments = arguments,
+                    Success = success
+                };
+            Repository repository = new Repository();
+            GithubClient.Setup(x => x.Repository.Get(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(repository);
+            Mock<Octokit.Issue> issue = new Mock<Issue>();
+            GithubClient.Setup(x => x.Issue.Create(It.IsAny<long>(), It.IsAny<NewIssue>())).ReturnsAsync(issue.Object);
+            Context.RepoBranchUpdateInMemory = new List<RepositoryBranchUpdateHistoryEntry>
+                {repositoryBranchUpdate};
+            DependencyUpdateErrorProcessor errorProcessor =
+                ActivatorUtilities.CreateInstance<DependencyUpdateErrorProcessor>(Scope.ServiceProvider,
+                    Context);
+            await errorProcessor.ProcessDependencyUpdateErrorsAsync();
+            GithubClient.Verify(x => x.Issue.Create(It.IsAny<long>(), It.IsAny<NewIssue>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task CreateIssue()
+        {
+            RepositoryBranchUpdateHistoryEntry firstError =
+                new RepositoryBranchUpdateHistoryEntry
+                {
+                    Repository = RepoUrl,
+                    Branch = BranchOne,
+                    Method = "ProcessPendingUpdatesAsync",
+                    Timestamp = new DateTime(2200, 1, 1),
+                    Arguments = "[Error Message]",
+                    Success = false,
+                    ErrorMessage = "Error Message",
+                    Action = "Creating new issue"
+                };
+
+            RepositoryBranchUpdateHistoryEntry secondError =
+                new RepositoryBranchUpdateHistoryEntry
+                {
+                    Repository = RepoUrl,
+                    Branch = BranchTwo,
+                    Method = "ProcessPendingUpdatesAsync",
+                    Timestamp = new DateTime(2200, 1, 1),
+                    Arguments = "[Arguments]",
+                    Success = false,
+                    ErrorMessage = "ProcessPendingUpdatesAsync error",
+                    Action = "Create another issue"
+                };
+            Context.RepoBranchUpdateInMemory = new List<RepositoryBranchUpdateHistoryEntry>
+                {firstError , secondError};
+
+            Repository repository = new Repository();
+            GithubClient.Setup(x => x.Repository.Get(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(repository);
+            Mock<Octokit.Issue> issue = new Mock<Issue>();
+            List<NewIssue> newIssue = new List<NewIssue>();
+            GithubClient.Setup(x => x.Issue.Create(It.IsAny<long>(), Capture.In(newIssue))).ReturnsAsync(issue.Object);
+            DependencyUpdateErrorProcessor errorProcessor =
+                ActivatorUtilities.CreateInstance<DependencyUpdateErrorProcessor>(Scope.ServiceProvider,
+                    Context);
+            await errorProcessor.ProcessDependencyUpdateErrorsAsync();
+            Assert.Equal(2, newIssue.Count);
+            Assert.Equal("DependencyUpdateError", newIssue[0].Labels[0]);
+            Assert.Contains(RepoUrl, newIssue[0].Title);
+            Assert.Contains(BranchOne, newIssue[0].Body);
+            Assert.Contains("ProcessPendingUpdatesAsync", newIssue[0].Body);
+            Assert.Contains("1/1/2200 12:00:00 AM", newIssue[0].Body);
+            Assert.Contains(RepoUrl, newIssue[0].Body);
+            Assert.Equal( "DependencyUpdateError", newIssue[1].Labels[0]);
+            Assert.Contains(RepoUrl, newIssue[1].Title);
+            Assert.Contains(BranchTwo, newIssue[1].Body);
+            Assert.Contains("ProcessPendingUpdatesAsync error", newIssue[1].Body);
+            Assert.Contains("1/1/2200 12:00:00 AM", newIssue[1].Body);
+            Assert.Contains(RepoUrl, newIssue[1].Body);
+        }
+    }
+}
---
+++ src/Maestro/tests/DependencyUpdateErrorProcessor.Tests/UpdateGithubIssueTest.cs
@@ -0,0 +1,123 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Maestro.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Octokit;
+using Xunit;
+
+namespace DependencyUpdateErrorProcessor.Tests
+{
+    public class UpdateGithubIssueTest : DependencyUpdateErrorProcessorTests
+    {
+        private const string SubscriptionId = "00000000-0000-0000-0000-000000000001";
+        private const string MethodName = "UpdateAssetsAsync";
+        private const string ErrorMessage = "build: 14	Unexpected error processing action: Validation Failed";
+        private const string Branch = "BranchOne";
+        private const string RepoUrl = "https://github.test/test-org-1/test-repo-1";
+        private const int RepoId = 0;
+        private const int IssueNumber = 1;
+        [Fact]
+        public async Task CreateIssueAndUpdateIssueBody()
+        {
+            RepositoryBranchUpdateHistoryEntry firstError =
+                new RepositoryBranchUpdateHistoryEntry
+                {
+                    Repository = RepoUrl,
+                    Branch = Branch,
+                    Method = MethodName,
+                    Timestamp = new DateTime(2200, 1, 1),
+                    Arguments = $"[\"{SubscriptionId}\",\"{MethodName}\",\"{ErrorMessage}\"]",
+                    Success = false,
+                    ErrorMessage = ErrorMessage,
+                    Action = "Creating new issue"
+                };
+
+            RepositoryBranchUpdateHistoryEntry secondError =
+                new RepositoryBranchUpdateHistoryEntry
+                {
+                    Repository = RepoUrl,
+                    Branch = Branch,
+                    Method = MethodName,
+                    Timestamp = new DateTime(2200, 2, 1),
+                    Arguments = $"[\"{SubscriptionId}\",\"{MethodName}\",\"{ErrorMessage}\"]",
+                    Success = false,
+                    ErrorMessage = ErrorMessage,
+                    Action = "Updating existing issue",
+                };
+            Context.RepoBranchUpdateInMemory = new List<RepositoryBranchUpdateHistoryEntry>
+                {firstError , secondError};
+            Mock<Maestro.Data.Models.Subscription> subscription = new Mock<Maestro.Data.Models.Subscription>();
+            subscription.Object.Id = Guid.Parse(SubscriptionId);
+            subscription.Object.SourceRepository = "Source Repo";
+            subscription.Object.TargetRepository = "Target Repo";
+            Context.Subscriptions.Add(subscription.Object);
+            Context.SaveChanges();
+
+            var repository = new Repository();
+            //Shared mocks works for Xunit as it creates a separate file for each test, but for Nunit there will be a conflict. 
+            //We need to take care of this if we port to Nunit in future.
+            GithubClient.Setup(x => x.Repository.Get(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(repository);
+            Issue issueCreated = GetIssue();
+            List<NewIssue> newIssues = new List<NewIssue>();
+            GithubClient.Setup(x => x.Issue.Create(It.IsAny<long>(), Capture.In(newIssues))).ReturnsAsync(issueCreated);
+            GithubClient.Setup(x => x.Issue.Get(RepoId, IssueNumber)).ReturnsAsync(issueCreated);
+            List<IssueUpdate> issueUpdate = new List<IssueUpdate>();
+            GithubClient.Setup(x => x.Issue.Update(RepoId, IssueNumber, Capture.In(issueUpdate))).ReturnsAsync(issueCreated);
+            DependencyUpdateErrorProcessor errorProcessor =
+                ActivatorUtilities.CreateInstance<DependencyUpdateErrorProcessor>(Scope.ServiceProvider,
+                    Context);
+            await errorProcessor.ProcessDependencyUpdateErrorsAsync();
+
+            Assert.Single(newIssues);
+            Assert.Contains(RepoUrl, newIssues[0].Title);
+            Assert.Equal("DependencyUpdateError", newIssues[0].Labels[0]);
+            Assert.Contains(SubscriptionId, newIssues[0].Body);
+            Assert.Contains(MethodName, newIssues[0].Body);
+            Assert.Contains(RepoUrl, newIssues[0].Body);
+            Assert.Contains("1/1/2200 12:00:00 AM", newIssues[0].Body);
+            Assert.Contains(Branch, newIssues[0].Body);
+            Assert.Contains(
+                $"[marker]: <> (subscriptionId: '{SubscriptionId}', method: '{MethodName}', errorMessage: '{ErrorMessage}')",
+                newIssues[0].Body);
+
+        }
+
+        public Issue GetIssue()
+        {
+            Issue issue = new Issue
+            (
+                "testUrl",
+                "testHtml",
+                "testCommentUrl",
+                "testEvents",
+                1,
+                ItemState.Open,
+                "testTitle",
+                $"[marker]: <> (subscriptionId: '{SubscriptionId}', method: '{MethodName}', errorMessage: '{ErrorMessage}')",
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                1,
+                null,
+                null,
+                new DateTime(2020, 01, 01),
+                null,
+                12,
+                "test",
+                false,
+                null,
+                null
+            );
+            return issue;
+        }
+    }
+}
