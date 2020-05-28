1d92f7491ae15af339a035759506ef29ee261cc9
Missy Messa <mjanecke@microsoft.com>
2020-05-28 09:10:27 -0700

    clean up merge conflicts

--- src/DotNet.Status.Web/ArcadeValidationData.cs
+++ src/DotNet.Status.Web/ArcadeValidationData.cs
@@ -1,24 +1,10 @@
 using System;
-using System.Collections.Generic;
-<<<<<<< HEAD
-<<<<<<< HEAD
 using System.ComponentModel.DataAnnotations;
-=======
->>>>>>> Initial commit for new API and test project
-=======
-using System.ComponentModel.DataAnnotations;
->>>>>>> Addressing minor code review feedback
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DotNet.Status.Web
 {
     public class ArcadeValidationData
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Addressing minor code review feedback
         [Required]
         public DateTime BuildDateTime { get; set; }
         [Required]
@@ -36,19 +22,6 @@ namespace DotNet.Status.Web
         [Required]
         public string ProductRepoBuildResult { get; set; }
         [Required]
-<<<<<<< HEAD
-=======
-        public DateTime BuildDateTime { get; set; }
-        public string ArcadeVersion { get; set; }
-        public int BARBuildID { get; set; }
-        public string ArcadeBuildLink { get; set; }
-        public string ArcadeValidationBuildLink { get; set; }
-        public string ProductRepoName { get; set; }
-        public string ProductRepoBuildLink { get; set; }
-        public string ProductRepoBuildResult { get; set; }
->>>>>>> Initial commit for new API and test project
-=======
->>>>>>> Addressing minor code review feedback
         public string ArcadeDiffLink { get; set; }
     }
 }
--- src/DotNet.Status.Web/Controllers/TelemetryController.cs
+++ src/DotNet.Status.Web/Controllers/TelemetryController.cs
@@ -16,8 +16,6 @@ namespace DotNet.Status.Web.Controllers
     {
         private readonly ILogger<TelemetryController> _logger;
         private readonly IOptionsSnapshot<TelemetryOptions> _options;
-<<<<<<< HEAD
-<<<<<<< HEAD
         private readonly IKustoIngestClient _client;
 
         public TelemetryController(
@@ -31,7 +29,6 @@ namespace DotNet.Status.Web.Controllers
         }
 
         [HttpPost("collect/arcade-validation")]
-<<<<<<< HEAD
         public async Task<IActionResult> CollectArcadeValidation([Required] ArcadeValidationData data)
         {
             TelemetryOptions options = _options.Value;
@@ -43,41 +40,6 @@ namespace DotNet.Status.Web.Controllers
             }
 
             IKustoIngestClient ingest = _client ?? KustoIngestFactory.CreateQueuedIngestClient(options.KustoIngestConnectionString);
-=======
-=======
-        private readonly IKustoIngestClient _client;
->>>>>>> Adding API for collecting telemetry from Arcade Validation runs; test project for DotNet.Status.Web
-
-        public TelemetryController(
-            ILogger<TelemetryController> logger,
-            IOptionsSnapshot<TelemetryOptions> options,
-            IKustoIngestClient client = null)
-        {
-            _logger = logger;
-            _options = options;
-            _client = client;
-        }
-
-        [HttpPost("collect/ArcadeValidation")]
-=======
->>>>>>> Addressing minor code review feedback
-        public async Task<IActionResult> CollectArcadeValidation([Required] ArcadeValidationData data)
-        {
-            TelemetryOptions options = _options.Value;
-
-            if (_client == null && string.IsNullOrEmpty(options.KustoIngestConnectionString))
-            {
-                _logger.LogError("No KustoIngestConnectionString set");
-                return StatusCode(500);
-            }
-
-<<<<<<< HEAD
-            IKustoIngestClient ingest =
-                KustoIngestFactory.CreateQueuedIngestClient(options.KustoIngestConnectionString);
->>>>>>> Initial commit for new API and test project
-=======
-            IKustoIngestClient ingest = _client ?? KustoIngestFactory.CreateQueuedIngestClient(options.KustoIngestConnectionString);
->>>>>>> Adding API for collecting telemetry from Arcade Validation runs; test project for DotNet.Status.Web
 
             List<ArcadeValidationData> arcadeValidationDatas = new List<ArcadeValidationData>{ data };
 
@@ -100,20 +62,8 @@ namespace DotNet.Status.Web.Controllers
                     new KustoValue("ArcadeDiffLink", b.ArcadeDiffLink, KustoDataTypes.String)
                 });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
             return Ok();
-=======
-            return NoContent();
->>>>>>> Initial commit for new API and test project
-=======
-            _logger.LogInformation("End Collect Arcade Validation data");
 
-=======
->>>>>>> Addressing minor code review feedback
-            return Ok();
->>>>>>> Adding API for collecting telemetry from Arcade Validation runs; test project for DotNet.Status.Web
         }
     }
 }
