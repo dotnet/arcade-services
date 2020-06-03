60947b504b79ef6eb0a73e739bacb62bed6efcbe
Alex Perovich <alperovi@microsoft.com>
2020-06-03 15:57:46 -0700

    Fix auth bug too

--- src/Maestro/Maestro.Web/Startup.Auth.cs
+++ src/Maestro/Maestro.Web/Startup.Auth.cs
@@ -44,9 +44,7 @@ namespace Maestro.Web
             
             services.AddAuthentication(options =>
                     {
-                        // The "AddIdentity" above messed with these, so we need to re-mess with them.
-                        options.DefaultChallengeScheme = options.DefaultAuthenticateScheme =
-                            options.DefaultSignInScheme = options.DefaultScheme = "Contextual";
+                        options.DefaultScheme = "Contextual";
                     })
                 .AddPolicyScheme("Contextual","Contextual",
                     policyOptions => { policyOptions.ForwardDefaultSelector = ctx => ctx.Request.Path.StartsWithSegments("/api") ? PersonalAccessTokenDefaults.AuthenticationScheme : IdentityConstants.ApplicationScheme; })
@@ -137,6 +135,8 @@ namespace Maestro.Web
                 {
                     options.ExpireTimeSpan = LoginCookieLifetime;
                     options.SlidingExpiration = true;
+                    options.ReturnUrlParameter = "returnUrl";
+                    options.LoginPath = "/Account/SignIn";
                     options.Events = new CookieAuthenticationEvents
                     {
                         OnSigningIn = async ctx =>
--- src/Shared/Microsoft.DotNet.Web.Authentication/GitHub/GitHubAuthenticationOptions.cs
+++ src/Shared/Microsoft.DotNet.Web.Authentication/GitHub/GitHubAuthenticationOptions.cs
@@ -2,10 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using Microsoft.AspNetCore.Authentication.OAuth;
-using Microsoft.DotNet.GitHub.Authentication;
-using Octokit;
 
 namespace Microsoft.DotNet.Web.Authentication.GitHub
 {
