8fdb8f7fb2a661feaf1476f70ba53640d3858789
Missy Messa <47990216+missymessa@users.noreply.github.com>
2020-06-04 14:05:13 -0700

    Telemetry API for Arcade Validation and test project for DotNet.Status.Web (#1217)
    * Initial commit for new API and test project
    
    * handling merge conflicts for test project
    
    * Adding API for collecting telemetry from Arcade Validation runs; test project for DotNet.Status.Web
    
    * Addressing minor code review feedback
    
    * Initial commit for new API and test project
    
    * handling merge conflicts for test project
    
    * Adding API for collecting telemetry from Arcade Validation runs; test project for DotNet.Status.Web
    
    * Addressing minor code review feedback
    
    * removed extra test project
    
    * clean up merge conflicts
    
    * Refactoring to address code review feedback
    
    * A little bit more refactoring
    
    * Removed some elements that weren't necessary
    
    * Forgot a couple elements to remove from the project file
    
    * derp, forgot an await
    
    * removed the async from the SetUp method
    
    * Added code coverage reference to test project
    
    * Need to publish symbols for code coverage
    
    * A little bit more refactoring

---
+++ src/DotNet.Status.Web.Tests/TelemetryControllerTests.cs
@@ -0,0 +1,84 @@
+using Xunit;
+using DotNet.Status.Web.Controllers;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using Kusto.Ingest;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit.Abstractions;
+using System;
+
+namespace DotNet.Status.Web.Tests
+{
+    public class TelemetryControllerTests : IDisposable
+    {
+        private readonly ITestOutputHelper _output;
+        private TelemetryController _controller;
+        private ServiceProvider _services;
+
+        public TelemetryControllerTests(ITestOutputHelper output)
+        {
+            _output = output;            
+        }
+
+        protected void SetUp(KustoOptions customOptions = null)
+        {
+            var collection = new ServiceCollection();
+            collection.AddOptions();
+            collection.AddLogging(l =>
+            {
+                l.AddProvider(new XUnitLogger(_output));
+            });
+
+            collection.Configure<KustoOptions>(options =>
+            {
+                options.IngestConnectionString = customOptions != null ? customOptions.IngestConnectionString : "fakekustoconnectionstring";
+                options.Database = customOptions != null ? customOptions.Database : "fakekustodatbase";
+            });
+
+            collection.AddScoped<TelemetryController>();
+
+            var kustoIngestClientMock = new Mock<IKustoIngestClient>();
+            kustoIngestClientMock.Setup(x => x.IngestFromStreamAsync(It.IsAny<System.IO.Stream>(), It.IsAny<KustoIngestionProperties>(), null))
+                .Returns(Task.FromResult(Mock.Of<IKustoIngestionResult>()));
+
+            collection.AddSingleton(kustoIngestClientMock.Object);
+
+            _services = collection.BuildServiceProvider();
+            _controller = _services.GetRequiredService<TelemetryController>();
+        }
+
+        public void Dispose()
+        {
+            _services.Dispose();
+        }
+
+        [Fact]
+        public async void TestArcadeValidationTelemetryCollection()
+        {
+            SetUp();
+            var result = await _controller.CollectArcadeValidation(new ArcadeValidationData
+            {
+                BuildDateTime = new DateTime(),
+                ArcadeVersion = "fakearcadeversion",
+                BARBuildID = -1,
+                ArcadeBuildLink = "fakearcadebuildlink", 
+                ArcadeValidationBuildLink = "fakearcadevalidationbuildlink",
+                ProductRepoName = "fakeproductreponame",
+                ProductRepoBuildLink = "fakeproductrepobuildlink",
+                ProductRepoBuildResult = "fakeproductrepobuildresult",
+                ArcadeDiffLink = "fakearcadedifflink"
+            });
+            Assert.NotNull(result);
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public async void TestArcadeValidationTelemetryCollectionWithMissingKustoConnectionString()
+        {
+            SetUp(new KustoOptions());
+            await Assert.ThrowsAsync<InvalidOperationException>(() => _controller.CollectArcadeValidation(new ArcadeValidationData()));
+        }
+    }
+}
---
+++ src/DotNet.Status.Web/Controllers/TelemetryController.cs
@@ -0,0 +1,67 @@
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Microsoft.DotNet.Kusto;
+using Kusto.Ingest;
+using System.Collections.Generic;
+using System;
+
+namespace DotNet.Status.Web.Controllers
+{
+    [Route("api/telemetry")]
+    [ApiController]
+    public class TelemetryController : ControllerBase
+    {
+        private readonly ILogger<TelemetryController> _logger;
+        private readonly IOptionsSnapshot<KustoOptions> _options;
+        private readonly IKustoIngestClient _client;
+
+        public TelemetryController(
+            ILogger<TelemetryController> logger,
+            IOptionsSnapshot<KustoOptions> options,
+            IKustoIngestClient client)
+        {
+            _logger = logger;
+            _options = options;
+            _client = client;
+        }
+
+        [HttpPost("collect/arcade-validation")]
+        public async Task<IActionResult> CollectArcadeValidation([Required] ArcadeValidationData data)
+        {
+            KustoOptions options = _options.Value;
+
+            if (string.IsNullOrEmpty(options.IngestConnectionString))
+            {
+                throw new InvalidOperationException("No IngestConnectionString set");
+            }
+
+            List<ArcadeValidationData> arcadeValidationDatas = new List<ArcadeValidationData>{ data };
+
+            await KustoHelpers.WriteDataToKustoInMemoryAsync(
+                _client,
+                options.Database,
+                "ArcadeValidation",
+                _logger,
+                arcadeValidationDatas,
+                b => new[]
+                {
+                    new KustoValue("BuildDateTime", b.BuildDateTime.ToString(), KustoDataTypes.DateTime),
+                    new KustoValue("ArcadeVersion", b.ArcadeVersion, KustoDataTypes.String),
+                    new KustoValue("BARBuildID", b.BARBuildID.ToString(), KustoDataTypes.Int),
+                    new KustoValue("ArcadeBuildLink", b.ArcadeBuildLink, KustoDataTypes.String),
+                    new KustoValue("ArcadeValidationBuildLink", b.ArcadeValidationBuildLink, KustoDataTypes.String),
+                    new KustoValue("ProductRepoName", b.ProductRepoName, KustoDataTypes.String),
+                    new KustoValue("ProductRepoBuildLink", b.ProductRepoBuildLink, KustoDataTypes.String),
+                    new KustoValue("ProductRepoBuildResult", b.ProductRepoBuildResult, KustoDataTypes.String),
+                    new KustoValue("ArcadeDiffLink", b.ArcadeDiffLink, KustoDataTypes.String)
+                });
+
+            return Ok();
+
+        }
+    }
+}
