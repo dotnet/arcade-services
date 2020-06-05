c0f115b9f9d990a9ea5dba21768abfb3e28f7c8e
Ricardo Arenas <riarenas@microsoft.com>
2020-06-05 11:07:51 -0700

    Revert "Fix failure with users in more than 30 orgs (#1229)" (#1239)
    This reverts commit 7dc3fdb3baf632a9831c4c3b081e3055b29a0712.
    
    This is causing github to return a sign in page with some error messages when making requests. See dotnet/core-eng#10023 (comment)

--- src/Shared/Microsoft.DotNet.GitHub.Authentication/GitHubClientFactory.cs
+++ src/Shared/Microsoft.DotNet.GitHub.Authentication/GitHubClientFactory.cs
@@ -1,3 +1,9 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading.Tasks;
+using Microsoft.DotNet.GitHub.Authentication;
 using Microsoft.Extensions.Options;
 using Octokit;
 
@@ -5,20 +11,20 @@ namespace Microsoft.DotNet.GitHub.Authentication
 {
     public class GitHubClientFactory : IGitHubClientFactory
     {
-        private readonly IOptionsMonitor<GitHubClientOptions> _githubClientOptions;
+        private readonly IOptions<GitHubClientOptions> _githubClientOptions;
+        private readonly IGitHubTokenProvider _tokenProvider;
 
-        public GitHubClientFactory(IOptionsMonitor<GitHubClientOptions> githubClientOptions)
+        public GitHubClientFactory(IOptions<GitHubClientOptions> githubClientOptions, IGitHubTokenProvider tokenProvider)
         {
             _githubClientOptions = githubClientOptions;
+            _tokenProvider = tokenProvider;
         }
 
-        public GitHubClientOptions Options => _githubClientOptions.CurrentValue; 
-
-        public IGitHubClient CreateGitHubClient(string token)
+        public async Task<IGitHubClient> CreateGitHubClientAsync(string owner, string repo)
         {
-            return new GitHubClient(Options.ProductHeader)
+            return new GitHubClient(_githubClientOptions.Value.ProductHeader)
             {
-                Credentials = new Credentials(token),
+                Credentials = new Credentials(await _tokenProvider.GetTokenForRepository(owner, repo))
             };
         }
     }
--- src/Shared/Microsoft.DotNet.Web.Authentication/GitHub/GitHubAuthenticationHandler.cs
+++ src/Shared/Microsoft.DotNet.Web.Authentication/GitHub/GitHubAuthenticationHandler.cs
@@ -5,12 +5,13 @@
 using System.Collections.Generic;
 using System.Security.Claims;
 using System.Text.Encodings.Web;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Octokit;
+using Newtonsoft.Json.Linq;
 
 namespace Microsoft.DotNet.Web.Authentication.GitHub
 {
@@ -34,8 +35,13 @@ namespace Microsoft.DotNet.Web.Authentication.GitHub
             OAuthTokenResponse tokens)
         {
             string accessToken = tokens.AccessToken;
-            (IEnumerable<Claim> claims, User user) = await _claimResolver.GetUserInformation(accessToken, Context.RequestAborted);
+            (IEnumerable<Claim> claims, JObject user) = await _claimResolver.GetUserInformation(accessToken, Context.RequestAborted);
             identity.AddClaims(claims);
+            JsonElement rootElement;
+            using (JsonDocument jsonDocument = JsonDocument.Parse(user.ToString()))
+            {
+                rootElement = jsonDocument.RootElement.Clone();
+            }
 
             var context = new OAuthCreatingTicketContext(
                 new ClaimsPrincipal(identity),
@@ -45,7 +51,7 @@ namespace Microsoft.DotNet.Web.Authentication.GitHub
                 Options,
                 Backchannel,
                 tokens,
-                default);
+                rootElement);
             await Options.Events.CreatingTicket(context);
             return new AuthenticationTicket(context.Principal, context.Properties, context.Scheme.Name);
         }
