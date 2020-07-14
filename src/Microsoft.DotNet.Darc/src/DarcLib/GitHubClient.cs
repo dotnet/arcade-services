61e6acf551b111152525d7f1328af52c8cb1916a
Epsitha Ananth <47157394+epananth@users.noreply.github.com>
2020-07-14 12:47:34 -0700

    Maestro++ better squashing (#1256)
    * condense commit message

--- src/Microsoft.DotNet.Darc/src/DarcLib/GitHubClient.cs
+++ src/Microsoft.DotNet.Darc/src/DarcLib/GitHubClient.cs
@@ -352,22 +352,43 @@ namespace Microsoft.DotNet.DarcLib
         }
 
         /// <summary>
-        ///     Merge a pull request
+        /// Gets all the commits related to the pull request
+        /// </summary>
+        /// <param name="pullRequestUrl"></param>
+        /// <returns>All the commits related to the pull request</returns>
+        public async Task<IList<Commit>> GetPullRequestCommitsAsync(string pullRequestUrl)
+        {
+            (string owner, string repo, int id) = ParsePullRequestUri(pullRequestUrl);
+            var pullRequestCommits = await Client.PullRequest.Commits(owner, repo, id);
+            IList<Commit> commits = new List<Commit>(pullRequestCommits.Count);
+            foreach (var commit in pullRequestCommits)
+            {
+                commits.Add(new Commit(commit.Commit.Author.Name,
+                    commit.Sha,
+                    commit.Commit.Message));
+            }
+            return commits;
+        }
+
+        /// <summary>
+        ///     Merge a dependency update pull request
         /// </summary>
         /// <param name="pullRequestUrl">Uri of pull request to merge</param>
         /// <param name="parameters">Settings for merge</param>
+        /// <param name="mergeCommitMessage">Commit message used to merge the pull request</param>
         /// <returns></returns>
-        public async Task MergePullRequestAsync(string pullRequestUrl, MergePullRequestParameters parameters)
+        public async Task MergeDependencyPullRequestAsync(string pullRequestUrl, MergePullRequestParameters parameters, string mergeCommitMessage)
         {
             (string owner, string repo, int id) = ParsePullRequestUri(pullRequestUrl);
-
+            Octokit.PullRequest pr = await Client.PullRequest.Get(owner, repo, id);
+            
             var mergePullRequest = new MergePullRequest
             {
+                CommitMessage = mergeCommitMessage,
                 Sha = parameters.CommitToMerge,
                 MergeMethod = parameters.SquashMerge ? PullRequestMergeMethod.Squash : PullRequestMergeMethod.Merge
             };
 
-            Octokit.PullRequest pr = await Client.PullRequest.Get(owner, repo, id);
             await Client.PullRequest.Merge(owner, repo, id, mergePullRequest);
 
             if (parameters.DeleteSourceBranch)
