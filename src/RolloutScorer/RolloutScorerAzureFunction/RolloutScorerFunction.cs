86ab646eb7ad520629ac4b615f25f5440d0caf72
Jon Fortescue <jofortes@microsoft.com>
2020-07-17 10:57:40 -0700

    Fix Rollout Scorer azure function not picking up issues (#1286)

--- src/RolloutScorer/RolloutScorerAzureFunction/RolloutScorerFunction.cs
+++ src/RolloutScorer/RolloutScorerAzureFunction/RolloutScorerFunction.cs
@@ -73,7 +73,7 @@ namespace RolloutScorerAzureFunction
                         RolloutScorer.RolloutScorer rolloutScorer = new RolloutScorer.RolloutScorer
                         {
                             Repo = deploymentGroup.Key,
-                            RolloutStartDate = deploymentGroup.First().Started.GetValueOrDefault().Date,
+                            RolloutStartDate = deploymentGroup.First().Started.GetValueOrDefault(),
                             RolloutWeightConfig = Configs.DefaultConfig.RolloutWeightConfig,
                             GithubConfig = Configs.DefaultConfig.GithubConfig,
                             Log = log,
