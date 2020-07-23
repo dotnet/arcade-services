c679c765a84d2190bba083eabc1671e29b48285a
meganaquinn <megan.quinn@microsoft.com>
2020-07-23 00:07:47 -0700

    Upate ignore message

--- src/Maestro/tests/Maestro.ScenarioTests/ScenarioTests_GitHubFlow.cs
+++ src/Maestro/tests/Maestro.ScenarioTests/ScenarioTests_GitHubFlow.cs
@@ -65,7 +65,7 @@ namespace Maestro.ScenarioTests
         }
 
         [Test]
-        [Ignore("Currently returns a 403, fixes in progress")]
+        [Ignore("Currently returns an error, investigation in progress")]
         public async Task Darc_GitHubFlow_NonBatched_WithCoherency()
         {
             TestContext.WriteLine("GitHub Dependency Flow, non-batched");
