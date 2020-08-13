5f787866081000c93278d8e5b98fb7023696ae66
Chad Nedzlek <ChadNedzlek@users.noreply.github.com>
2020-08-12 19:02:35 -0700

    Enable dump collection in cluster (#1347)

--- src/Microsoft.DncEng.DeployServiceFabricCluster/scripts/startup.ps1
+++ src/Microsoft.DncEng.DeployServiceFabricCluster/scripts/startup.ps1
@@ -8,3 +8,5 @@ Start-Transcript -Path "$PSScriptRoot\transcript.log"
 setx APPLICATION_INSIGHTS_KEY "$AppInsightsKey"  /M
 
 .\Set-TlsConfiguration.ps1
+
+reg.exe IMPORT "$PSScriptRoot\enable-dumps.reg"
