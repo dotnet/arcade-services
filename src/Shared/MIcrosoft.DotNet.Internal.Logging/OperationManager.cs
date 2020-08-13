07d2d0833489be1c838ecc669583a1ce6c301484
Chad Nedzlek <chadnedz@microsoft.com>
2020-08-12 18:29:40 -0700

    Offer a lighter weight LogginScope
    There are a few places that want all the logging goodness,
    but can't afford to create and dispose entire ServiceProviders,
    (like the Sql processor, which processes millions of messages).
    
    So offer a new method that doesn't do the service scope stuff.

--- src/Shared/MIcrosoft.DotNet.Internal.Logging/OperationManager.cs
+++ src/Shared/MIcrosoft.DotNet.Internal.Logging/OperationManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -8,19 +9,29 @@ namespace Microsoft.DotNet.Internal.Logging
     public class OperationManager
     {
         private readonly IOptions<OperationManagerOptions> _options;
+        private readonly IServiceProvider _serviceProvider;
         private readonly IServiceScopeFactory _scope;
         private readonly ILogger<OperationManager> _logger;
 
         public OperationManager(
             IOptions<OperationManagerOptions> options,
+            IServiceProvider serviceProvider,
             IServiceScopeFactory scope,
             ILogger<OperationManager> logger = null)
         {
             _options = options;
+            _serviceProvider = serviceProvider;
             _scope = scope;
             _logger = logger;
         }
 
+        /// <summary>
+        /// Bing an operation.  This will create a logging scope (including setting Activity.Id)
+        /// as well as create a new scoped IServiceProvider (available on the return value <see cref="Operation.ServiceProvider"/>)
+        /// </summary>
+        /// <param name="name">Logging format string for scope. <example><code>Processing message {messageId}</code></example></param>
+        /// <param name="args">Optional parameters to format the logging message. <example>message.Id will set the {messageId} in the name example</example></param>
+        /// <returns>A new operation that exposes the <see cref="IServiceProvider"/>, and, when disposed, will end the operation and all resulting scopes</returns>
         public Operation BeginOperation(string name, params object[] args)
         {
             string formatted = FormattableStringFormatter.Format(name, args);
@@ -33,6 +44,30 @@ namespace Microsoft.DotNet.Internal.Logging
             }
 
             o.TrackDisposable(scope);
+
+            if (_options.Value.ShouldCreateLoggingScope && _logger != null)
+            {
+                o.TrackDisposable(_logger.BeginScope(name, args));
+            }
+
+            return o;
+        }
+        
+        /// <summary>
+        /// The same as <see cref="BeginOperation"/> except this does not create a new ServiceProvider scope.
+        /// In general, BeginOperation should be preferred, to ensure proper cleanup of scoped dependencies,
+        /// but for tight loops where performance is a concern, this is a lighter weight alternative.
+        /// </summary>
+        /// <seealso cref="BeginOperation"/>
+        public Operation BeginLoggingScope(string name, params object[] args)
+        {
+            string formatted = FormattableStringFormatter.Format(name, args);
+            var o = new Operation(_serviceProvider);
+            if (_options.Value.ShouldStartActivity)
+            {
+                var activity = new Activity(formatted);
+                o.TrackActivity(activity.Start());
+            }
 
             if (_options.Value.ShouldCreateLoggingScope && _logger != null)
             {
