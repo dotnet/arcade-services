6ab749cb6812b98213200f6319b791d42c4f0cb2
Alex Perovich <alperovi@microsoft.com>
2020-10-02 13:40:43 -0700

    Add comment

--- src/Microsoft.DotNet.ServiceFabric.ServiceHost/LoadReporting/IServiceLoadReporter.cs
+++ src/Microsoft.DotNet.ServiceFabric.ServiceHost/LoadReporting/IServiceLoadReporter.cs
@@ -1,7 +1,15 @@
 namespace Microsoft.DotNet.ServiceFabric.ServiceHost
 {
+    /// <summary>
+    ///   This type enables reporting load metrics to service fabric
+    /// </summary>
     public interface IServiceLoadReporter
     {
+        /// <summary>
+        ///   Reports a service fabric load metric.
+        /// </summary>
+        /// <param name="name">The load metric name</param>
+        /// <param name="value">The load metric value</param>
         void ReportLoad(string name, int value);
     }
 }
