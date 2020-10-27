a8bc94277ee36a287710332b7ab2280ece8416a7
Epsitha Ananth <epananth@microsoft.com>
2020-10-27 10:13:06 -0700

    replacing /

--- src/Maestro/Microsoft.DotNet.Maestro.Tasks/src/PushMetadataToBuildAssetRegistry.cs
+++ src/Maestro/Microsoft.DotNet.Maestro.Tasks/src/PushMetadataToBuildAssetRegistry.cs
@@ -615,7 +615,7 @@ namespace Microsoft.DotNet.Maestro.Tasks
         /// <returns>An AssetData with data about the merge manifest</returns>
         private AssetData GetManifestAsAsset(IImmutableList<AssetData> assets, string location, string manifestFileName)
         {
-            string repoName = GetAzDevRepositoryName();
+            string repoName = GetAzDevRepositoryName().Replace('/','-'); 
 
             if (string.IsNullOrEmpty(AssetVersion))
             {
