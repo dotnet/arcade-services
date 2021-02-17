7d093ca761be06e96318aa4e00102d93e4cba219
Lukasz Patalas <lupatala@microsoft.com>
2021-02-17 16:39:40 +0100

    Add some more logging and comments

--- src/Monitoring/ComponentGovernance/generate-cgmanifest.ps1
+++ src/Monitoring/ComponentGovernance/generate-cgmanifest.ps1
@@ -1,5 +1,24 @@
+<#
+.SYNOPSIS
+    Generates cgmanifest.json for Grafana
+
+.DESCRIPTION
+    Grafana is not automatically detected by component governance scanner.
+    Because of that we need to register it manually using cgmanifest.json file.
+    This script dynamically generates cgmanifest.json based on the Grafana version
+    we use to ensure that the version is always up to date in component governance.
+
+.LINK
+    https://docs.opensource.microsoft.com/tools/cg/cgmanifest.html
+#>
+
+$ErrorActionPreference = 'Stop'
+
 $monitoringRootPath = Split-Path $PSScriptRoot
 $version = Get-Content "$monitoringRootPath\grafana-init\grafana-version.txt"
+
+Write-Host "Found Grafana version '$version'"
+
 $cgmanifestContent = @"
 {
     "Registrations": [
@@ -17,4 +36,7 @@ $cgmanifestContent = @"
   }
 "@
 
-$cgmanifestContent | Set-Content "$PSScriptRoot\cgmanifest.json" 
+$cgmanifestPath = "$PSScriptRoot\cgmanifest.json" 
+
+Write-Host "Writing cgmanifest to file '$cgmanifestPath'"
+$cgmanifestContent | Set-Content $cgmanifestPath
