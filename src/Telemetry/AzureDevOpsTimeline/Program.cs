01c6a8c25d98e45869f88c0152a7d35d22fe47f3
Michael Stuckey <Michael.Stuckey@microsoft.com>
2021-04-20 13:18:02 -0700

    Add license header, remove and sort usings

--- src/Telemetry/AzureDevOpsTimeline/Program.cs
+++ src/Telemetry/AzureDevOpsTimeline/Program.cs
@@ -2,15 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using Microsoft.DncEng.Configuration.Extensions;
 using Microsoft.DotNet.Internal.AzureDevOps;
-using Microsoft.DotNet.Internal.DependencyInjection;
 using Microsoft.DotNet.ServiceFabric.ServiceHost;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Logging;
 
 namespace Microsoft.DotNet.AzureDevOpsTimeline
 {
