8fa1c1f0ee66b5165618a5dce9ceda5225e2904a
Alitzel Mendez Bustillo <almend@microsoft.com>
2021-05-10 16:40:35 -0700

    remove extra lines changes

--- src/Maestro/SubscriptionActorService/PullRequestActor.cs
+++ src/Maestro/SubscriptionActorService/PullRequestActor.cs
@@ -375,7 +375,8 @@ namespace SubscriptionActorService
                         Logger.LogError($"Unknown pull request synchronization result {result}");
                         break;
                 }
-            }
+            } 
+            
             await Reminders.TryUnregisterReminderAsync(PullRequestCheck);
             return (null, false);
         }
