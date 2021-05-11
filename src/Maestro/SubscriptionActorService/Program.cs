23910ce4de476017fc09f895d179e309a02698d2
Matt Galbraith <mattgal@microsoft.com>
2021-05-11 15:42:01 -0700

    Fix dependency registration (caught by registration tests)

--- src/Maestro/SubscriptionActorService/Program.cs
+++ src/Maestro/SubscriptionActorService/Program.cs
@@ -43,7 +43,7 @@ namespace SubscriptionActorService
             services.AddSingleton<TemporaryFiles>();
             services.AddGitHubTokenProvider();
             services.AddAzureDevOpsTokenProvider();
-            services.AddSingleton<IPullRequestPolicyFailureNotifier, PullRequestPolicyFailureNotifier>();
+            services.AddScoped<IPullRequestPolicyFailureNotifier, PullRequestPolicyFailureNotifier>();
             // We do not use AddMemoryCache here. We use our own cache because we wish to
             // use a sized cache and some components, such as EFCore, do not implement their caching
             // in such a way that will work with sizing.
