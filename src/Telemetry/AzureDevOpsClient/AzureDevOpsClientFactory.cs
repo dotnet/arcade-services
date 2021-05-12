9d41c47c32fce50f2d1b662f43281cfa46d8b666
Michelle McDaniel <michelm@microsoft.com>
2021-05-12 10:09:54 -0700

    Add testing for AzurePipelinesController

---
+++ src/Telemetry/AzureDevOpsClient/AzureDevOpsClientFactory.cs
@@ -0,0 +1,18 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading.Tasks;
+
+namespace Microsoft.DotNet.Internal.AzureDevOps
+{
+    public sealed class AzureDevOpsClientFactory : IAzureDevOpsClientFactory
+    {
+        public AzureDevOpsClientFactory() { }
+
+        public IAzureDevOpsClient CreateAzureDevOpsClient(string baseUrl, string organization, int maxParallelRequests, string accessToken)
+        {
+            return new AzureDevOpsClient(baseUrl, organization, maxParallelRequests, accessToken);
+        }
+    }
+}
