707ac1937944775deb7ccb00ef08968846594bd5
Michelle McDaniel <michelm@microsoft.com>
2021-07-12 12:42:53 -0700

    Capitalization is importabt

--- src/Shared/Microsoft.DotNet.GitHub.Authentication/IGitHubApplicationClientFactory.cs
+++ src/Shared/Microsoft.DotNet.GitHub.Authentication/IGitHubApplicationClientFactory.cs
@@ -10,6 +10,6 @@ namespace Microsoft.DotNet.GitHub.Authentication
     public interface IGitHubApplicationClientFactory
     { 
         Task<IGitHubClient> CreateGitHubClientAsync(string owner, string repo);
-        IGitHubClient CreateGithubAppClient();
+        IGitHubClient CreateGitHubAppClient();
     }
 }
