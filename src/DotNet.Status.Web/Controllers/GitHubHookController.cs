2842ed9ce06bc027444ad0b3cc767daa3836b75f
Michelle McDaniel <michelm@microsoft.com>
2022-04-18 10:20:55 -0700

    repond to feedback

--- src/DotNet.Status.Web/Controllers/GitHubHookController.cs
+++ src/DotNet.Status.Web/Controllers/GitHubHookController.cs
@@ -198,8 +198,7 @@ namespace DotNet.Status.Web.Controllers
                         date);
                     break;
                 case "edited":
-                    string fromBody = issueEvent.Changes.Body == null ? null : issueEvent.Changes.Body.From;
-                    await _teamMentionForwarder.HandleMentions(repo, fromBody, issueEvent.Issue.Body, title, uri, username, date);
+                    await _teamMentionForwarder.HandleMentions(repo, issueEvent.Changes.Body?.From, issueEvent.Issue.Body, title, uri, username, date);
                     break;
             }
         }
