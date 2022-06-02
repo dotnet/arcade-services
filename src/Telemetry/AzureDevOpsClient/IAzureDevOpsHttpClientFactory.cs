19000d94b1361b7f65bc8d945448c1b5ccb9b2a3
dkurepa <dkurepa@microsoft.com>
2022-06-02 15:19:23 +0200

    Make IAzureDevOpsHttpClientFactory interface, add licences to new files

--- src/Telemetry/AzureDevOpsClient/IAzureDevOpsHttpClientFactory.cs
+++ src/Telemetry/AzureDevOpsClient/IAzureDevOpsHttpClientFactory.cs
@@ -2,13 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Net.Http;
+
 namespace Microsoft.DotNet.Internal.AzureDevOps
 {
-    public class AzureDevOpsClientOptions
-    {
-        public string BaseUrl { get; set; }
-        public string Organization { get; set; }
-        public int MaxParallelRequests { get; set; }
-        public string AccessToken { get; set; }
-    }
+    public interface IAzureDevOpsHttpClientFactory : IHttpClientFactory { };
 }
