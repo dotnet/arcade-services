51c7cac27ff8d610ef9e7909baed32c3568c607f
Michael Stuckey <Michael.Stuckey@microsoft.com>
2022-06-13 13:30:04 -0700

    Mark false positive

--- src/Shared/Microsoft.DotNet.GitHub.Authentication/GitHubAppTokenProvider.cs
+++ src/Shared/Microsoft.DotNet.GitHub.Authentication/GitHubAppTokenProvider.cs
@@ -44,7 +44,7 @@ namespace Microsoft.DotNet.GitHub.Authentication
             {
                 SetDefaultTimesOnTokenCreation = false
             };
-            using var rsa = RSA.Create(4096);
+            using var rsa = RSA.Create(4096); // lgtm [cs/cryptography/default-rsa-key-construction] False positive. This does not use the default constructor.
             rsa.ImportFromPem(privateKey);
             var rsaSecurityKey = new RsaSecurityKey(rsa)
             {
