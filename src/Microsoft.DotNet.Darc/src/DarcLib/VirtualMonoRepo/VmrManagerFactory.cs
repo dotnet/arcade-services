94318762d8fab904cf6417f554846672d6e639af
Premek Vysoky <premek.vysoky@microsoft.com>
2022-08-23 15:45:28 +0200

    Add more factory methods

--- src/Microsoft.DotNet.Darc/src/DarcLib/VirtualMonoRepo/VmrManagerFactory.cs
+++ src/Microsoft.DotNet.Darc/src/DarcLib/VirtualMonoRepo/VmrManagerFactory.cs
@@ -12,7 +12,9 @@ namespace Microsoft.DotNet.DarcLib.VirtualMonoRepo;
 public interface IVmrManagerFactory
 {
     Task<IVmrInitializer> CreateVmrInitializer();
+    Task<IVmrInitializer> CreateVmrInitializer(string vmrPath, string tmpPath);
     Task<IVmrUpdater> CreateVmrUpdater();
+    Task<IVmrUpdater> CreateVmrUpdater(string vmrPath, string tmpPath);
 }
 
 public class VmrManagerFactory : IVmrManagerFactory
@@ -31,13 +33,27 @@ public class VmrManagerFactory : IVmrManagerFactory
         _configuration = configuration;
     }
 
-    public Task<IVmrInitializer> CreateVmrInitializer() => CreateVmrManager<IVmrInitializer, VmrInitializer>();
+    public Task<IVmrInitializer> CreateVmrInitializer()
+        => CreateVmrManager<IVmrInitializer, VmrInitializer>();
 
-    public Task<IVmrUpdater> CreateVmrUpdater() => CreateVmrManager<IVmrUpdater, VmrUpdater>();
+    public Task<IVmrInitializer> CreateVmrInitializer(string vmrPath, string tmpPath)
+        => CreateVmrManager<IVmrInitializer, VmrInitializer>(vmrPath, tmpPath);
+
+    public Task<IVmrUpdater> CreateVmrUpdater()
+        => CreateVmrManager<IVmrUpdater, VmrUpdater>();
+
+    public Task<IVmrUpdater> CreateVmrUpdater(string vmrPath, string tmpPath)
+        => CreateVmrManager<IVmrUpdater, VmrUpdater>(vmrPath, tmpPath);
 
     private async Task<R> CreateVmrManager<R, T>() where T : R
     {
         var mappings = await _sourceMappingParser.ParseMappings(_configuration.VmrPath);
         return ActivatorUtilities.CreateInstance<T>(_serviceProvider, mappings);
+    }
+
+    private async Task<R> CreateVmrManager<R, T>(string vmrPath, string tmpPath) where T : R
+    {
+        var mappings = await _sourceMappingParser.ParseMappings(_configuration.VmrPath);
+        return ActivatorUtilities.CreateInstance<T>(_serviceProvider, new VmrManagerConfiguration(vmrPath, tmpPath), mappings);
     }
 }
