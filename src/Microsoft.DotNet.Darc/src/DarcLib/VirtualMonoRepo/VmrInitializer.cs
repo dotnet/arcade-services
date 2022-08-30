85bfe1698a8144112c8b9c5736a97694088a7f98
Premek Vysoky <premek.vysoky@microsoft.com>
2022-08-30 16:33:53 +0200

    Add recursive initialization

--- src/Microsoft.DotNet.Darc/src/DarcLib/VirtualMonoRepo/VmrInitializer.cs
+++ src/Microsoft.DotNet.Darc/src/DarcLib/VirtualMonoRepo/VmrInitializer.cs
@@ -2,8 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using LibGit2Sharp;
@@ -24,20 +26,27 @@ public class VmrInitializer : VmrManagerBase, IVmrInitializer
         Original commit: {remote}/commit/{newSha}
         """;
 
+    private readonly IVersionDetailsParser _versionDetailsParser;
     private readonly ILogger<VmrUpdater> _logger;
 
     public VmrInitializer(
         IProcessManager processManager,
         IRemoteFactory remoteFactory,
+        IVersionDetailsParser versionDetailsParser,
         ILogger<VmrUpdater> logger,
         IVmrManagerConfiguration configuration,
         IReadOnlyCollection<SourceMapping> mappings)
         : base(processManager, remoteFactory, logger, mappings, configuration.VmrPath, configuration.TmpPath)
     {
+        _versionDetailsParser = versionDetailsParser;
         _logger = logger;
     }
 
-    public async Task InitializeVmr(SourceMapping mapping, string? targetRevision, CancellationToken cancellationToken)
+    public async Task InitializeRepository(
+        SourceMapping mapping,
+        string? targetRevision,
+        bool initializeDependencies,
+        CancellationToken cancellationToken)
     {
         if (File.Exists(GetTagFilePath(mapping)))
         {
@@ -73,5 +82,46 @@ public class VmrInitializer : VmrManagerBase, IVmrInitializer
         Commit(message, DotnetBotCommitSignature);
 
         _logger.LogInformation("Initialization of {name} finished", mapping.Name);
+
+        if (initializeDependencies)
+        {
+            await InitializeDependencies(mapping, cancellationToken);
+        }
+    }
+
+    private async Task InitializeDependencies(SourceMapping mapping, CancellationToken cancellationToken)
+    {
+        var versionDetailsPath = Path.Combine(
+            GetRepoSourcesPath(mapping),
+            VersionFiles.VersionDetailsXml.Replace("/", Environment.NewLine));
+
+        var versionDetailsContent = await File.ReadAllTextAsync(versionDetailsPath, cancellationToken);
+
+        var dependencies = _versionDetailsParser.ParseVersionDetailsXml(versionDetailsContent, true)
+            .Where(d => d.Type == DependencyType.Product && d.SourceBuild is not null);
+
+        foreach (var dependency in dependencies)
+        {
+            var dependencyMapping = Mappings.FirstOrDefault(m => m.Name == dependency.SourceBuild.RepoName);
+
+            if (dependencyMapping is null)
+            {
+                throw new Exception($"No source mapping named '{dependency.SourceBuild.RepoName}' found " +
+                                    $"for a {VersionFiles.VersionDetailsXml} dependency {dependency.Name}");
+            }
+
+            if (Directory.Exists(GetRepoSourcesPath(dependencyMapping)))
+            {
+                _logger.LogDebug("Dependency {repo} has already been initialized", dependencyMapping.Name);
+                continue;
+            }
+
+            _logger.LogInformation("Recursively initializing dependency {repo} / {commit} ({version})",
+                dependencyMapping.Name,
+                dependency.Commit,
+                dependency.Version);
+
+            await InitializeRepository(dependencyMapping, dependency.Commit, true, cancellationToken);
+        }
     }
 }
