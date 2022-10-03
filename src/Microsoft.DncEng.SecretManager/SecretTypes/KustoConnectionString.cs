1eb0523e1bdcf0a0db186f8f3ecf3f5434361e1f
Alex Perovich <alperovi@microsoft.com>
2022-10-03 12:48:17 -0700

    Fix bad merge

--- src/Microsoft.DncEng.SecretManager/SecretTypes/KustoConnectionString.cs
+++ src/Microsoft.DncEng.SecretManager/SecretTypes/KustoConnectionString.cs
@@ -3,31 +3,40 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Microsoft.DncEng.SecretManager.SecretTypes
+namespace Microsoft.DncEng.SecretManager.SecretTypes;
+
+[Name("kusto-connection-string")]
+public class KustoConnectionString : SecretType<KustoConnectionString.Parameters>
 {
-    [Name("kusto-connection-string")]
-    public class KustoConnectionString : SecretType<KustoConnectionString.Parameters>
+    public class Parameters
     {
-        public class Parameters
+        public string DataSource { get; set; }
+        public string InitialCatalog { get; set; }
+        public string AdditionalParameters { get; set; }
+        public SecretReference ADApplication { get; set; }
+    }
+
+    protected override async Task<SecretData> RotateValue(Parameters parameters, RotationContext context,
+        CancellationToken cancellationToken)
+    {
+        string adAppId = await context.GetSecretValue(new SecretReference
         {
-            public string DataSource { get; set; }
-            public string InitialCatalog { get; set; }
-            public string AdditionalParameters { get; set; }
-            public SecretReference ADApplication { get; set; }
+            Location = parameters.ADApplication.Location,
+            Name = parameters.ADApplication.Name + ADApplication.AppIdSuffix
+        });
+        SecretValue adAppSecret = await context.GetSecret(new SecretReference
+        {
+            Location = parameters.ADApplication.Location,
+            Name = parameters.ADApplication.Name + ADApplication.AppSecretSuffix
+        });
+
+        if (adAppSecret == null)
+        {
+            throw new InvalidOperationException($"The secret referenced by secret {context.SecretName} parameter 'adApplication' does not exist.");
         }
 
-        protected override async Task<SecretData> RotateValue(Parameters parameters, RotationContext context, CancellationToken cancellationToken)
-        {
-            string adAppId = await context.GetSecretValue(new SecretReference { Location = parameters.ADApplication.Location, Name = parameters.ADApplication.Name + ADApplication.AppIdSuffix });
-            SecretValue adAppSecret = await context.GetSecret(new SecretReference { Location = parameters.ADApplication.Location, Name = parameters.ADApplication.Name + ADApplication.AppSecretSuffix });
-
-            if (adAppSecret == null)
-            {
-                throw new InvalidOperationException($"The secret referenced by secret {context.SecretName} parameter 'adApplication' does not exist.");
-            }
-
-            var connectionString = new StringBuilder();
-            connectionString.Append($"Data Source={parameters.DataSource}");
+        var connectionString = new StringBuilder();
+        connectionString.Append($"Data Source={parameters.DataSource}");
         if (!string.IsNullOrEmpty(parameters.InitialCatalog))
         {
             connectionString.Append($";Initial Catalog={parameters.InitialCatalog}");
