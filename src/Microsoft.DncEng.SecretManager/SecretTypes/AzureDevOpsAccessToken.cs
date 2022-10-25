a5180710cd7c0b375ea179ad0df3597ec98654e6
Alex Perovich <alperovi@microsoft.com>
2022-10-25 15:43:44 -0700

    Enable automatic rotation of azdo pats

--- src/Microsoft.DncEng.SecretManager/SecretTypes/AzureDevOpsAccessToken.cs
+++ src/Microsoft.DncEng.SecretManager/SecretTypes/AzureDevOpsAccessToken.cs
@@ -1,8 +1,20 @@
 using System;
-using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.DncEng.CommandLineLib;
+using Microsoft.VisualStudio.Services.Account.Client;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.DelegatedAuthorization;
+using Microsoft.VisualStudio.Services.DelegatedAuthorization.WebApi;
+using Microsoft.VisualStudio.Services.OAuth;
+using Microsoft.VisualStudio.Services.Profile;
+using Microsoft.VisualStudio.Services.Profile.Client;
+using Microsoft.VisualStudio.Services.WebApi;
 
 namespace Microsoft.DncEng.SecretManager.SecretTypes;
 
@@ -28,13 +40,46 @@ public class AzureDevOpsAccessToken : SecretType<AzureDevOpsAccessToken.Paramete
         Console = console;
     }
 
-    protected override async Task<SecretData> RotateValue(Parameters parameters, RotationContext context, CancellationToken cancellationToken)
+    private static class Constants
     {
-        if (!Console.IsInteractive)
+        // Note that the below two GUIDs are for VSTS resource ID and Azure Powershell Client ID. Do not modify.
+        internal const string ClientId = "1950a258-227b-4e31-a9cf-717495945fc2";
+        internal const string VstsResourceId = "499b84ac-1321-427f-aa17-267ca6975798";
+    }
+
+    private record OauthAccessTokenResponse
+    {
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; init; }
+        
+    }
+
+    private async Task<VssConnection> ConnectToAzDo(string userName, string password, CancellationToken cancellationToken)
+    {
+        using var oauthClient = new HttpClient();
+        var values = new Dictionary<string, string>
         {
-            throw new HumanInterventionRequiredException($"User intervention required for creation or rotation of an Azure DevOps access token.");
+            ["grant_type"] = "password",
+            ["resource"] = Constants.VstsResourceId,
+            ["username"] = userName,
+            ["password"] = password,
+            ["client_id"] = Constants.ClientId,
+        };
+
+        using var content = new FormUrlEncodedContent(values);
+        using var response = await oauthClient.PostAsync(new Uri("https://login.microsoftonline.com/microsoft.onmicrosoft.com/oauth2/token"), content, cancellationToken);
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(await response.Content.ReadAsStringAsync(cancellationToken));
         }
 
+        var result = await response.Content.ReadFromJsonAsync<OauthAccessTokenResponse>(cancellationToken: cancellationToken);
+        string baseUri = "https://app.vssps.visualstudio.com";
+        return new VssConnection(new Uri(baseUri), new VssCredentials(new VssOAuthAccessTokenCredential(result.AccessToken)));
+    }
+
+    protected override async Task<SecretData> RotateValue(Parameters parameters, RotationContext context, CancellationToken cancellationToken)
+    {
         if (string.IsNullOrEmpty(parameters.Organizations))
         {
             throw new ArgumentException("Organizations is required.");
@@ -45,19 +90,47 @@ public class AzureDevOpsAccessToken : SecretType<AzureDevOpsAccessToken.Paramete
             throw new ArgumentException("Scopes is required.");
         }
 
-        string patGeneratorParams = $"--scopes {parameters.Scopes} --organizations {parameters.Organizations} --name {context.SecretName} --expires-in 180";
+        var orgs = parameters.Organizations.Split(' ');
 
+        var userName = parameters.DomainAccountName;
+        if (!userName.EndsWith("@microsoft.com"))
+        {
+            userName += "@microsoft.com";
+        }
+        
         var password = await context.GetSecretValue(parameters.DomainAccountSecret);
-        Console.WriteLine($"Please run `dotnet pat-generator {patGeneratorParams}`\nLog in using account {parameters.DomainAccountName} and password: {password}");
 
-        var expiration = await Console.PromptAndValidateAsync("PAT expiration (M/d/yyyy)",
-            "PAT expiration format must be M/d/yyyy.",
-            (string value, out DateTime parsedValue) => DateTime.TryParseExact(value, "M/d/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedValue));
+        using var connection = await ConnectToAzDo(userName, password, cancellationToken);
 
-        var pat = await Console.PromptAndValidateAsync("PAT",
-            "PAT must have at least 52 characters.",
-            value => value != null && value.Length >= 52);
+        Console.WriteLine("Connecting to AzDo and retrieving account guids.");
+        
+        var profileClient = await connection.GetClientAsync<ProfileHttpClient>(cancellationToken);
+        var accountClient = await connection.GetClientAsync<AccountHttpClient>(cancellationToken);
+        var tokenClient = await connection.GetClientAsync<TokenHttpClient>(cancellationToken);
 
-        return new SecretData(pat, expiration, expiration.Add(_rotateBeforeExpiration));
+        var me = await profileClient.GetProfileAsync(new ProfileQueryContext(AttributesScope.Core), cancellationToken: cancellationToken);
+        var accounts = await accountClient.GetAccountsByMemberAsync(me.Id, cancellationToken: cancellationToken);
+        var accountGuidMap = accounts.ToDictionary(account => account.AccountName, account => account.AccountId);
+
+        var orgIds = orgs.Select(name => accountGuidMap[name]).ToArray();
+        var now = Clock.UtcNow;
+
+        var scopes = parameters.Scopes
+            .Split(' ')
+            .Select(s => s.StartsWith("vso.") ? s : $"vso.{s}")
+            .ToArray();
+
+        Console.WriteLine($"Creating new pat in orgs '{string.Join(" ", orgIds)}' with scopes '{string.Join("", scopes)}'");
+        var expiresOn = now.AddDays(180);
+        var newToken = await tokenClient.CreateSessionTokenAsync(new SessionToken
+        {
+            DisplayName = $"{context.SecretName} {now:u}",
+            Scope = string.Join(" ", scopes),
+            ValidFrom = now.UtcDateTime,
+            ValidTo = expiresOn.UtcDateTime,
+            TargetAccounts = orgIds,
+        }, cancellationToken: cancellationToken);
+
+        return new SecretData(newToken.Token, expiresOn, expiresOn.Add(_rotateBeforeExpiration));
     }
 }
