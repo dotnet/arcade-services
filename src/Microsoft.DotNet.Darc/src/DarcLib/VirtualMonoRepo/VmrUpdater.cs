f6222e960f8d54125cc38b2a6e73207e1c4238be
Premek Vysoky <premek.vysoky@microsoft.com>
2022-11-01 15:59:19 +0100

    Improve the while

--- src/Microsoft.DotNet.Darc/src/DarcLib/VirtualMonoRepo/VmrUpdater.cs
+++ src/Microsoft.DotNet.Darc/src/DarcLib/VirtualMonoRepo/VmrUpdater.cs
@@ -206,21 +206,16 @@ public class VmrUpdater : VmrManagerBase, IVmrUpdater
             var commitMessages = new StringBuilder();
             var commitCount = 1;
             bool commitLimitHit = false;
-            while (commitsToCopy.TryPop(out LibGit2Sharp.Commit? commit))
+            while (!commitLimitHit && commitsToCopy.TryPop(out LibGit2Sharp.Commit? commit))
             {
                 // Do not list over 23 commits in the message
                 // If there are more, list first 20
-                commitCount++;                
+                commitCount++;          
                 if (commitCount == 20 && commitsToCopy.Count > 3)
                 {
                     commitLimitHit = true;
                     commitMessages.AppendLine($"  [... commit list trimmed ...]");
                     continue;
-                }
-
-                if (commitCount > 20 && commitLimitHit)
-                {
-                    break;
                 }
 
                 commitMessages
