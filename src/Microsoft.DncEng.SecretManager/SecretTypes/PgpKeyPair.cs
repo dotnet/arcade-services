fb51892d52ef30e275bdc8744bc5d803f44116c5
dkurepa <dkurepa@microsoft.com>
2022-11-30 15:16:39 +0100

    add pgp key pair secret type

---
+++ src/Microsoft.DncEng.SecretManager/SecretTypes/PgpKeyPair.cs
@@ -0,0 +1,28 @@
+using Microsoft.DncEng.CommandLineLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.DncEng.SecretManager.SecretTypes
+{
+    [Name("pgp-key-pair")]
+    internal class PgpKeyPair : SecretType<PgpKeyPair.Parameters>
+    {
+        private readonly IConsole _console;
+
+        public class Parameters { }
+    
+        public PgpKeyPair(IConsole console)
+        {
+            _console = console;
+        }
+
+        protected override async Task<SecretData> RotateValue(Parameters parameters, RotationContext context, CancellationToken cancellationToken)
+        {
+            throw new HumanInterventionRequiredException($"Pgp key pair secret rotation required. Human intervention required. Please go to https://aka.ms/signalr-java-publishing and follow the instructions at the bottom of the page");
+        }
+    }
+}
