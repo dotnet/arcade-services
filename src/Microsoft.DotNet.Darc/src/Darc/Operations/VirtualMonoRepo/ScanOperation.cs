9c7d613a091d53b35e90712fd66b54fb3aec8223
dkurepa <dkurepa@microsoft.com>
2023-01-02 13:49:01 +0100

    sort 'using' lines

--- src/Microsoft.DotNet.Darc/src/Darc/Operations/VirtualMonoRepo/ScanOperation.cs
+++ src/Microsoft.DotNet.Darc/src/Darc/Operations/VirtualMonoRepo/ScanOperation.cs
@@ -2,10 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Threading.Tasks;
 using Microsoft.DotNet.Darc.Options.VirtualMonoRepo;
 using Microsoft.DotNet.DarcLib.VirtualMonoRepo;
 using Microsoft.Extensions.DependencyInjection;
-using System.Threading.Tasks;
 
 namespace Microsoft.DotNet.Darc.Operations.VirtualMonoRepo;
 
