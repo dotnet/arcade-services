dc679d5713a47954d9a878cdb4877116d626334e
Premek Vysoky <premek.vysoky@microsoft.com>
2023-01-18 14:21:41 +0100

    Add the `darc vmr get-version` command

--- src/Microsoft.DotNet.Darc/src/Darc/Options/VirtualMonoRepo/VmrSyncCommandLineOptions.cs
+++ src/Microsoft.DotNet.Darc/src/Darc/Options/VirtualMonoRepo/VmrSyncCommandLineOptions.cs
@@ -16,6 +16,6 @@ internal abstract class VmrSyncCommandLineOptions : VmrCommandLineOptions
     
     [Value(0, Required = true, HelpText =
         "Repository names in the form of NAME or NAME:REVISION where REVISION is a commit SHA or other git reference (branch, tag). " +
-        "Omitting REVISION will synchronize the repo to current HEAD. Pass 'all' to update all repositories.")]
+        "Omitting REVISION will synchronize the repo to current HEAD.")]
     public IEnumerable<string> Repositories { get; set; }
 }
