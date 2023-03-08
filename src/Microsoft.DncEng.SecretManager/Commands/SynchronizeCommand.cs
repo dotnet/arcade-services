75cedd5c12eeaaaee8ed26a49faa155230377344
dkurepa <dkurepa@microsoft.com>
2023-03-08 13:42:48 +0100

    resolve comments

--- src/Microsoft.DncEng.SecretManager/Commands/SynchronizeCommand.cs
+++ src/Microsoft.DncEng.SecretManager/Commands/SynchronizeCommand.cs
@@ -86,12 +86,10 @@ public class SynchronizeCommand : Command
             {
                 _console.WriteLine($"Synchronizing secret {name}, type {secret.Type}");
 
-                if (existingSecrets.TryGetValue(name, out var existingSecret))
+                if (existingSecrets.TryGetValue(name, out var existingSecret)
+                    && !existingSecret.Tags.ContainsKey(AzureKeyVault.NextRotationOnTag))
                 {
-                    if (!existingSecret.Tags.ContainsKey(AzureKeyVault.NextRotationOnTag))
-                    {
-                        _console.LogError($"Key Vault Secret '{name}' is missing {AzureKeyVault.NextRotationOnTag} tag, using the end of time as value. Please force a rotation or manually set this value.");
-                    }
+                    _console.LogError($"Key Vault Secret '{name}' is missing {AzureKeyVault.NextRotationOnTag} tag, using the end of time as value. Please force a rotation or manually set this value.");
                 }
 
                 List<string> names = secretType.GetCompositeSecretSuffixes().Select(suffix => name + suffix).ToList();
