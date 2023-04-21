679c062bf2e2e01858b9cd9f468baf759b4bf955
dkurepa <dkurepa@microsoft.com>
2023-04-21 15:22:50 +0200

    delete all projects migrated to dnceng-shared

--- src/Microsoft.DotNet.ServiceFabric.ServiceHost/LoadReporting/EmptyServiceLoadReporter.cs
+++
@@ -1,8 +0,0 @@
-namespace Microsoft.DotNet.ServiceFabric.ServiceHost;
-
-public class EmptyServiceLoadReporter : IServiceLoadReporter
-{
-    public void ReportLoad(string name, int value)
-    {
-    }
-}
