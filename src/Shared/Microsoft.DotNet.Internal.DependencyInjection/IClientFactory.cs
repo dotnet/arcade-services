8e55d7812021155679eab0d49544608a537da701
Djuradj Kurepa <91743470+dkurepa@users.noreply.github.com>
2023-04-24 13:42:57 +0200

    Use dnceng-shared published libs (#2269)
    * add dependencies
    
    * Use packages published by dnceng-shared
    
    * delete all projects migrated to dnceng-shared

--- src/Shared/Microsoft.DotNet.Internal.DependencyInjection/IClientFactory.cs
+++
@@ -1,6 +0,0 @@
-namespace Microsoft.DotNet.Internal.DependencyInjection;
-
-public interface IClientFactory<TClient>
-{
-    Reference<TClient> GetClient(string name);
-}
--- src/Shared/Microsoft.DotNet.Web.Authentication.Tests/EmptyTestStartup.cs
+++
@@ -1,5 +0,0 @@
-namespace Microsoft.DotNet.Web.Authentication.Tests;
-
-public class EmptyTestStartup
-{
-}
