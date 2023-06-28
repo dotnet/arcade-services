b748b24c9fea4b0dc14ff8d64c6c13b866d5c849
Premek Vysoky <premek.vysoky@microsoft.com>
2023-06-28 15:41:36 +0200

    Fix repository name

--- eng/enforce-issue.ps1
+++ eng/enforce-issue.ps1
@@ -11,9 +11,9 @@ function HasReleaseNotes($description) {
 }
 
 $prDetail = Invoke-WebRequest `
-	-UseBasicParsing `
-	-Uri "https://api.github.com/repos/$RepositoryName/pulls/$PullRequestNumber" `
-| ConvertFrom-Json
+		-UseBasicParsing `
+		-Uri "https://api.github.com/repos/$RepositoryName/pulls/$PullRequestNumber" `
+	| ConvertFrom-Json
 
 if ($prDetail.draft) {
 	Write-Host "Draft PR does not have to have GitHub issue specified. Check passed."
@@ -28,16 +28,17 @@ elseif ($prDetail.title -match "\[automated\]") {
 	exit 0
 }
 
-if (HasReleaseNotes $prDetail.body) {
-	Write-Host "PR has release notes in the description. Check passed."
-	exit 0
-}
-
 $hasIssue = $prDetail.body -Match "github\.com/dotnet/(.+)/issues/(\d+)"
 if (-not $hasIssue) {
 	Write-Host "##vso[task.LogIssue type=error;]Link to the corresponding GitHub issue is missing in the PR description. Check failed."
 	exit 1
 }
+
+if (HasReleaseNotes $prDetail.body) {
+	Write-Host "PR has release notes in the description. Check passed."
+	exit 0
+}
+
 
 try {
 	$issueDetail = Invoke-WebRequest `
