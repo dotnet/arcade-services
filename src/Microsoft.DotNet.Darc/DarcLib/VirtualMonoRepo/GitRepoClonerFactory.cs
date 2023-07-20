8e12f371d106dddee6d1699ede40b1b6547738c2
Premek Vysoky <premek.vysoky@microsoft.com>
2023-07-20 15:17:22 +0200

    Use GitNativeRepoCloner but without auth

--- src/Microsoft.DotNet.Darc/DarcLib/VirtualMonoRepo/GitRepoClonerFactory.cs
+++ src/Microsoft.DotNet.Darc/DarcLib/VirtualMonoRepo/GitRepoClonerFactory.cs
@@ -16,19 +16,19 @@ public interface IGitRepoClonerFactory
 public class GitRepoClonerFactory : IGitRepoClonerFactory
 {
     private readonly VmrRemoteConfiguration _vmrRemoteConfig;
+    private readonly IProcessManager _processManager;
 
-    public GitRepoClonerFactory(VmrRemoteConfiguration vmrRemoteConfig)
+    public GitRepoClonerFactory(VmrRemoteConfiguration vmrRemoteConfig, IProcessManager processManager)
     {
         _vmrRemoteConfig = vmrRemoteConfig;
+        _processManager = processManager;
     }
 
     public IGitRepoCloner GetCloner(string repoUri, ILogger logger) => GitRepoTypeParser.ParseFromUri(repoUri) switch
     {
-        GitRepoType.GitHub => new GitRepoCloner(_vmrRemoteConfig.GitHubToken, logger),
-        GitRepoType.AzureDevOps => new GitRepoCloner(_vmrRemoteConfig.AzureDevOpsToken, logger),
-        GitRepoType.Local => new GitRepoCloner(string.Empty, logger),
+        GitRepoType.GitHub => new GitNativeRepoCloner(_processManager, logger),
+        GitRepoType.AzureDevOps => new GitNativeRepoCloner(_processManager, logger),
+        GitRepoType.Local => new GitNativeRepoCloner(_processManager, logger),
         _ => throw new NotImplementedException($"Unsupported repository remote {repoUri}"),
     };
 }
-
-
