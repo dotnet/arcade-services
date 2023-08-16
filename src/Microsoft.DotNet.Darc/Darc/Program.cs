dotnet/arcade-services

commit 03a09d78a145ecadf5799eaa5a31347d3e710d54

[main] Update dependencies from dotnet/dnceng-shared (#2809)
Co-authored-by: dotnet-maestro[bot] <dotnet-maestro[bot]@users.noreply.github.com>
Co-authored-by: dkurepa <dkurepa@microsoft.com>
Co-authored-by: Djuradj Kurepa <91743470+dkurepa@users.noreply.github.com>

src/Microsoft.DotNet.Darc/Darc/Program.cs
@@ -7,11 +7,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using CommandLine;
-using Microsoft.ApplicationInsights;
-using Microsoft.ApplicationInsights.Channel;
-using Microsoft.ApplicationInsights.DataContracts;
-using Microsoft.ApplicationInsights.DependencyCollector;
-using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.DotNet.Darc.Operations;
 using Microsoft.DotNet.Darc.Options;
 using Microsoft.DotNet.Darc.Options.VirtualMonoRepo;
@@ -20,12 +15,8 @@ namespace Microsoft.DotNet.Darc;
 
 internal static class Program
 {
-    private static TelemetryClient s_telemetryClient;
-
     private static int Main(string[] args)
     {
-        InitializeTelemetry();
-
         if (args.Contains("--debug"))
         {
             // Print header (version, sha, arguments)
@@ -35,82 +26,21 @@ internal static class Program
                 "darc command issued: " + string.Join(' ', args));
         }
 
-        try
+        Type[] options;
+        if (args.FirstOrDefault() == "vmr")
         {
-            Type[] options;
-            if (args.FirstOrDefault() == "vmr")
-            {
-                options = GetVmrOptions();
-                args = args.Skip(1).ToArray();
-            }
-            else
-            {
-                options = GetOptions();
-            }
+            options = GetVmrOptions();
+            args = args.Skip(1).ToArray();
+        }
+        else
+        {
+            options = GetOptions();
+        }
 
-            return Parser.Default.ParseArguments(args, options)
+        return Parser.Default.ParseArguments(args, GetOptions())
                 .MapResult(
                     (CommandLineOptions opts) => RunOperation(opts),
-                    RecordFailedCommand
-                );
-        }
-        finally
-        {
-            s_telemetryClient.Flush();
-        }
-    }
-
-    private static int RecordFailedCommand(IEnumerable<Error> errors)
-    {
-        foreach (Error error in errors)
-        {
-            switch (error)
-            {
-                case NamedError named:
-                    s_telemetryClient.TrackTrace(
-                        $"Command line parsing error: {named.Tag}, name '{named.NameInfo.LongName}'",
-                        SeverityLevel.Error,
-                        new Dictionary<string, string>
-                        {
-                            {"errorTag", named.Tag.ToString()},
-                            {"errorName", named.NameInfo.LongName},
-                        }
-                    );
-                    break;
-                case TokenError token:
-                    s_telemetryClient.TrackTrace(
-                        $"Command line parsing error: {token.Tag}, token '{token.Token}'",
-                        SeverityLevel.Error,
-                        new Dictionary<string, string>
-                        {
-                            {"errorTag", token.Tag.ToString()},
-                            {"errorToken", token.Token},
-                        }
-                    );
-                    break;
-                default:
-                    s_telemetryClient.TrackTrace(
-                        $"Command line parsing error: {error.Tag}",
-                        SeverityLevel.Error,
-                        new Dictionary<string, string>
-                        {
-                            {"errorTag", error.Tag.ToString()}
-                        }
-                    );
-                    break;
-                case HelpRequestedError _:
-                    ReportVerb("help");
-                    return 0;
-                case HelpVerbRequestedError verbHelp:
-                    ReportVerb("verb.help", "for", verbHelp.Verb);
-                    return 0;
-                case VersionRequestedError _:
-                    ReportVerb("version");
-                    return 0;
-            }
-        }
-
-        return 1;
+                    (errs => 1));
     }
 
     /// <summary>
@@ -127,23 +57,13 @@ internal static class Program
         {
             using (Operation operation = opts.GetOperation())
             {
-                Stopwatch stopwatch = Stopwatch.StartNew();
-                try
-                {
-                    return operation.ExecuteAsync().GetAwaiter().GetResult();
-                }
-                finally
-                {
-                    stopwatch.Stop();
-                    ReportInvocation(opts, stopwatch.Elapsed);
-                }
+                return operation.ExecuteAsync().GetAwaiter().GetResult();
             }
         }
         catch (Exception e)
         {
             Console.WriteLine("Unhandled exception encountered");
             Console.WriteLine(e);
-            s_telemetryClient.TrackException(e);
             return Constants.ErrorCode;
         }
     }
@@ -199,104 +119,4 @@ internal static class Program
         typeof(GetRepoVersionCommandLineOptions),
         typeof(VmrPushCommandLineOptions)
     };
-
-    private static void InitializeTelemetry()
-    {
-        var isDebugging = Debugger.IsAttached;
-        var config = TelemetryConfiguration.CreateDefault();
-        if (!isDebugging)
-        {
-            config.InstrumentationKey = "9fcef3c7-f401-41c7-9e91-1f6029c8dcc3";
-        }
-
-        var dependencyTracking = new DependencyTrackingTelemetryModule();
-        dependencyTracking.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("core.windows.net");
-        dependencyTracking.Initialize(config);
-
-        config.TelemetryInitializers.Add(new HttpDependenciesParsingTelemetryInitializer());
-        var channel = new InMemoryChannel {DeveloperMode = isDebugging};
-        config.TelemetryChannel = channel;
-
-        s_telemetryClient = new TelemetryClient(config);
-    }
-
-    private static void ReportVerb(string verb, params string [] extra)
-    {
-        var properties = new Dictionary<string, string> {{"verb", verb}};
-        for (int i = 0; i < extra.Length - 1; i += 2)
-        {
-            properties.Add(extra[i], extra[i + 1]);
-        }
-
-        s_telemetryClient.TrackEvent("CommandExecuted",
-            properties,
-            new Dictionary<string, double>
-            {
-                {"duration", 0}
-            }
-        );
-    }
-
-    private static void ReportInvocation(CommandLineOptions options, TimeSpan stopwatchElapsed)
-    {
-        Type optionType = options.GetType();
-        string verb = optionType.GetCustomAttribute<VerbAttribute>()?.Name;
-        if (string.IsNullOrEmpty(verb))
-        {
-            s_telemetryClient.TrackTrace($"Unrecognized options/verb detected: {optionType.Name}");
-            return;
-        }
-
-        Dictionary<string,string> arguments = new Dictionary<string, string>();
-        Dictionary<Type, object> defaultValueCache = new Dictionary<Type, object>();
-        foreach (var prop in optionType.GetProperties(BindingFlags.FlattenHierarchy | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
-        {
-            var optionAttribute = prop.GetCustomAttribute<OptionAttribute>();
-            if (optionAttribute == null)
-            {
-                // Whatever this property is, it's not an option, ignore it
-                continue;
-            }
-
-            var value = prop.GetValue(options);
-            if (value == null)
-            {
-                // This argument wasn't passed, just ignore it
-                continue;
-            }
-
-            if (prop.PropertyType.IsValueType)
-            {
-                // Value types, like "int", are hard, because they aren't null
-                // So we need to Activator.CreateInstance one to get the "default"
-                // value, and then compare to that.
-                if (!defaultValueCache.TryGetValue(prop.PropertyType, out var defaultValue))
-                {
-                    defaultValueCache.Add(prop.PropertyType, defaultValue = Activator.CreateInstance(prop.PropertyType));
-                }
-
-                if (defaultValue != null && defaultValue.Equals(value))
-                {
-                    continue;
-                }
-            }
-
-            if (prop.GetCustomAttribute<RedactFromLoggingAttribute>() != null)
-            {
-                value = "<<REDACTED>>";
-            }
-
-            arguments.Add($"opt.{optionAttribute.LongName}", value.ToString());
-        }
-
-        arguments.Add("verb", verb);
-
-        s_telemetryClient.TrackEvent("CommandExecuted",
-            arguments,
-            new Dictionary<string, double>
-            {
-                {"duration", stopwatchElapsed.TotalMilliseconds}
-            }
-        );
-    }
 }

eng/Version.Details.xml
@@ -1,93 +1,93 @@
 <?xml version="1.0" encoding="utf-8"?>
 <Dependencies>
   <ProductDependencies>
-    <Dependency Name="Microsoft.DotNet.Internal.Logging" Version="1.1.0-beta.23402.1">
+    <Dependency Name="Microsoft.DotNet.Internal.Logging" Version="1.1.0-beta.23403.1">
       <Uri>https://github.com/dotnet/dnceng-shared</Uri>
-      <Sha>a5d81ac102368665d6b17abc4e92ef4fc2219a60</Sha>
+      <Sha>ee1b0a7ca4b3398a96630c957ae8bb0f3aea1e02</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.AspNetCore.ApiPagination" Version="1.1.0-beta.23402.1">
+    <Dependency Name="Microsoft.AspNetCore.ApiPagination" Version="1.1.0-beta.23403.1">
       <Uri>https://github.com/dotnet/dnceng-shared</Uri>
-      <Sha>a5d81ac102368665d6b17abc4e92ef4fc2219a60</Sha>
+      <Sha>ee1b0a7ca4b3398a96630c957ae8bb0f3aea1e02</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.AspNetCore.ApiVersioning" Version="1.1.0-beta.23402.1">
+    <Dependency Name="Microsoft.AspNetCore.ApiVersioning" Version="1.1.0-beta.23403.1">
       <Uri>https://github.com/dotnet/dnceng-shared</Uri>
-      <Sha>a5d81ac102368665d6b17abc4e92ef4fc2219a60</Sha>
+      <Sha>ee1b0a7ca4b3398a96630c957ae8bb0f3aea1e02</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.AspNetCore.ApiVersioning.Analyzers" Version="1.1.0-beta.23402.1">
+    <Dependency Name="Microsoft.AspNetCore.ApiVersioning.Analyzers" Version="1.1.0-beta.23403.1">
       <Uri>https://github.com/dotnet/dnceng-shared</Uri>
-      <Sha>a5d81ac102368665d6b17abc4e92ef4fc2219a60</Sha>
+      <Sha>ee1b0a7ca4b3398a96630c957ae8bb0f3aea1e02</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.AspNetCore.ApiVersioning.Swashbuckle" Version="1.1.0-beta.23402.1">
+    <Dependency Name="Microsoft.AspNetCore.ApiVersioning.Swashbuckle" Version="1.1.0-beta.23403.1">
       <Uri>https://github.com/dotnet/dnceng-shared</Uri>
-      <Sha>a5d81ac102368665d6b17abc4e92ef4fc2219a60</Sha>
+      <Sha>ee1b0a7ca4b3398a96630c957ae8bb0f3aea1e02</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.DncEng.CommandLineLib" Version="1.1.0-beta.23402.1">
+    <Dependency Name="Microsoft.DncEng.CommandLineLib" Version="1.1.0-beta.23403.1">
       <Uri>https://github.com/dotnet/dnceng-shared</Uri>
-      <Sha>a5d81ac102368665d6b17abc4e92ef4fc2219a60</Sha>
+      <Sha>ee1b0a7ca4b3398a96630c957ae8bb0f3aea1e02</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.DncEng.Configuration.Extensions" Version="1.1.0-beta.23402.1">
+    <Dependency Name="Microsoft.DncEng.Configuration.Extensions" Version="1.1.0-beta.23403.1">
       <Uri>https://github.com/dotnet/dnceng-shared</Uri>
-      <Sha>a5d81ac102368665d6b17abc4e92ef4fc2219a60</Sha>
+      <Sha>ee1b0a7ca4b3398a96630c957ae8bb0f3aea1e02</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.DotNet.Authentication.Algorithms" Version="1.1.0-beta.23402.1">
+    <Dependency Name="Microsoft.DotNet.Authentication.Algorithms" Version="1.1.0-beta.23403.1">
       <Uri>https://github.com/dotnet/dnceng-shared</Uri>
-      <Sha>a5d81ac102368665d6b17abc4e92ef4fc2219a60</Sha>
+      <Sha>ee1b0a7ca4b3398a96630c957ae8bb0f3aea1e02</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.DotNet.GitHub.Authentication" Version="1.1.0-beta.23402.1">
+    <Dependency Name="Microsoft.DotNet.GitHub.Authentication" Version="1.1.0-beta.23403.1">
       <Uri>https://github.com/dotnet/dnceng-shared</Uri>
-      <Sha>a5d81ac102368665d6b17abc4e92ef4fc2219a60</Sha>
+      <Sha>ee1b0a7ca4b3398a96630c957ae8bb0f3aea1e02</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.DotNet.Internal.DependencyInjection.Testing" Version="1.1.0-beta.23402.1">
+    <Dependency Name="Microsoft.DotNet.Internal.DependencyInjection.Testing" Version="1.1.0-beta.23403.1">
       <Uri>https://github.com/dotnet/dnceng-shared</Uri>
-      <Sha>a5d81ac102368665d6b17abc4e92ef4fc2219a60</Sha>
+      <Sha>ee1b0a7ca4b3398a96630c957ae8bb0f3aea1e02</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.DotNet.Internal.DependencyInjection" Version="1.1.0-beta.23402.1">
+    <Dependency Name="Microsoft.DotNet.Internal.DependencyInjection" Version="1.1.0-beta.23403.1">
       <Uri>https://github.com/dotnet/dnceng-shared</Uri>
-      <Sha>a5d81ac102368665d6b17abc4e92ef4fc2219a60</Sha>
+      <Sha>ee1b0a7ca4b3398a96630c957ae8bb0f3aea1e02</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.DotNet.Internal.Health" Version="1.1.0-beta.23402.1">
+    <Dependency Name="Microsoft.DotNet.Internal.Health" Version="1.1.0-beta.23403.1">
       <Uri>https://github.com/dotnet/dnceng-shared</Uri>
-      <Sha>a5d81ac102368665d6b17abc4e92ef4fc2219a60</Sha>
+      <Sha>ee1b0a7ca4b3398a96630c957ae8bb0f3aea1e02</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.DotNet.Internal.Testing.DependencyInjection.Abstractions" Version="1.1.0-beta.23402.1">
+    <Dependency Name="Microsoft.DotNet.Internal.Testing.DependencyInjection.Abstractions" Version="1.1.0-beta.23403.1">
       <Uri>https://github.com/dotnet/dnceng-shared</Uri>
-      <Sha>a5d81ac102368665d6b17abc4e92ef4fc2219a60</Sha>
+      <Sha>ee1b0a7ca4b3398a96630c957ae8bb0f3aea1e02</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.DotNet.Internal.Testing.DependencyInjectionCodeGen" Version="1.1.0-beta.23402.1">
+    <Dependency Name="Microsoft.DotNet.Internal.Testing.DependencyInjectionCodeGen" Version="1.1.0-beta.23403.1">
       <Uri>https://github.com/dotnet/dnceng-shared</Uri>
-      <Sha>a5d81ac102368665d6b17abc4e92ef4fc2219a60</Sha>
+      <Sha>ee1b0a7ca4b3398a96630c957ae8bb0f3aea1e02</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.DotNet.Internal.Testing.Utility" Version="1.1.0-beta.23402.1">
+    <Dependency Name="Microsoft.DotNet.Internal.Testing.Utility" Version="1.1.0-beta.23403.1">
       <Uri>https://github.com/dotnet/dnceng-shared</Uri>
-      <Sha>a5d81ac102368665d6b17abc4e92ef4fc2219a60</Sha>
+      <Sha>ee1b0a7ca4b3398a96630c957ae8bb0f3aea1e02</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.DotNet.Kusto" Version="1.1.0-beta.23402.1">
+    <Dependency Name="Microsoft.DotNet.Kusto" Version="1.1.0-beta.23403.1">
       <Uri>https://github.com/dotnet/dnceng-shared</Uri>
-      <Sha>a5d81ac102368665d6b17abc4e92ef4fc2219a60</Sha>
+      <Sha>ee1b0a7ca4b3398a96630c957ae8bb0f3aea1e02</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.DotNet.Metrics" Version="1.1.0-beta.23402.1">
+    <Dependency Name="Microsoft.DotNet.Metrics" Version="1.1.0-beta.23403.1">
       <Uri>https://github.com/dotnet/dnceng-shared</Uri>
-      <Sha>a5d81ac102368665d6b17abc4e92ef4fc2219a60</Sha>
+      <Sha>ee1b0a7ca4b3398a96630c957ae8bb0f3aea1e02</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.DotNet.ServiceFabric.ServiceHost" Version="1.1.0-beta.23402.1">
+    <Dependency Name="Microsoft.DotNet.ServiceFabric.ServiceHost" Version="1.1.0-beta.23403.1">
       <Uri>https://github.com/dotnet/dnceng-shared</Uri>
-      <Sha>a5d81ac102368665d6b17abc4e92ef4fc2219a60</Sha>
+      <Sha>ee1b0a7ca4b3398a96630c957ae8bb0f3aea1e02</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.DotNet.Services.Utility" Version="1.1.0-beta.23402.1">
+    <Dependency Name="Microsoft.DotNet.Services.Utility" Version="1.1.0-beta.23403.1">
       <Uri>https://github.com/dotnet/dnceng-shared</Uri>
-      <Sha>a5d81ac102368665d6b17abc4e92ef4fc2219a60</Sha>
+      <Sha>ee1b0a7ca4b3398a96630c957ae8bb0f3aea1e02</Sha>
     </Dependency>
-    <Dependency Name="Microsoft.DotNet.Web.Authentication" Version="1.1.0-beta.23402.1">
+    <Dependency Name="Microsoft.DotNet.Web.Authentication" Version="1.1.0-beta.23403.1">
       <Uri>https://github.com/dotnet/dnceng-shared</Uri>
-      <Sha>a5d81ac102368665d6b17abc4e92ef4fc2219a60</Sha>
+      <Sha>ee1b0a7ca4b3398a96630c957ae8bb0f3aea1e02</Sha>
     </Dependency>
-    <Dependency Name="ServiceFabricMocks" Version="1.1.0-beta.23402.1">
+    <Dependency Name="ServiceFabricMocks" Version="1.1.0-beta.23403.1">
       <Uri>https://github.com/dotnet/dnceng-shared</Uri>
-      <Sha>a5d81ac102368665d6b17abc4e92ef4fc2219a60</Sha>
+      <Sha>ee1b0a7ca4b3398a96630c957ae8bb0f3aea1e02</Sha>
     </Dependency>
-    <Dependency Name="CoreHealthMonitor" Version="1.1.0-beta.23402.1">
+    <Dependency Name="CoreHealthMonitor" Version="1.1.0-beta.23403.1">
       <Uri>https://github.com/dotnet/dnceng-shared</Uri>
-      <Sha>a5d81ac102368665d6b17abc4e92ef4fc2219a60</Sha>
+      <Sha>ee1b0a7ca4b3398a96630c957ae8bb0f3aea1e02</Sha>
     </Dependency>
   </ProductDependencies>
   <ToolsetDependencies>

eng/Versions.props
@@ -15,28 +15,28 @@
     <MicrosoftDotNetGitIssueManagerVersion>6.0.0-beta.23409.1</MicrosoftDotNetGitIssueManagerVersion>
     <MicrosoftDotNetVersionToolsVersion>6.0.0-beta.23409.1</MicrosoftDotNetVersionToolsVersion>
     <MicrosoftNetTestSdkVersion>17.4.1</MicrosoftNetTestSdkVersion>
-    <MicrosoftDotNetInternalLoggingVersion>1.1.0-beta.23402.1</MicrosoftDotNetInternalLoggingVersion>
-    <MicrosoftAspNetCoreApiPaginationVersion>1.1.0-beta.23402.1</MicrosoftAspNetCoreApiPaginationVersion>
-    <MicrosoftAspNetCoreApiVersioningVersion>1.1.0-beta.23402.1</MicrosoftAspNetCoreApiVersioningVersion>
-    <MicrosoftAspNetCoreApiVersioningAnalyzersVersion>1.1.0-beta.23402.1</MicrosoftAspNetCoreApiVersioningAnalyzersVersion>
-    <MicrosoftAspNetCoreApiVersioningSwashbuckleVersion>1.1.0-beta.23402.1</MicrosoftAspNetCoreApiVersioningSwashbuckleVersion>
-    <MicrosoftDncEngCommandLineLibVersion>1.1.0-beta.23402.1</MicrosoftDncEngCommandLineLibVersion>
-    <MicrosoftDncEngConfigurationExtensionsVersion>1.1.0-beta.23402.1</MicrosoftDncEngConfigurationExtensionsVersion>
-    <MicrosoftDotNetAuthenticationAlgorithmsVersion>1.1.0-beta.23402.1</MicrosoftDotNetAuthenticationAlgorithmsVersion>
-    <MicrosoftDotNetGitHubAuthenticationVersion>1.1.0-beta.23402.1</MicrosoftDotNetGitHubAuthenticationVersion>
-    <MicrosoftDotNetInternalDependencyInjectionTestingVersion>1.1.0-beta.23402.1</MicrosoftDotNetInternalDependencyInjectionTestingVersion>
-    <MicrosoftDotNetInternalDependencyInjectionVersion>1.1.0-beta.23402.1</MicrosoftDotNetInternalDependencyInjectionVersion>
-    <MicrosoftDotNetInternalHealthVersion>1.1.0-beta.23402.1</MicrosoftDotNetInternalHealthVersion>
-    <MicrosoftDotNetInternalTestingDependencyInjectionAbstractionsVersion>1.1.0-beta.23402.1</MicrosoftDotNetInternalTestingDependencyInjectionAbstractionsVersion>
-    <MicrosoftDotNetInternalTestingDependencyInjectionCodeGenVersion>1.1.0-beta.23402.1</MicrosoftDotNetInternalTestingDependencyInjectionCodeGenVersion>
-    <MicrosoftDotNetInternalTestingUtilityVersion>1.1.0-beta.23402.1</MicrosoftDotNetInternalTestingUtilityVersion>
-    <MicrosoftDotNetKustoVersion>1.1.0-beta.23402.1</MicrosoftDotNetKustoVersion>
-    <MicrosoftDotNetMetricsVersion>1.1.0-beta.23402.1</MicrosoftDotNetMetricsVersion>
-    <MicrosoftDotNetServiceFabricServiceHostVersion>1.1.0-beta.23402.1</MicrosoftDotNetServiceFabricServiceHostVersion>
-    <MicrosoftDotNetServicesUtilityVersion>1.1.0-beta.23402.1</MicrosoftDotNetServicesUtilityVersion>
-    <MicrosoftDotNetWebAuthenticationVersion>1.1.0-beta.23402.1</MicrosoftDotNetWebAuthenticationVersion>
-    <ServiceFabricMocksVersion>1.1.0-beta.23402.1</ServiceFabricMocksVersion>
-    <CoreHealthMonitorVersion>1.1.0-beta.23402.1</CoreHealthMonitorVersion>
+    <MicrosoftDotNetInternalLoggingVersion>1.1.0-beta.23403.1</MicrosoftDotNetInternalLoggingVersion>
+    <MicrosoftAspNetCoreApiPaginationVersion>1.1.0-beta.23403.1</MicrosoftAspNetCoreApiPaginationVersion>
+    <MicrosoftAspNetCoreApiVersioningVersion>1.1.0-beta.23403.1</MicrosoftAspNetCoreApiVersioningVersion>
+    <MicrosoftAspNetCoreApiVersioningAnalyzersVersion>1.1.0-beta.23403.1</MicrosoftAspNetCoreApiVersioningAnalyzersVersion>
+    <MicrosoftAspNetCoreApiVersioningSwashbuckleVersion>1.1.0-beta.23403.1</MicrosoftAspNetCoreApiVersioningSwashbuckleVersion>
+    <MicrosoftDncEngCommandLineLibVersion>1.1.0-beta.23403.1</MicrosoftDncEngCommandLineLibVersion>
+    <MicrosoftDncEngConfigurationExtensionsVersion>1.1.0-beta.23403.1</MicrosoftDncEngConfigurationExtensionsVersion>
+    <MicrosoftDotNetAuthenticationAlgorithmsVersion>1.1.0-beta.23403.1</MicrosoftDotNetAuthenticationAlgorithmsVersion>
+    <MicrosoftDotNetGitHubAuthenticationVersion>1.1.0-beta.23403.1</MicrosoftDotNetGitHubAuthenticationVersion>
+    <MicrosoftDotNetInternalDependencyInjectionTestingVersion>1.1.0-beta.23403.1</MicrosoftDotNetInternalDependencyInjectionTestingVersion>
+    <MicrosoftDotNetInternalDependencyInjectionVersion>1.1.0-beta.23403.1</MicrosoftDotNetInternalDependencyInjectionVersion>
+    <MicrosoftDotNetInternalHealthVersion>1.1.0-beta.23403.1</MicrosoftDotNetInternalHealthVersion>
+    <MicrosoftDotNetInternalTestingDependencyInjectionAbstractionsVersion>1.1.0-beta.23403.1</MicrosoftDotNetInternalTestingDependencyInjectionAbstractionsVersion>
+    <MicrosoftDotNetInternalTestingDependencyInjectionCodeGenVersion>1.1.0-beta.23403.1</MicrosoftDotNetInternalTestingDependencyInjectionCodeGenVersion>
+    <MicrosoftDotNetInternalTestingUtilityVersion>1.1.0-beta.23403.1</MicrosoftDotNetInternalTestingUtilityVersion>
+    <MicrosoftDotNetKustoVersion>1.1.0-beta.23403.1</MicrosoftDotNetKustoVersion>
+    <MicrosoftDotNetMetricsVersion>1.1.0-beta.23403.1</MicrosoftDotNetMetricsVersion>
+    <MicrosoftDotNetServiceFabricServiceHostVersion>1.1.0-beta.23403.1</MicrosoftDotNetServiceFabricServiceHostVersion>
+    <MicrosoftDotNetServicesUtilityVersion>1.1.0-beta.23403.1</MicrosoftDotNetServicesUtilityVersion>
+    <MicrosoftDotNetWebAuthenticationVersion>1.1.0-beta.23403.1</MicrosoftDotNetWebAuthenticationVersion>
+    <ServiceFabricMocksVersion>1.1.0-beta.23403.1</ServiceFabricMocksVersion>
+    <CoreHealthMonitorVersion>1.1.0-beta.23403.1</CoreHealthMonitorVersion>
     <MicrosoftDncEngSecretManagerVersion>1.1.0-beta.23381.1</MicrosoftDncEngSecretManagerVersion>
     <MicrosoftDncEngConfigurationBootstrapVersion>1.1.0-beta.23381.1</MicrosoftDncEngConfigurationBootstrapVersion>
   </PropertyGroup>

src/Maestro/Maestro.Web/Pages/Index.cshtml
@@ -91,7 +91,7 @@
             @:{ name: "@theme.name", file: "@theme.file" },
             }
           ],
-          aiKey: '@Html.Raw(Model.InstrumentationKey)',
+          aiConnectionString: '@Html.Raw(Model.ConnectionString)',
           isApiRedirecting: @(DoApiRedirect ? "true" : "false"),
         };
     </script>

src/Maestro/Maestro.Web/Pages/Index.cshtml.cs
@@ -21,12 +21,12 @@ public class IndexModel : PageModel
     public IndexModel(IWebHostEnvironment environment, IOptions<ApplicationInsightsServiceOptions> applicationInsightsOptions)
     {
         Environment = environment;
-        InstrumentationKey = applicationInsightsOptions.Value.InstrumentationKey;
+        ConnectionString = applicationInsightsOptions.Value.ConnectionString;
     }
 
     public IReadOnlyList<(string name, string file)> Themes { get; private set; }
     public string CurrentThemeFile { get; private set; }
-    public string InstrumentationKey { get; }
+    public string ConnectionString { get; }
 
     public PageResult OnGet()
     {

src/Maestro/maestro-angular/src/app/services/application-insights.service.ts
@@ -7,12 +7,12 @@ import { Router, Event, NavigationEnd, ActivatedRouteSnapshot } from '@angular/r
 })
 export class ApplicationInsightsService extends ApplicationInsights {
   constructor() {
-    const ikey = (window as any).applicationData.aiKey;
-    if (ikey) {
+    const aiConnectionString = (window as any).applicationData.aiConnectionString;
+    if (aiConnectionString) {
       super({
         config: {
-          instrumentationKey: ikey,
-          disableTelemetry: ikey == '00000000-0000-0000-0000-000000000000',
+          connectionString: aiConnectionString,
+          disableTelemetry: aiConnectionString == 'InstrumentationKey=00000000-0000-0000-0000-000000000000',
           disableFetchTracking: false,
         },
       });

src/Maestro/maestro-angular/src/index.html
@@ -78,7 +78,7 @@
       { name: "light", file: "" },
       { name: "dark",  file: "" },
     ],
-    aiKey: '00000000-0000-0000-0000-000000000000',
+    aiConnectionString: 'InstrumentationKey=00000000-0000-0000-0000-000000000000',
   };
   </script>
 

commit 212bda48a74778c87531ce5aa3f03b4116ea483c

Fix darc vmr command argument parsing regression Introduced in https://github.com/dotnet/arcade-services/pull/2809

src/Microsoft.DotNet.Darc/Darc/Program.cs
@@ -37,7 +37,7 @@ internal static class Program
             options = GetOptions();
         }
 
-        return Parser.Default.ParseArguments(args, GetOptions())
+        return Parser.Default.ParseArguments(args, options)
                 .MapResult(
                     (CommandLineOptions opts) => RunOperation(opts),
                     (errs => 1));