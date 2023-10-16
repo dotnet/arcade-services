1f02a0507dfc4277a617800c71710eed3af694de
dkurepa <dkurepa@microsoft.com>
2023-10-16 15:34:20 +0200

    Add Listener on the http endpoint

---
+++ src/Maestro/Maestro.Web/MaestroDelegatedStatelessWebService.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Fabric;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.DotNet.ServiceFabric.ServiceHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace Maestro.Web;
+
+public class MaestroDelegatedStatelessWebService<TStartup> : DelegatedStatelessWebService<TStartup> where TStartup : class
+{
+    public MaestroDelegatedStatelessWebService(
+        StatelessServiceContext context,
+        Action<IWebHostBuilder> configureHost,
+        Action<IServiceCollection> configureServices)
+        : base(context, configureHost, configureServices)
+    {
+    }
+
+    protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+    {
+        return new[]
+        {
+            CreateServiceInstanceListener("ServiceEndpoint"),
+            CreateServiceInstanceListener("ServiceEndpointHttp")
+        };
+    }
+}
