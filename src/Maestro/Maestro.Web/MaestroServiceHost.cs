329b79d4c8d9357ebf8e693ed4eb88f71633116d
Djuradj Kurepa <91743470+dkurepa@users.noreply.github.com>
2023-10-16 16:35:01 +0200

    Add Listener on the http endpoint (#2971)

---
+++ src/Maestro/Maestro.Web/MaestroServiceHost.cs
@@ -0,0 +1,31 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.DotNet.ServiceFabric.ServiceHost;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Maestro.Web;
+
+public class MaestroServiceHost : ServiceHost
+{
+    private MaestroServiceHost() { }
+
+    public static new void Run(Action<ServiceHost> configure)
+    {
+        var host = new MaestroServiceHost();
+        host.InternalRun(configure);
+    }
+
+    public override ServiceHost RegisterStatelessWebService<TStartup>(string serviceTypeName, Action<IWebHostBuilder> configureWebHost = null) where TStartup : class
+    {
+        RegisterStatelessService(
+            serviceTypeName,
+            context => new MaestroDelegatedStatelessWebService<TStartup>(
+                context,
+                configureWebHost ?? (builder => { }),
+                ApplyConfigurationToServices));
+        return ConfigureServices(builder => builder.AddScoped<TStartup>());
+    }
+}
