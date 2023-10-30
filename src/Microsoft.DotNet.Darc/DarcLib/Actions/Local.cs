7e90e13349085c39abc2c2d8cedcddf7f586551b
Přemek Vysoký <premek.vysoky@microsoft.com>
2023-10-30 14:06:34 +0100

    Merge `Microsoft.DotNet.DarcLib.AzDev` into `DarcLib` (#3032)

--- src/Microsoft.DotNet.Darc/DarcLib/Actions/Local.cs
+++ src/Microsoft.DotNet.Darc/DarcLib/Actions/Local.cs
@@ -1,15 +1,15 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Microsoft.DotNet.DarcLib.Helpers;
-using Microsoft.Extensions.Logging;
-using NuGet.Versioning;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.DotNet.DarcLib.Helpers;
+using Microsoft.Extensions.Logging;
+using NuGet.Versioning;
 
 namespace Microsoft.DotNet.DarcLib;
 
