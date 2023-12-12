ff7e751d0ae5b507edb9af4e1a5bb344141be167
Přemek Vysoký <premek.vysoky@microsoft.com>
2023-12-12 17:57:41 +0100

    Re-enable disabled tests from the 8.0 bump (#3126)

--- test/Maestro.Data.Tests/DependencyRegistrationTests.cs
+++ test/Maestro.Data.Tests/DependencyRegistrationTests.cs
@@ -1,14 +1,10 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Collections.Generic;
 using FluentAssertions;
 using Microsoft.DotNet.Internal.DependencyInjection.Testing;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Hosting.Internal;
 using NUnit.Framework;
@@ -19,13 +15,15 @@ namespace Maestro.Data.Tests;
 public class DependencyRegistrationTests
 {
     [Test]
-    [Ignore("Needs https://github.com/dotnet/dnceng-shared/pull/42")]
     public void AreDependenciesRegistered()
     {
         DependencyInjectionValidation.IsDependencyResolutionCoherent(s =>
             {
-                s.AddSingleton<IHostEnvironment>(new HostingEnvironment{EnvironmentName = 
-                    Environments.Development});
+                s.AddSingleton<IHostEnvironment>(new HostingEnvironment
+                {
+                    EnvironmentName =
+                    Environments.Development
+                });
                 s.AddBuildAssetRegistry(options =>
                 {
                     options.UseInMemoryDatabase("BuildAssetRegistry");
