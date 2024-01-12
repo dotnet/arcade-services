93c516232c0bd79fd2eb8b78ae605eb48dd2b0e4
dkurepa <dkurepa@microsoft.com>
2024-01-12 16:28:20 +0100

    test it out

---
+++ src/ProductConstructionService/ProductConstructionService.Api/HomeController.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Maestro.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductConstructionService.Api;
+
+[Route("test")]
+public class HomeController(BuildAssetRegistryContext dbContext) : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return Ok(dbContext.Channels.Count());
+    }
+}
