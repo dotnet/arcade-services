9f74568d9dc929c4a0c5aab0316517f8d6740ae8
dkurepa <dkurepa@microsoft.com>
2024-01-15 12:35:21 +0100

    remove test endpoint, enable swagger in all environments

--- src/ProductConstructionService/ProductConstructionService.Api/Program.cs
+++ src/ProductConstructionService/ProductConstructionService.Api/Program.cs
@@ -28,11 +28,9 @@ var app = builder.Build();
 app.MapDefaultEndpoints();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
