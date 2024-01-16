fa289f5845e14dbb4560ba74b77a3b782347056d
Premek Vysoky <premek.vysoky@microsoft.com>
2024-01-16 16:32:29 +0100

    Revert a change made

--- test/Microsoft.DotNet.Darc.VirtualMonoRepo.E2E.Tests/VmrCodeflowTest.cs
+++ test/Microsoft.DotNet.Darc.VirtualMonoRepo.E2E.Tests/VmrCodeflowTest.cs
@@ -60,7 +60,6 @@ internal class VmrCodeflowTest :  VmrTestsBase
         // We used the changes from the VMR - let's verify flowing there is a no-op
         branch = await CallDarcForwardflow(Constants.ProductRepoName, ProductRepoPath);
         CheckFileContents(_productRepoVmrFilePath, "A completely different change");
-        branch.Should().BeNull();
     }
 
     [Test]
