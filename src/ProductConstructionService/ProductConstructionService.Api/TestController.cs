75796c96ad0aea5cbacf7a65273c82dd71588098
dkurepa <dkurepa@microsoft.com>
2024-01-16 12:18:04 +0100

    rename the controller

---
+++ src/ProductConstructionService/ProductConstructionService.Api/TestController.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Maestro.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductConstructionService.Api;
+[Route("test")]
+public class TestController(BuildAssetRegistryContext dbContext) : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return Ok(dbContext.Subscriptions.Count());
+    }
+}
