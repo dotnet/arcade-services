532918cfc986acfc3c1364a9e053d825ac1553f9
Premek Vysoky <premek.vysoky@microsoft.com>
2024-01-16 12:54:22 +0100

    Convert to file scoped namespaces

--- src/Maestro/MaestroCoreHealthMonitor/Program.cs
+++ src/Maestro/MaestroCoreHealthMonitor/Program.cs
@@ -5,26 +5,25 @@ using CoreHealthMonitor;
 using Microsoft.DotNet.ServiceFabric.ServiceHost;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace MaestroCoreHealthMonitor
-{
-    internal static class Program
-    {
-        /// <summary>
-        /// This is the entry point of the service host process.
-        /// </summary>
-        private static void Main()
-        {
-            ServiceHost.Run(
-                host =>
-                {
-                    host.RegisterStatelessService<CoreHealthMonitorService>("CoreHealthMonitorType");
-                    host.ConfigureServices(Configure);
-                });
-        }
+namespace MaestroCoreHealthMonitor;
 
-        public static void Configure(IServiceCollection services)
-        {
-            CoreHealthMonitorService.Configure(services);
-        }
+internal static class Program
+{
+    /// <summary>
+    /// This is the entry point of the service host process.
+    /// </summary>
+    private static void Main()
+    {
+        ServiceHost.Run(
+            host =>
+            {
+                host.RegisterStatelessService<CoreHealthMonitorService>("CoreHealthMonitorType");
+                host.ConfigureServices(Configure);
+            });
+    }
+
+    public static void Configure(IServiceCollection services)
+    {
+        CoreHealthMonitorService.Configure(services);
     }
 }
