13127001582f67623ee96d79e13cf11d1df31017
Djuradj Kurepa <91743470+dkurepa@users.noreply.github.com>
2024-01-16 10:51:12 +0100

    Production construction service <--> BAR connection (#3197)

--- src/ProductConstructionService/ProductConstructionService.Api/Program.cs
+++ src/ProductConstructionService/ProductConstructionService.Api/Program.cs
@@ -2,8 +2,19 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Azure.Identity;
+using Maestro.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddAzureKeyVault(
+    new Uri($"https://{builder.Configuration["KeyVaultName"]}.vault.azure.net/"),
+    new DefaultAzureCredential());
+
+builder.Services.AddDbContext<BuildAssetRegistryContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["BuildAssetRegistryConnectionString"] ?? string.Empty);
+});
 
 builder.AddServiceDefaults();
 
@@ -12,20 +23,14 @@ builder.AddServiceDefaults();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
-builder.Configuration.AddAzureKeyVault(
-    new Uri($"https://{builder.Configuration["KeyVaultName"]}.vault.azure.net/"),
-    new DefaultAzureCredential());
-
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
