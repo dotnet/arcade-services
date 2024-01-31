52f4bd03a03529c823e078aceb7301a19a06ce49
dkurepa <dkurepa@microsoft.com>
2024-01-31 17:25:03 +0100

    Add ManagedIdentityClient to DefaultAzureCredential

--- src/ProductConstructionService/ProductConstructionService.Api/Queue/QueueConfiguration.cs
+++ src/ProductConstructionService/ProductConstructionService.Api/Queue/QueueConfiguration.cs
@@ -1,15 +1,17 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Azure.Identity;
+
 namespace ProductConstructionService.Api.Queue;
 
 public static class QueueConfiguration
 {
     public const string PcsJobQueueConfigurationKey = "PcsJobQueueName";
 
-    public static void AddWorkitemQueues(this WebApplicationBuilder builder)
+    public static void AddWorkitemQueues(this WebApplicationBuilder builder, DefaultAzureCredential credential)
     {
-        builder.AddAzureQueueService("queues");
+        builder.AddAzureQueueService("queues", (settings) => { settings.Credential = credential; });
 
         var queueName = builder.Configuration[PcsJobQueueConfigurationKey] ??
             throw new ArgumentException($"{PcsJobQueueConfigurationKey} missing from the configuration");
