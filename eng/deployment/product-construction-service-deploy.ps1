93952b74e49aec35838b794109587ea74a3b8e92
dkurepa <dkurepa@microsoft.com>
2024-02-09 15:54:00 +0100

    fix the git add command

--- eng/deployment/product-construction-service-deploy.ps1
+++ eng/deployment/product-construction-service-deploy.ps1
@@ -51,7 +51,7 @@ function Compare-Properties($before, $after) {
     git config --global user.name "temp"
     Set-Content -Path output.json -Value $before -Encoding UTF8
     git init
-    git add .\
+    git add .
     git commit -m "Before deployment"
     Set-Content -Path output.json -Value $after -Encoding UTF8
     git diff .
