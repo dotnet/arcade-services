fb29f97317afcec6d76948d98629216d19fc386f
Premek Vysoky <premek.vysoky@microsoft.com>
2024-02-20 15:19:33 +0100

    Suppress whole namespace

--- src/Maestro/Client/src/GlobalSuppressions.cs
+++ src/Maestro/Client/src/GlobalSuppressions.cs
@@ -3,8 +3,7 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:Microsoft.DotNet.Maestro.Client.MaestroApi.Serialize(System.String)~System.String")]
-[assembly: SuppressMessage("Performance", "CA1834:Consider using 'StringBuilder.Append(char)' when applicable", Justification = "<Pending>", Scope = "member", Target = "~M:Microsoft.DotNet.Maestro.Client.QueryBuilder.ToString~System.String")]
-[assembly: SuppressMessage("Performance", "CA1844:Provide memory-based overrides of async methods when subclassing 'Stream'", Justification = "<Pending>", Scope = "type", Target = "~T:Microsoft.DotNet.Maestro.Client.ResponseStream")]
-[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:Microsoft.DotNet.Maestro.Client.MaestroApi.Serialize(System.Boolean)~System.String")]
-[assembly: SuppressMessage("Style", "IDE0034:Simplify 'default' expression", Justification = "<Pending>", Scope = "member", Target = "~P:Microsoft.DotNet.Maestro.Client.Models.SubscriptionPolicy.IsValid")]
+[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "namespaceanddescendants", Target = "~N:Microsoft.DotNet.Maestro.Client")]
+[assembly: SuppressMessage("Performance", "CA1834:Consider using 'StringBuilder.Append(char)' when applicable", Justification = "<Pending>", Scope = "namespaceanddescendants", Target = "~N:Microsoft.DotNet.Maestro.Client")]
+[assembly: SuppressMessage("Performance", "CA1844:Provide memory-based overrides of async methods when subclassing 'Stream'", Justification = "<Pending>", Scope = "namespaceanddescendants", Target = "~N:Microsoft.DotNet.Maestro.Client")]
+[assembly: SuppressMessage("Style", "IDE0034:Simplify 'default' expression", Justification = "<Pending>", Scope = "namespaceanddescendants", Target = "~N:Microsoft.DotNet.Maestro.Client")]
