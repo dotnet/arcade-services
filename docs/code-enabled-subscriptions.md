65b793f3727ab959725a3c04c9a96fc55142b518
Premek Vysoky <premek.vysoky@microsoft.com>
2024-02-20 15:07:24 +0100

    Suppress analyzers in auto-generated code

---
+++ docs/code-enabled-subscriptions.md
@@ -0,0 +1,4 @@
+# Code-enabled subscription processing flow
+
+This document describes how the Maestro and PCS services work in tandem to process changes related to code-enabled PRs (VMR code flow).
+
---
+++ src/Maestro/Client/src/GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:Microsoft.DotNet.Maestro.Client.MaestroApi.Serialize(System.String)~System.String")]
+[assembly: SuppressMessage("Performance", "CA1834:Consider using 'StringBuilder.Append(char)' when applicable", Justification = "<Pending>", Scope = "member", Target = "~M:Microsoft.DotNet.Maestro.Client.QueryBuilder.ToString~System.String")]
+[assembly: SuppressMessage("Performance", "CA1844:Provide memory-based overrides of async methods when subclassing 'Stream'", Justification = "<Pending>", Scope = "type", Target = "~T:Microsoft.DotNet.Maestro.Client.ResponseStream")]
+[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:Microsoft.DotNet.Maestro.Client.MaestroApi.Serialize(System.Boolean)~System.String")]
