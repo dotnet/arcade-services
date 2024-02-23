e5e8aa33800a736e3fa8b02c666371785955fc62
dkurepa <dkurepa@microsoft.com>
2024-02-23 14:53:00 +0100

    add darc memory cache

--- src/ProductConstructionService/ProductConstructionService.Api/AuthenticationConfiguration.cs
+++ src/ProductConstructionService/ProductConstructionService.Api/AuthenticationConfiguration.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Maestro.Authentication;
+using Maestro.DataProviders;
 
 namespace ProductConstructionService.Api;
 
@@ -15,6 +16,8 @@ public static class AuthenticationConfiguration
 
         gitHubAuthentication["ClientId"] = builder.Configuration["github-oauth-id"];
         gitHubAuthentication["ClientSecret"] = builder.Configuration["github-oauth-secret"];
+
+        builder.Services.AddSingleton<DarcRemoteMemoryCache>();
 
         builder.Services.ConfigureAuthServices(builder.Environment.IsDevelopment(), gitHubAuthentication);
     }
