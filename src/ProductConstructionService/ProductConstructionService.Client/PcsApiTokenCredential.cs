fb3c6fc16f818b9d4df44d49d1f3b16ce547ae36
Premek Vysoky <premek.vysoky@microsoft.com>
2024-03-01 14:03:12 +0100

    Add token credential

---
+++ src/ProductConstructionService/ProductConstructionService.Client/PcsApiTokenCredential.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Core;
+
+namespace ProductConstructionService.Client
+{
+    public class PcsApiTokenCredential : TokenCredential
+    {
+        public PcsApiTokenCredential(string token)
+        {
+            Token = token;
+        }
+
+        public string Token { get; }
+
+        public override AccessToken GetToken(TokenRequestContext requestContext, CancellationToken cancellationToken)
+            => new AccessToken(Token, DateTimeOffset.MaxValue);
+
+        public override ValueTask<AccessToken> GetTokenAsync(TokenRequestContext requestContext, CancellationToken cancellationToken)
+            => new ValueTask<AccessToken>(new AccessToken(Token, DateTimeOffset.MaxValue));
+    }
+}
