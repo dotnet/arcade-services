be55169a9c0325dffd955b61b551459e6a80cafd
Premek Vysoky <premek.vysoky@microsoft.com>
2024-03-22 11:49:13 +0100

    Make the bootstrap script error handling graceful

--- src/Maestro/bootstrap.ps1
+++ src/Maestro/bootstrap.ps1
@@ -12,8 +12,19 @@ if (-not $?) {
   exit $?
 }
 
-New-LocalGroup -Name "DncEngConfigurationUsers" -ErrorAction Continue
-Add-LocalGroupMember -Group "DncEngConfigurationUsers" -Member $(whoami) -ErrorAction Continue
+$groupName = "DncEngConfigurationUsers"
+$userName = $(whoami)
+
+$groupExists = Get-LocalGroup -Name $groupName -ErrorAction SilentlyContinue
+$userIsMember = $groupExists | Get-LocalGroupMember | Where-Object { $_.Name -eq $userName }
+
+if (-not $groupExists) {
+  New-LocalGroup -Name $groupName -ErrorAction Continue
+}
+
+if (-not $userIsMember) {
+  Add-LocalGroupMember -Group $groupName -Member $userName -ErrorAction Continue
+}
 
 dotnet tool restore
 dotnet bootstrap-dnceng-configuration -r "https://vault.azure.net" -r "https://management.azure.com"
