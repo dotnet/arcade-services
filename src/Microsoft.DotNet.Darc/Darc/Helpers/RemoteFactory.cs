9a57d37a4c7ac9e83e5eab2e2d523ebce548cbb9
Premek Vysoky <premek.vysoky@microsoft.com>
2024-03-25 17:45:25 +0100

    Create the correct dependency file manager for Arcade updates

--- src/Microsoft.DotNet.Darc/Darc/Helpers/RemoteFactory.cs
+++ src/Microsoft.DotNet.Darc/Darc/Helpers/RemoteFactory.cs
@@ -21,42 +21,7 @@ internal class RemoteFactory : IRemoteFactory
 
     public static IRemote GetRemote(ICommandLineOptions options, string repoUrl, ILogger logger)
     {
-        DarcSettings darcSettings = LocalSettings.GetDarcSettings(options, logger, repoUrl);
-
-        if (darcSettings.GitType != GitRepoType.None &&
-            string.IsNullOrEmpty(darcSettings.GitRepoPersonalAccessToken))
-        {
-            throw new DarcException($"No personal access token was provided for repo type '{darcSettings.GitType}'");
-        }
-
-        // If a temporary repository root was not provided, use the environment
-        // provided temp directory.
-        string temporaryRepositoryRoot = darcSettings.TemporaryRepositoryRoot;
-        if (string.IsNullOrEmpty(temporaryRepositoryRoot))
-        {
-            temporaryRepositoryRoot = Path.GetTempPath();
-        }
-
-        IRemoteGitRepo gitClient = null;
-        if (darcSettings.GitType == GitRepoType.GitHub)
-        {
-            gitClient = new GitHubClient(
-                options.GitLocation,
-                darcSettings.GitRepoPersonalAccessToken,
-                logger,
-                temporaryRepositoryRoot,
-                // Caching not in use for Darc local client.
-                null);
-        }
-        else if (darcSettings.GitType == GitRepoType.AzureDevOps)
-        {
-            gitClient = new AzureDevOpsClient(
-                options.GitLocation,
-                darcSettings.GitRepoPersonalAccessToken,
-                logger,
-                temporaryRepositoryRoot);
-        }
-
+        IRemoteGitRepo gitClient = GetRemoteGitClient(options, repoUrl, logger);
         return new Remote(gitClient, new VersionDetailsParser(), logger);
     }
 
@@ -76,4 +41,50 @@ internal class RemoteFactory : IRemoteFactory
 
     public Task<IRemote> GetRemoteAsync(string repoUrl, ILogger logger)
         => Task.FromResult(GetRemote(_options, repoUrl, logger));
+
+    public Task<IDependencyFileManager> GetDependencyFileManagerAsync(string repoUrl, ILogger logger)
+    {
+        IRemoteGitRepo gitClient = GetRemoteGitClient(_options, repoUrl, logger);
+        return Task.FromResult<IDependencyFileManager>(new DependencyFileManager(gitClient, new VersionDetailsParser(), logger));
+    }
+
+    private static IRemoteGitRepo GetRemoteGitClient(ICommandLineOptions options, string repoUrl, ILogger logger)
+    {
+        DarcSettings darcSettings = LocalSettings.GetDarcSettings(options, logger, repoUrl);
+
+        if (darcSettings.GitType != GitRepoType.None &&
+            string.IsNullOrEmpty(darcSettings.GitRepoPersonalAccessToken))
+        {
+            throw new DarcException($"No personal access token was provided for repo type '{darcSettings.GitType}'");
+        }
+
+        // If a temporary repository root was not provided, use the environment
+        // provided temp directory.
+        string temporaryRepositoryRoot = darcSettings.TemporaryRepositoryRoot;
+        if (string.IsNullOrEmpty(temporaryRepositoryRoot))
+        {
+            temporaryRepositoryRoot = Path.GetTempPath();
+        }
+
+        return darcSettings.GitType switch
+        {
+            GitRepoType.GitHub =>
+                new GitHubClient(
+                    options.GitLocation,
+                    darcSettings.GitRepoPersonalAccessToken,
+                    logger,
+                    temporaryRepositoryRoot,
+                    // Caching not in use for Darc local client.
+                    null),
+
+            GitRepoType.AzureDevOps =>
+                new AzureDevOpsClient(
+                    options.GitLocation,
+                    darcSettings.GitRepoPersonalAccessToken,
+                    logger,
+                    temporaryRepositoryRoot),
+
+            _ => throw new System.InvalidOperationException($"Cannot create a remote of type {darcSettings.GitType}"),
+        };
+    }
 }
