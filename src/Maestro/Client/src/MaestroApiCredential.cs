e3267a3f0f659582c8285c66c77742d1efcc3017
Premek Vysoky <premek.vysoky@microsoft.com>
2024-05-30 12:47:23 +0200

    Use AzureCliCredential

--- src/Maestro/Client/src/MaestroApiCredential.cs
+++ src/Maestro/Client/src/MaestroApiCredential.cs
@@ -50,11 +50,13 @@ namespace Microsoft.DotNet.Maestro.Client
         /// </summary>
         internal static MaestroApiCredential CreateUserCredential(string barApiBaseUri)
         {
+            string appId = WellKnownAuthIds[barApiBaseUri].EntraAppId;
+
             // This is a usual credential obtained against an entra app through a browser sign-in
             var credential = new InteractiveBrowserCredential(new InteractiveBrowserCredentialOptions
             {
                 TenantId = TENANT_ID,
-                ClientId = WellKnownAuthIds[barApiBaseUri].EntraAppId,
+                ClientId = appId,
                 RedirectUri = new Uri("http://localhost"),
                 TokenCachePersistenceOptions = new TokenCachePersistenceOptions()
                 {
@@ -62,30 +64,19 @@ namespace Microsoft.DotNet.Maestro.Client
                 }
             });
 
-            var requestContext = new TokenRequestContext(new string[] { $"api://{WellKnownAuthIds[barApiBaseUri].EntraAppId}/Maestro.User" });
+            var requestContext = new TokenRequestContext(new string[] { $"api://{appId}/Maestro.User" });
             return new MaestroApiCredential(credential, requestContext);
         }
 
         /// <summary>
         /// Use this for non-user-based flows (darc invocation from pipelines).
         /// </summary>
-        internal static MaestroApiCredential CreateNonUserCredential(string barApiBaseUri, string? managedIdentityId = null)
+        internal static MaestroApiCredential CreateNonUserCredential(string barApiBaseUri)
         {
             string appId = WellKnownAuthIds[barApiBaseUri].EntraAppId;
-
-            // This is a required audience during federated credential token creation
-            var miRequestContext = new TokenRequestContext(new string[] { "api://AzureADTokenExchange/.default" });
-            var appRequestContext = new TokenRequestContext(new string[] { $"{appId}/.default" });
-
-            // This credential is obtained through a managed identity
-            // which is added as a federated credential onto the entra app
-            var miCreds = new ManagedIdentityCredential(managedIdentityId ?? WellKnownAuthIds[barApiBaseUri].ManagedIdentityId);
-            var credential = new ClientAssertionCredential(
-               TENANT_ID,
-               appId,
-               async (ct) => (await miCreds.GetTokenAsync(miRequestContext, ct).ConfigureAwait(false)).Token);
-
-            return new MaestroApiCredential(credential, appRequestContext);
+            var requestContext = new TokenRequestContext(new string[] { $"{appId}/.default" });
+            var credential = new AzureCliCredential();
+            return new MaestroApiCredential(credential, requestContext);
         }
     }
 }
