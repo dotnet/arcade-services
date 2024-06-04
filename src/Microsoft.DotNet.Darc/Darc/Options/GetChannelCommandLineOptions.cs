6ab75b43378a0fc5f858e74eec848058b7bf4917
Přemek Vysoký <premek.vysoky@microsoft.com>
2024-06-04 14:16:40 +0200

    Add the `darc get-channel` command (#3584)

---
+++ src/Microsoft.DotNet.Darc/Darc/Options/GetChannelCommandLineOptions.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using CommandLine;
+using Microsoft.DotNet.Darc.Operations;
+
+namespace Microsoft.DotNet.Darc.Options;
+
+[Verb("get-channel", HelpText = "Get a specific channel.")]
+internal class GetChannelCommandLineOptions : CommandLineOptions
+{
+    [Option("id", Required = true, HelpText = "ID of the channel to show.")]
+    public int Id { get; set; }
+
+    public override Operation GetOperation()
+    {
+        return new GetChannelOperation(this);
+    }
+}
