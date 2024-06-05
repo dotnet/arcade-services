385ba47f78fc16638f113e027204941ce32d768a
dkurepa <dkurepa@microsoft.com>
2024-06-05 18:10:55 +0200

    test sql

--- src/ProductConstructionService/ProductConstructionService.Api/Controllers/StatusController.cs
+++ src/ProductConstructionService/ProductConstructionService.Api/Controllers/StatusController.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Maestro.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.OpenApi.Extensions;
@@ -10,7 +11,8 @@ using ProductConstructionService.Api.Queue;
 namespace ProductConstructionService.Api.Controllers;
 
 [Route("status")]
-internal class StatusController(JobScopeManager jobProcessorScopeManager)
+internal class StatusController(JobScopeManager jobProcessorScopeManager, BuildAssetRegistryContext context)
+
     : InternalController
 {
     private readonly JobScopeManager _jobProcessorScopeManager = jobProcessorScopeManager;
@@ -40,6 +42,7 @@ internal class StatusController(JobScopeManager jobProcessorScopeManager)
     [HttpGet(Name = "Status")]
     public IActionResult GetPcsJobProcessorStatus()
     {
-        return Ok(_jobProcessorScopeManager.State.GetDisplayName());
+        var a = context.Channels.First().Name;
+        return Ok(a);
     }
 }
