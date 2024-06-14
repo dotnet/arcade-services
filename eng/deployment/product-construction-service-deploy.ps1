76adee3d2b4bfe6356c2e5812e1709b959307f9a
dkurepa <dkurepa@microsoft.com>
2024-06-14 13:28:38 +0200

    try

--- eng/deployment/product-construction-service-deploy.ps1
+++ eng/deployment/product-construction-service-deploy.ps1
@@ -18,7 +18,7 @@ $authenticationHeader = @{
     "Authorization" = "Bearer $token"
 }
 
-Invoke-WebRequest -Uri https://product-construction-int.delightfuldune-c0f01ab0.westus2.azurecontainerapps.io/status/start -Method Put -Headers $authenticationHeader
+Invoke-WebRequest -Uri $pcsStatusUrl -Method Put -Headers $authenticationHeader
 
 function StopAndWait([string]$pcsStatusUrl, [string]$pcsStopUrl, [hashtable]$authenticationHeader) {
     try {
