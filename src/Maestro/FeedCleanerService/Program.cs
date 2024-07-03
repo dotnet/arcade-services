f814fb23aba2cff4b0f228429f24b2c8bb75e09c
dkurepa <dkurepa@microsoft.com>
2024-07-03 17:45:08 +0200

    Remove the previous registration

--- src/Maestro/FeedCleanerService/Program.cs
+++ src/Maestro/FeedCleanerService/Program.cs
@@ -53,7 +53,6 @@ public static class Program
         services.AddAzureDevOpsTokenProvider();
         services.Configure<AzureDevOpsTokenProviderOptions>("AzureDevOps", (o, s) => s.Bind(o));
         services.AddTransient<IAzureDevOpsClient, AzureDevOpsClient>();
-        services.AddTransient<IProcessManager>(sp => ActivatorUtilities.CreateInstance<ProcessManager>(sp, "git"));
         services.AddTransient<IProcessManager>(sp =>
             new ProcessManager(
                 sp.GetRequiredService<ILogger<ProcessManager>>(),
