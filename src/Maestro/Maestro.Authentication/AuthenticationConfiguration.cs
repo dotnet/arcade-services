294b06018e3c0491ea64d4e4ecca792d1080ae91
Premek Vysoky <premek.vysoky@microsoft.com>
2024-07-26 12:06:34 +0200

    Remove JwtBearer

--- src/Maestro/Maestro.Authentication/AuthenticationConfiguration.cs
+++ src/Maestro/Maestro.Authentication/AuthenticationConfiguration.cs
@@ -2,12 +2,17 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Linq;
+using System.Security.Claims;
+using Maestro.Data;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.DotNet.Web.Authentication;
+using Microsoft.DotNet.Web.Authentication.AccessToken;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Identity.Web;
@@ -42,20 +47,13 @@ public static class AuthenticationConfiguration
             options.HandleSameSiteCookieCompatibility();
         });
 
-        var authentication = services
-            .AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = options.DefaultChallengeScheme = options.DefaultScheme = "Contextual";
-                options.DefaultSignInScheme = OpenIdConnectDefaults.AuthenticationScheme;
-            })
-            .AddPolicyScheme("Contextual", "Contextual", policyOptions =>
-            {
-                policyOptions.ForwardDefaultSelector = ctx =>
-                    !ctx.Request.Path.StartsWithSegments(authenticationSchemeRequestPath)
-                        ? OpenIdConnectDefaults.AuthenticationScheme
-                        : JwtBearerDefaults.AuthenticationScheme;
-            });
+        // Support for old Maestro tokens
+        services
+            .AddIdentity<ApplicationUser, IdentityRole<int>>(
+                options => options.Lockout.AllowedForNewUsers = false)
+            .AddEntityFrameworkStores<BuildAssetRegistryContext>();
 
+        // Register Entra based authentication
         if (entraAuthConfig?.Exists() ?? false)
         {
             var openIdAuth = services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme);
@@ -66,6 +64,80 @@ public static class AuthenticationConfiguration
             openIdAuth
                 .AddMicrosoftIdentityWebApp(entraAuthConfig);
         }
+
+        var authentication = services
+            .AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = options.DefaultChallengeScheme = options.DefaultScheme = "Contextual";
+                options.DefaultSignInScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddPolicyScheme("Contextual", "Contextual", policyOptions =>
+            {
+                policyOptions.ForwardDefaultSelector = ctx =>
+                {
+                    if (!ctx.Request.Path.StartsWithSegments(authenticationSchemeRequestPath))
+                    {
+                        return OpenIdConnectDefaults.AuthenticationScheme;
+                    }
+
+                    // This is a really simple and a bit hacky (but temporary) quick way to tell between BAR and Entra tokens
+                    string? authHeader = ctx.Request.Headers.Authorization.FirstOrDefault();
+                    return authHeader == null || (authHeader?.Length > 100 && authHeader.ToLower().StartsWith("bearer ey"))
+                        ? OpenIdConnectDefaults.AuthenticationScheme
+                        : PersonalAccessTokenDefaults.AuthenticationScheme;
+                };
+            });
+
+        // Support for old Maestro tokens
+        authentication
+            .AddPersonalAccessToken<ApplicationUser>(
+                options =>
+                {
+                    options.Events = new PersonalAccessTokenEvents<ApplicationUser>
+                    {
+                        OnSetTokenHash = async context =>
+                        {
+                            var dbContext = context.HttpContext.RequestServices
+                                .GetRequiredService<BuildAssetRegistryContext>();
+                            int userId = context.User.Id;
+                            var token = new ApplicationUserPersonalAccessToken
+                            {
+                                ApplicationUserId = userId,
+                                Name = context.Name,
+                                Hash = context.Hash,
+                                Created = DateTimeOffset.UtcNow
+                            };
+                            await dbContext.Set<ApplicationUserPersonalAccessToken>().AddAsync(token);
+                            await dbContext.SaveChangesAsync();
+
+                            return token.Id;
+                        },
+                        OnGetTokenHash = async context =>
+                        {
+                            var dbContext = context.HttpContext.RequestServices
+                                .GetRequiredService<BuildAssetRegistryContext>();
+                            ApplicationUserPersonalAccessToken? token = await dbContext
+                                .Set<ApplicationUserPersonalAccessToken>()
+                                .Where(t => t.Id == context.TokenId)
+                                .Include(t => t.ApplicationUser)
+                                .FirstOrDefaultAsync();
+                            if (token != null)
+                            {
+                                context.Success(token.Hash, token.ApplicationUser);
+                            }
+                        },
+                        OnValidatePrincipal = async context =>
+                        {
+                            var dbContext = context.HttpContext.RequestServices
+                                .GetRequiredService<BuildAssetRegistryContext>();
+                            var signInManager = context.HttpContext.RequestServices
+                                .GetRequiredService<SignInManager<ApplicationUser>>();
+
+                            ClaimsPrincipal principal = await signInManager.CreateUserPrincipalAsync(context.User);
+                            context.ReplacePrincipal(principal);
+                        }
+                    };
+                });
 
         // While entra is optional, we only verify the role when it's available in configuration
         // When it's disabled, we create a random GUID policy that will be never satisfied
