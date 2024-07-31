3a5ba8dd660eb5d460ebca8bd8db5d672dad6583
Přemek Vysoký <premek.vysoky@microsoft.com>
2024-07-31 12:04:35 +0200

    Fix BAR token validation (#3766)

--- src/Maestro/Maestro.Authentication/BarTokenAuthenticationHandler.cs
+++ src/Maestro/Maestro.Authentication/BarTokenAuthenticationHandler.cs
@@ -1,12 +1,15 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using System.Linq;
+using System.Net;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Maestro.Data;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.DotNet.Web.Authentication.AccessToken;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -17,51 +20,80 @@ namespace Maestro.Authentication;
 
 public class BarTokenAuthenticationHandler : AuthenticationHandler<PersonalAccessTokenAuthenticationOptions<ApplicationUser>>
 {
-    private readonly BuildAssetRegistryContext _context;
+    private readonly BuildAssetRegistryContext _dbContext;
     private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly IPasswordHasher<ApplicationUser> _passwordHasher;
+    private readonly ILogger<BarTokenAuthenticationHandler> _logger;
 
     public BarTokenAuthenticationHandler(
         BuildAssetRegistryContext context,
         SignInManager<ApplicationUser> signInManager,
+        IPasswordHasher<ApplicationUser> passwordHasher,
         IOptionsMonitor<PersonalAccessTokenAuthenticationOptions<ApplicationUser>> options,
-        ILoggerFactory logger,
+        ILoggerFactory loggerFactory,
         UrlEncoder encoder,
-        ISystemClock clock)
-        : base(options, logger, encoder, clock)
+        ISystemClock clock,
+        ILogger<BarTokenAuthenticationHandler> logger)
+        : base(options, loggerFactory, encoder, clock)
     {
-        _context = context;
+        _dbContext = context;
         _signInManager = signInManager;
+        _passwordHasher = passwordHasher;
+        _logger = logger;
     }
 
     protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
     {
         try
         {
-            string? token = GetToken();
-            if (string.IsNullOrEmpty(token))
+            string? requestToken = GetToken();
+            if (string.IsNullOrEmpty(requestToken))
             {
                 return AuthenticateResult.NoResult();
             }
 
-            ApplicationUserPersonalAccessToken? user = await _context
+            (int tokenId, string password)? decodedToken = DecodeToken(requestToken);
+
+            if (!decodedToken.HasValue || decodedToken.Value.tokenId == 0 || string.IsNullOrEmpty(decodedToken.Value.password))
+            {
+                string message = "Failed to decode personal access token";
+                _logger.LogInformation(message);
+                return AuthenticateResult.Fail(message);
+            }
+
+            (int tokenId, string password) = decodedToken.Value;
+
+            ApplicationUserPersonalAccessToken? dbToken = await _dbContext
                 .Set<ApplicationUserPersonalAccessToken>()
-                .Where(t => t.Hash == token)
+                .Where(t => t.Id == tokenId)
                 .Include(t => t.ApplicationUser)
                 .FirstOrDefaultAsync();
 
-            if (user != null)
+            if (dbToken == null)
             {
-                var ticket = new AuthenticationTicket(await _signInManager.CreateUserPrincipalAsync(user.ApplicationUser), Scheme.Name);
-                var userContext = new PersonalAccessTokenValidatePrincipalContext<ApplicationUser>(Context, Scheme, Options, ticket, user.ApplicationUser);
-                if (userContext.Principal == null)
-                {
-                    return AuthenticateResult.Fail("No principal found");
-                }
-                return AuthenticateResult.Success(new AuthenticationTicket(userContext.Principal, userContext.Properties, Scheme.Name));
+                return AuthenticateResult.Fail("No existing token found");
             }
+
+            string hash = _passwordHasher.HashPassword(dbToken.ApplicationUser, password);
+            PasswordVerificationResult result = _passwordHasher.VerifyHashedPassword(dbToken.ApplicationUser, hash, password);
+
+            if (result != PasswordVerificationResult.Success && result != PasswordVerificationResult.SuccessRehashNeeded)
+            {
+                return AuthenticateResult.Fail("Invalid personal access token password");
+            }
+
+            var ticket = new AuthenticationTicket(await _signInManager.CreateUserPrincipalAsync(dbToken.ApplicationUser), Scheme.Name);
+            var userContext = new PersonalAccessTokenValidatePrincipalContext<ApplicationUser>(Context, Scheme, Options, ticket, dbToken.ApplicationUser);
+            if (userContext.Principal == null)
+            {
+                return AuthenticateResult.Fail("No principal found");
+            }
+
+            return AuthenticateResult.Success(new AuthenticationTicket(userContext.Principal, userContext.Properties, Scheme.Name));
         }
-        catch
+        catch (Exception e)
         {
+            _logger.LogDebug(e, "Failed to authenticate personal access token");
         }
 
         return AuthenticateResult.NoResult();
@@ -80,5 +112,18 @@ public class BarTokenAuthenticationHandler : AuthenticationHandler<PersonalAcces
         }
 
         return authHeader;
+    }
+
+    private (int tokenId, string password)? DecodeToken(string input)
+    {
+        byte[] tokenBytes = WebEncoders.Base64UrlDecode(input);
+        if (tokenBytes.Length != PersonalAccessTokenUtilities.CalculateTokenSizeForPasswordSize(16))
+        {
+            return null;
+        }
+
+        int tokenId = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(tokenBytes, 0));
+        string password = WebEncoders.Base64UrlEncode(tokenBytes, PersonalAccessTokenUtilities.TokenIdByteCount, Options.PasswordSize);
+        return (tokenId, password);
     }
 }
