fa12960dcdb37120277eee4829590259bda51e0c
Stephen Halter <halter73@gmail.com>
2024-08-05 06:22:18 -0700

    Add DependencyFlow BarViz page (#3777)

--- src/Maestro/Maestro.Web/Program.cs
+++ src/Maestro/Maestro.Web/Program.cs
@@ -4,13 +4,28 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using Microsoft.DotNet.ServiceFabric.ServiceHost;
 
 namespace Maestro.Web;
 
 internal static class Program
 {
-    internal static int LocalHttpsPort => int.Parse(Environment.GetEnvironmentVariable("ASPNETCORE_HTTPS_PORT") ?? "443");
+    internal static int LocalHttpsPort { get; }
+    internal static string SourceVersion { get; }
+    internal static string SourceBranch { get; }
+
+    static Program()
+    {
+        LocalHttpsPort = int.Parse(Environment.GetEnvironmentVariable("ASPNETCORE_HTTPS_PORT") ?? "443");
+
+        var metadata = typeof(Program).Assembly
+            .GetCustomAttributes()
+            .OfType<AssemblyMetadataAttribute>()
+            .ToDictionary(m => m.Key, m => m.Value);
+        SourceVersion = metadata.TryGetValue("Build.SourceVersion", out var sourceVer) ? sourceVer : null;
+        SourceBranch = metadata.TryGetValue("Build.SourceBranch", out var sourceBranch) ? sourceBranch : null;
+    }
 
     /// <summary>
     /// Path to the compiled static files for the Angular app.
