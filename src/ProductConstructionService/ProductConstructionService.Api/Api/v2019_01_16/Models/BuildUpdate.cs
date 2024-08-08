8b38a82bf0bb01c85b152539651f774e49f06e26
Premek Vysoky <premek.vysoky@microsoft.com>
2024-08-08 12:56:31 +0200

    Port 2019 Models

---
+++ src/ProductConstructionService/ProductConstructionService.Api/Api/v2019_01_16/Models/BuildUpdate.cs
@@ -0,0 +1,9 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace ProductConstructionService.Api.Api.v2019_01_16.Models;
+
+public class BuildUpdate
+{
+    public bool? Released { get; set; }
+}
