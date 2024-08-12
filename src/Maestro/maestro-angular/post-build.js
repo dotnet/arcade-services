38bbede95596a945cf7ef006ef58ab5647822cf1
Premek Vysoky <premek.vysoky@microsoft.com>
2024-08-12 17:57:50 +0200

    Copy JS libs in post-build.js

--- src/Maestro/maestro-angular/post-build.js
+++ src/Maestro/maestro-angular/post-build.js
@@ -1,7 +1,9 @@
 const path = require("path");
 const fs = require("fs");
+const mkdirp = require("mkdirp");
 
 const outDir = path.join(__dirname, "dist/maestro-angular");
+const node_modules = path.join(__dirname, "node_modules");
 const statsJson = path.join(outDir, "stats.json");
 const assetsJson = path.join(outDir, "assets.json");
 
@@ -55,3 +57,43 @@ for (const asset of Object.keys(stats.assetsByChunkName)) {
 }
 fs.writeFileSync(assetsJson, JSON.stringify(assets, undefined, 2));
 
+// Libraries which are included in pages directly and not part of the bundle
+mkdirp.sync(path.join(outDir, "libs/bootstrap/dist/css"));
+mkdirp.sync(path.join(outDir, "libs/bootstrap/dist/js"));
+mkdirp.sync(path.join(outDir, "libs/d3/dist"));
+mkdirp.sync(path.join(outDir, "libs/popper.js/dist/umd"));
+
+fs.copyFile(
+  path.join(node_modules, "bootstrap/dist/css/bootstrap.min.css"),
+  path.join(outDir, "libs/bootstrap/dist/css/bootstrap.min.css"),
+  (err) => {
+    if (err) throw err;
+  });
+
+fs.copyFile(
+  path.join(node_modules, "bootstrap/dist/css/bootstrap-reboot.min.css"),
+  path.join(outDir, "libs/bootstrap/dist/css/bootstrap-reboot.min.css"),
+  (err) => {
+    if (err) throw err;
+  });
+
+fs.copyFile(
+  path.join(node_modules, "bootstrap/dist/js/bootstrap.min.js"),
+  path.join(outDir, "libs/bootstrap/dist/js/bootstrap.min.js"),
+  (err) => {
+    if (err) throw err;
+  });
+
+fs.copyFile(
+  path.join(node_modules, "d3/dist/d3.min.js"),
+  path.join(outDir, "libs/d3/dist/d3.min.js"),
+  (err) => {
+    if (err) throw err;
+  });
+
+fs.copyFile(
+  path.join(node_modules, "popper.js/dist/umd/popper.min.js"),
+  path.join(outDir, "libs/popper.js/dist/umd/popper.min.js"),
+  (err) => {
+    if (err) throw err;
+  });
