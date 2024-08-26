c47a8d0fb49c65bc744f5cf3717bcaccb2133e03
Djuradj Kurepa <91743470+dkurepa@users.noreply.github.com>
2024-08-26 13:29:15 +0200

    Port dependency updater tests (#3864)

--- test/ProductConstructionService.SubscriptionTriggerer.Tests/DependencyRegistrationTests.cs
+++ test/ProductConstructionService.SubscriptionTriggerer.Tests/DependencyRegistrationTests.cs
@@ -1,12 +1,11 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-using FluentAssertions;
+
 using Microsoft.ApplicationInsights.Channel;
 using Microsoft.DotNet.Internal.DependencyInjection.Testing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
-using NUnit.Framework;
 using ProductConstructionService.SubscriptionTriggerer;
 
 namespace SubscriptionTriggerer.Tests;
@@ -34,7 +33,8 @@ public class DependencyRegistrationTests
             },
             out var message,
             additionalExemptTypes: [
-            "Microsoft.Extensions.Azure.AzureClientsGlobalOptions"
+                "Microsoft.Extensions.Azure.AzureClientsGlobalOptions",
+                "Microsoft.Extensions.Hosting.ConsoleLifetimeOptions"
             ])
             .Should().BeTrue(message);
     }
