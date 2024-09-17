e590960c3d1bf5c8741d4234d78d10dbbeb4b5a0
dkurepa <dkurepa@microsoft.com>
2024-09-17 18:11:11 +0200

    Hmm

--- src/ProductConstructionService/ProductConstructionService.BarViz/Program.cs
+++ src/ProductConstructionService/ProductConstructionService.BarViz/Program.cs
@@ -5,15 +5,21 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
 using ProductConstructionService.BarViz;
+using TextCopy;
+using ProductConstructionService.BarViz.Code.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-const string PcsApiBaseAddress = "https://localhost:53180/";
+string PcsApiBaseAddress = builder.HostEnvironment.IsDevelopment()
+    ? "https://localhost:53180/"
+    : builder.HostEnvironment.BaseAddress;
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(PcsApiBaseAddress) });
 builder.Services.AddFluentUIComponents();
 builder.Services.AddSingleton(ProductConstructionService.Client.PcsApiFactory.GetAnonymous(PcsApiBaseAddress));
+builder.Services.InjectClipboard();
+builder.Services.AddSingleton<UrlRedirectManager>();
 
 await builder.Build().RunAsync();
---
+++ test/ProductConstructionService.BarViz.Tests/RedirectsTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using ProductConstructionService.BarViz.Code.Services;
+
+namespace ProductConstructionService.BarViz.Tests;
+
+
+[TestFixture]
+public class RedirectsTests
+{
+    [Test]
+    public void RedirectFromOldUrlType()
+    {
+        var urlRedirectManager = new UrlRedirectManager();
+
+        urlRedirectManager.ApplyLocatinoRedirects("http://localhost:11/3883/https:%2F%2Fgithub.com%2Fdotnet%2Faspire/latest/graph")
+            .Should().Be("/channel-3883/github:dotnet:aspire/build-latest");
+
+        urlRedirectManager.ApplyLocatinoRedirects("http://localhost/5172/https%3A%2F%2Fdev.azure.com%2Fdnceng%2Finternal%2F_git%2Fdotnet-wpf-int/latest/graph")
+            .Should().Be("/channel-5172/azdo:dnceng:internal:dotnet-wpf-int/build-latest");
+    }
+}
