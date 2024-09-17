e405af9e0a0acb542c2b885de534d890dbd968b9
dkurepa <dkurepa@microsoft.com>
2024-09-17 16:39:06 +0200

    move the controler

---
+++ src/ProductConstructionService/ProductConstructionService.Api/Api/v2020_02_20/Controllers/AzDoController.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.AspNetCore.ApiVersioning;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.DarcLib;
+
+namespace ProductConstructionService.Api.Api.v2020_02_20.Controllers;
+
+[Route("azdo")]
+[ApiVersion("2020-02-20")]
+public class AzDoController(IAzureDevOpsClient azureDevOpsClient) : ControllerBase
+{
+    private readonly IAzureDevOpsClient _azureDevOpsClient = azureDevOpsClient;
+
+    [HttpGet("build/status/{a}")]
+    public async Task<IActionResult> GetBuildStatus(string a)
+    {
+        await Task.Delay(50);
+        return Ok(a);
+        //return Ok(await _azureDevOpsClient.GetBuildsAsync(account, project, definitionId, branch, count, status));
+    }
+}
