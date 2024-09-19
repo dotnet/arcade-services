9ac7a3bb050de344be3d07c2c83c15ed08378874
dkurepa <dkurepa@microsoft.com>
2024-09-19 14:38:05 +0200

    Add DateFormatter

---
+++ src/ProductConstructionService/ProductConstructionService.BarViz/Code/Helpers/DateFormatter.cs
@@ -0,0 +1,31 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace ProductConstructionService.BarViz.Code.Helpers;
+
+public static class DateFormatter
+{
+    public static string FormatDate(DateTimeOffset date)
+    {
+        var now = DateTimeOffset.Now;
+        var timeFormat = "HH:mm";
+        var dateFormat = "yyyy-MM-dd";
+
+        if (date.Date == now.Date)
+        {
+            return $"Today at {date.ToString(timeFormat)}";
+        }
+        else if (date.Date == now.AddDays(-1).Date)
+        {
+            return $"Yesterday at {date.ToString(timeFormat)}";
+        }
+        else if (date.Date >= now.AddDays(-7).Date)
+        {
+            return $"Last {date.DayOfWeek} at {date.ToString(timeFormat)}";
+        }
+        else
+        {
+            return date.ToString(dateFormat);
+        }
+    }
+}
