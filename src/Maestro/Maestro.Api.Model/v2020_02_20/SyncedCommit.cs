a4f2883cdfc36099778addaafc35c9c11b5bb260
Milena Hristova <mhristova@microsoft.com>
2024-09-20 14:50:51 +0200

    Add VMR page (#3991)

---
+++ src/Maestro/Maestro.Api.Model/v2020_02_20/SyncedCommit.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Maestro.Api.Model.v2020_02_20;
+public class SyncedCommit
+{
+    public string RepoPath { get; set; }
+
+    public string CommitUrl { get; set; }
+
+    public string DateCommitted { get; set; }
+}
