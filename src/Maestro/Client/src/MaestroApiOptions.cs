0ddfbeb097f2942b5d93606578936fd447026c68
dkurepa <dkurepa@microsoft.com>
2024-10-11 11:09:16 +0200

    Add PCS endpoint to MaestroApiOptions

--- src/Maestro/Client/src/MaestroApiOptions.cs
+++ src/Maestro/Client/src/MaestroApiOptions.cs
@@ -22,6 +22,7 @@ namespace Microsoft.DotNet.Maestro.Client
 
         public const string StagingMaestroUri = "https://maestro.int-dot.net/";
         public const string OldPcsStagingUri = "https://maestro-int.westus2.cloudapp.azure.com/";
+        public const string PcsProdUri = "https://product-construction-prod.wittysky-0c79e3cc.westus2.azurecontainerapps.io";
         public const string PcsStagingUri = "https://product-construction-int.delightfuldune-c0f01ab0.westus2.azurecontainerapps.io/";
         public const string PcsLocalUri = "https://localhost:53180/";
 
@@ -34,6 +35,7 @@ namespace Microsoft.DotNet.Maestro.Client
             [PcsStagingUri.TrimEnd('/')] = MaestroStagingAppId,
             [PcsLocalUri.TrimEnd('/')] = MaestroStagingAppId,
 
+            [ProductionMaestroUri.TrimEnd('/')] = MaestroProductionAppId,
             [ProductionMaestroUri.TrimEnd('/')] = MaestroProductionAppId,
             [OldProductionMaestroUri.TrimEnd('/')] = MaestroProductionAppId,
         };
