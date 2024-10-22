a1f50fbc4c837e3681cd35eda395683b57cef569
Oleksandr Didyk <106967057+oleksandr-didyk@users.noreply.github.com>
2024-10-22 16:30:47 +0200

    Fixup dependency update from commit in PR description (#4079)

--- src/ProductConstructionService/ProductConstructionService.DependencyFlow/PullRequestUpdater.cs
+++ src/ProductConstructionService/ProductConstructionService.DependencyFlow/PullRequestUpdater.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Collections.Generic;
 using System.Net;
 using Maestro.Contracts;
 using Maestro.Data.Models;
@@ -649,8 +650,11 @@ internal abstract class PullRequestUpdater : IPullRequestUpdater
             MergePolicyCheckResult.PendingPolicies,
             pr.Url);
 
+        var requiredDescriptionUpdates =
+            await CalculateOriginalDependencies(darcRemote, targetRepository, targetBranch, targetRepositoryUpdates);
+
         pullRequest.Description = await _pullRequestBuilder.CalculatePRDescriptionAndCommitUpdatesAsync(
-            targetRepositoryUpdates.RequiredUpdates,
+            requiredDescriptionUpdates,
             pullRequest.Description,
             targetRepository,
             pullRequest.HeadBranch);
@@ -834,6 +838,48 @@ internal abstract class PullRequestUpdater : IPullRequestUpdater
         await _pullRequestState.TryDeleteAsync();
         await _pullRequestCheckReminders.UnsetReminderAsync();
         await _pullRequestUpdateReminders.UnsetReminderAsync();
+    }
+
+    /// <summary>
+    ///     Given a set of updates, replace the `from` version of every dependency update with the corresponding version
+    ///     from the target branch 
+    /// </summary>
+    /// <param name="darcRemote">Darc client used to fetch target branch dependencies.</param>
+    /// <param name="targetRepository">Target repository to fetch the dependencies from.</param>
+    /// <param name="targetBranch">Target branch to fetch the dependencies from.</param>
+    /// <param name="targetRepositoryUpdates">Incoming updates to the repository</param>
+    /// <returns>
+    ///     Subscription update and the corresponding list of altered dependencies
+    /// </returns>
+    /// <remarks>
+    ///     This method is intended for use in situations where we want to keep the information about the original dependency
+    ///     version, such as when updating PR descriptions.
+    /// </remarks>
+    private static async Task<List<(SubscriptionUpdateWorkItem update, List<DependencyUpdate> deps)>> CalculateOriginalDependencies(
+        IRemote darcRemote,
+        string targetRepository,
+        string targetBranch,
+        TargetRepoDependencyUpdate targetRepositoryUpdates)
+    {
+        List<DependencyDetail> targetBranchDeps = [.. await darcRemote.GetDependenciesAsync(targetRepository, targetBranch)];
+
+        List<(SubscriptionUpdateWorkItem update, List<DependencyUpdate> deps)> alteredUpdates = [];
+        foreach (var requiredUpdate in targetRepositoryUpdates.RequiredUpdates)
+        {
+            var updatedDependencies = requiredUpdate.deps
+                .Select(dependency => new DependencyUpdate()
+                {
+                    From = targetBranchDeps
+                            .Where(replace => dependency.From.Name == replace.Name)
+                            .FirstOrDefault(dependency.From),
+                    To = dependency.To,
+                })
+                .ToList();
+
+            alteredUpdates.Add((requiredUpdate.update, updatedDependencies));
+        }
+
+        return alteredUpdates;
     }
 
     #region Code flow subscriptions
