05ab35aa7be1623d260d9c177834ac900c43c50b
Premek Vysoky <premek.vysoky@microsoft.com>
2024-11-04 14:13:24 +0100

    Rename variable

--- test/Microsoft.DotNet.Darc.Tests/DependencyRegistrationTests.cs
+++ test/Microsoft.DotNet.Darc.Tests/DependencyRegistrationTests.cs
@@ -20,14 +20,14 @@ public class DependencyRegistrationTests
     [Test]
     public void AreDarcOperationsRegistered()
     {
-        foreach (Type operationType in Program.GetOptions().Concat(Program.GetVmrOptions()))
+        foreach (Type optionType in Program.GetOptions().Concat(Program.GetVmrOptions()))
         {
             DependencyInjectionValidation.IsDependencyResolutionCoherent(services =>
             {
                 // Register the option type
-                services.AddTransient(operationType);
+                services.AddTransient(optionType);
 
-                var operationOption = (CommandLineOptions)Activator.CreateInstance(operationType);
+                var operationOption = (CommandLineOptions)Activator.CreateInstance(optionType);
                 // Set IsCi to true to avoid login pop up
                 operationOption.IsCi = true;
                 operationOption.RegisterServices(services);
@@ -35,7 +35,7 @@ public class DependencyRegistrationTests
 
                 // Verify we can create the operation
                 var operation = operationOption.GetOperation(provider);
-                operation.Should().NotBeNull($"Operation {operationType.Name} could not be created");
+                operation.Should().NotBeNull($"Operation for {optionType.Name} could not be created");
                 services.AddTransient(operation.GetType());
             },
             out string message).Should().BeTrue(message);
