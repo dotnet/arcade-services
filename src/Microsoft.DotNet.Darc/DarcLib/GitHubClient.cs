9c512948e8d52ee26ca3d7d114acde3db2745035
Milena Hristova <mhristova@microsoft.com>
2024-11-25 09:46:28 +0100

    Log more detailed information if a PR couldn't be merged (#4177)

--- src/Microsoft.DotNet.Darc/DarcLib/GitHubClient.cs
+++ src/Microsoft.DotNet.Darc/DarcLib/GitHubClient.cs
@@ -426,7 +426,10 @@ public class GitHubClient : RemoteRepoBase, IRemoteGitRepo
     public async Task MergeDependencyPullRequestAsync(string pullRequestUrl, MergePullRequestParameters parameters, string mergeCommitMessage)
     {
         (string owner, string repo, int id) = ParsePullRequestUri(pullRequestUrl);
-        Octokit.PullRequest pr = await GetClient(owner, repo).PullRequest.Get(owner, repo, id);
+
+        IGitHubClient gitHubClient = GetClient(owner, repo);
+
+        Octokit.PullRequest pr = await gitHubClient.PullRequest.Get(owner, repo, id);
 
         var mergePullRequest = new MergePullRequest
         {
@@ -435,11 +438,25 @@ public class GitHubClient : RemoteRepoBase, IRemoteGitRepo
             MergeMethod = parameters.SquashMerge ? PullRequestMergeMethod.Squash : PullRequestMergeMethod.Merge
         };
 
-        await GetClient(owner, repo).PullRequest.Merge(owner, repo, id, mergePullRequest);
+        try
+        {
+            await gitHubClient.PullRequest.Merge(owner, repo, id, mergePullRequest);
+        }
+        catch (Octokit.PullRequestNotMergeableException notMergeableException)
+        {
+            throw new PullRequestNotMergeableException(notMergeableException.Message);
+        }
 
         if (parameters.DeleteSourceBranch)
         {
-            await GetClient(owner, repo).Git.Reference.Delete(owner, repo, $"heads/{pr.Head.Ref}");
+            try
+            {
+                await gitHubClient.Git.Reference.Delete(owner, repo, $"heads/{pr.Head.Ref}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation("Couldn't delete branch {sourceBranch} - {message}", pr.Head.Ref, ex.Message);
+            }
         }
     }
 
