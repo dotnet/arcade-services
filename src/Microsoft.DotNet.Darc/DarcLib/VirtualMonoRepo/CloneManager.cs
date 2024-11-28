4dd2d585113631a23f636da9767477c499c6d903
Přemek Vysoký <premek.vysoky@microsoft.com>
2024-11-28 10:59:19 +0100

    Fix E2E code flow tests (fetching refs, updating version files) (#4190)

--- src/Microsoft.DotNet.Darc/DarcLib/VirtualMonoRepo/CloneManager.cs
+++ src/Microsoft.DotNet.Darc/DarcLib/VirtualMonoRepo/CloneManager.cs
@@ -76,28 +76,17 @@ public abstract class CloneManager
             // Path should be returned the same for all invocations
             // We checkout a default ref
             path = await PrepareCloneInternal(remoteUri, dirName, cancellationToken);
-            var missingCommit = false;
 
             // Verify that all requested commits are available
-            foreach (string commit in refsToVerify.ToArray())
+            foreach (string gitRef in refsToVerify.ToArray())
             {
-                try
+                if (await _localGitRepo.GitRefExists(path, gitRef, cancellationToken))
                 {
-                    var objectType = await _localGitRepo.GetObjectTypeAsync(path, commit);
-                    if (objectType == GitObjectType.Commit)
-                    {
-                        refsToVerify.Remove(commit);
-                    }
-                }
-                catch
-                {
-                    // Ref not found yet, let's try another remote
-                    missingCommit = true;
-                    break;
+                    refsToVerify.Remove(gitRef);
                 }
             }
 
-            if (!missingCommit)
+            if (!refsToVerify.Any())
             {
                 _logger.LogDebug("All requested refs ({refs}) found in {repo}", string.Join(", ", requestedRefs), path);
                 break;
