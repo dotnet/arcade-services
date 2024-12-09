2fccf42ccfbcdf780c0c82c2ed59d83d61b2296f
Premek Vysoky <premek.vysoky@microsoft.com>
2024-12-09 09:56:06 +0100

    Fix build errors

--- src/ProductConstructionService/ProductConstructionService.Api/Api/v2020_02_20/Controllers/PullRequestController.cs
+++ src/ProductConstructionService/ProductConstructionService.Api/Api/v2020_02_20/Controllers/PullRequestController.cs
@@ -2,11 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Net;
+using System.Text.RegularExpressions;
+using Maestro.Data;
 using Microsoft.AspNetCore.ApiVersioning;
 using Microsoft.AspNetCore.ApiVersioning.Swashbuckle;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.DotNet.DarcLib;
-using Microsoft.DotNet.Maestro.Client.Models;
+using Microsoft.EntityFrameworkCore;
 using ProductConstructionService.Common;
 using ProductConstructionService.DependencyFlow;
 
@@ -14,17 +15,23 @@ namespace ProductConstructionService.Api.Api.v2020_02_20.Controllers;
 
 [Route("pull-requests")]
 [ApiVersion("2020-02-20")]
-public class PullRequestController : ControllerBase
+public partial class PullRequestController : ControllerBase
 {
+    [GeneratedRegex(@"https://api.github.com/repos/(?<org>[^/]+)/(?<repo>[^/]+)/pulls/(?<id>[0-9]+)/?")]
+    private static partial Regex GitHubApiPrUrlRegex();
+
+    [GeneratedRegex(@"https://dev.azure.com/(?<org>[^/]+)/(?<project>[^/]+)/_apis/git/repositories/(?<repo>[^/]+)/pullRequests/(?<id>[0-9]+)/?")]
+    private static partial Regex AzdoApiPrUrlRegex();
+
     private readonly IRedisCacheFactory _cacheFactory;
-    private readonly IBasicBarClient _barClient;
+    private readonly BuildAssetRegistryContext _context;
 
     public PullRequestController(
         IRedisCacheFactory cacheFactory,
-        IBasicBarClient barClient)
+        BuildAssetRegistryContext context)
     {
         _cacheFactory = cacheFactory;
-        _barClient = barClient;
+        _context = context;
     }
 
     [HttpGet("tracked")]
@@ -46,27 +53,54 @@ public class PullRequestController : ControllerBase
                 continue;
             }
 
-            var subscriptions = pr.ContainedSubscriptions.Select(s => _barClient.GetSubscriptionAsync(s.SubscriptionId));
-
-            await Task.WhenAll(subscriptions);
+            var subscriptionIds = pr.ContainedSubscriptions.Select(s => s.SubscriptionId).ToList();
+            var subscriptions = await _context.Subscriptions
+                .Where(s => subscriptionIds.Contains(s.Id))
+                .Include(s => s.Channel)
+                .ToListAsync();
 
             var updates = subscriptions
-                .Select(task => task.Result)
-                .Select(update => new PullRequestUpdate(update.SourceRepository))
+                .Select(update => new PullRequestUpdate(
+                    update.SourceRepository,
+                    pr.ContainedSubscriptions.First(s => s.SubscriptionId == update.Id).BuildId))
                 .ToList();
 
-            Subscription sampleUpdate = subscriptions.First().Result;
+            var sampleSub = subscriptions.First();
 
-            var targetBranch = sampleUpdate.TargetBranch;
-            var channel = sampleUpdate.Channel.Name;
-
-            prs.Add(new TrackedPullRequest(pr.Url, channel, targetBranch, updates));
+            prs.Add(new TrackedPullRequest(
+                TurnApiUrlToWebsite(pr.Url),
+                sampleSub.Channel.Name,
+                sampleSub.TargetBranch,
+                updates));
         }
 
         return Ok(prs.AsQueryable());
     }
 
-    private record TrackedPullRequest(string Url, string Channel, string TargetBranch, List<PullRequestUpdate> Updates);
+    private static string TurnApiUrlToWebsite(string url)
+    {
+        var match = GitHubApiPrUrlRegex().Match(url);
+        if (match.Success)
+        {
+            return $"https://github.com/{match.Groups["org"]}/{match.Groups["repo"]}/pull/{match.Groups["id"]}";
+        }
 
-    private record PullRequestUpdate(string SourceRepository);
+        match = AzdoApiPrUrlRegex().Match(url);
+        if (match.Success)
+        {
+            return $"https://dev.azure.com/{match.Groups["org"]}/{match.Groups["project"]}/_git/{match.Groups["repo"]}/pullrequest/{match.Groups["id"]}";
+        }
+
+        return url;
+    }
+
+    private record TrackedPullRequest(
+        string Url,
+        string Channel,
+        string TargetBranch,
+        List<PullRequestUpdate> Updates);
+
+    private record PullRequestUpdate(
+        string SourceRepository,
+        int BuildId);
 }
