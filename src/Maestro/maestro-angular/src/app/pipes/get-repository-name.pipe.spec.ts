8471abc69cce316db16da9232d7f96f900986c3d
Premek Vysoky <premek.vysoky@microsoft.com>
2025-01-02 16:00:41 +0100

    Remove leaf projects

--- src/Maestro/maestro-angular/src/app/pipes/get-repository-name.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GetRepositoryNamePipe } from './get-repository-name.pipe';
-
-describe('GetRepositoryNamePipe', () => {
-  it('create an instance', () => {
-    const pipe = new GetRepositoryNamePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
