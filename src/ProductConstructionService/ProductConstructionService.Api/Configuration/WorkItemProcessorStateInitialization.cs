b9478415ed0a699f231215f04e7435c1c29c266e
Premek Vysoky <premek.vysoky@microsoft.com>
2025-01-03 11:11:06 +0100

    Remove unused packages

--- src/ProductConstructionService/ProductConstructionService.Api/Configuration/WorkItemProcessorStateInitialization.cs
+++ src/ProductConstructionService/ProductConstructionService.Api/Configuration/WorkItemProcessorStateInitialization.cs
@@ -1,11 +1,9 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using ProductConstructionService.WorkItems;
 
-namespace ProductConstructionService.WorkItems;
+namespace ProductConstructionService.Api.Configuration;
 
 public static class WorkItemProcessorStateInitialization
 {
