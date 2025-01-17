a8f352daf10c17b59f417590fe300e4d02af6852
dkurepa <dkurepa@microsoft.com>
2025-01-17 13:53:19 +0100

    Make ArcadeRepoDir a UnixPath

--- src/Microsoft.DotNet.Darc/DarcLib/Remote.cs
+++ src/Microsoft.DotNet.Darc/DarcLib/Remote.cs
@@ -228,7 +228,7 @@ public sealed class Remote : IRemote
             {
                 engCommonFiles = engCommonFiles
                     .Select(f => new GitFile(
-                        f.FilePath.Replace(new UnixPath(VmrInfo.ArcadeRepoDir), null).TrimStart('/'),
+                        f.FilePath.Replace(VmrInfo.ArcadeRepoDir, null).TrimStart('/'),
                         f.Content,
                         f.ContentEncoding,
                         f.Mode))
