a20c772793ead4922788e1e6b5ec32a9b77a0801
Premek Vysoky <premek.vysoky@microsoft.com>
2025-01-18 20:45:30 +0100

    Display check times

--- src/ProductConstructionService/Microsoft.DotNet.ProductConstructionService.Client/Generated/Models/TrackedPullRequest.cs
+++ src/ProductConstructionService/Microsoft.DotNet.ProductConstructionService.Client/Generated/Models/TrackedPullRequest.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 
@@ -8,9 +9,11 @@ namespace Microsoft.DotNet.ProductConstructionService.Client.Models
 {
     public partial class TrackedPullRequest
     {
-        public TrackedPullRequest(bool sourceEnabled)
+        public TrackedPullRequest(bool sourceEnabled, DateTimeOffset lastUpdate, DateTimeOffset lastCheck)
         {
             SourceEnabled = sourceEnabled;
+            LastUpdate = lastUpdate;
+            LastCheck = lastCheck;
         }
 
         [JsonProperty("url")]
@@ -24,6 +27,15 @@ namespace Microsoft.DotNet.ProductConstructionService.Client.Models
 
         [JsonProperty("sourceEnabled")]
         public bool SourceEnabled { get; set; }
+
+        [JsonProperty("lastUpdate")]
+        public DateTimeOffset LastUpdate { get; set; }
+
+        [JsonProperty("lastCheck")]
+        public DateTimeOffset LastCheck { get; set; }
+
+        [JsonProperty("nextCheck")]
+        public DateTimeOffset? NextCheck { get; set; }
 
         [JsonProperty("updates")]
         public List<PullRequestUpdate> Updates { get; set; }
