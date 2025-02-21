3045f446aaabeb8c4f2ee4f5bd9d0c924b594f7d
Oleksandr.Didyk <odidyk@microsoft.com>
2025-02-21 11:45:42 +0100

    fixup active revision fetch

--- src/ProductConstructionService/ProductConstructionService.WorkItems/ReplicaWorkItemProcessorStateCache.cs
+++ src/ProductConstructionService/ProductConstructionService.WorkItems/ReplicaWorkItemProcessorStateCache.cs
@@ -12,7 +12,7 @@ public interface IReplicaWorkItemProcessorStateCacheFactory
     /// <summary>
     /// Returns a list of WorkItemProcessorStateCaches for all active Replicas
     /// </summary>
-    Task<List<WorkItemProcessorStateCache>> GetAllWorkItemProcessorStateCachesAsync(string revisionName);
+    Task<List<WorkItemProcessorStateCache>> GetAllWorkItemProcessorStateCachesAsync(string? revisionName = null);
 }
 
 public class ReplicaWorkItemProcessorStateCache : IReplicaWorkItemProcessorStateCacheFactory
@@ -31,8 +31,20 @@ public class ReplicaWorkItemProcessorStateCache : IReplicaWorkItemProcessorState
         _logger = logger;
     }
 
-    public async Task<List<WorkItemProcessorStateCache>> GetAllWorkItemProcessorStateCachesAsync(string revisionName)
+    public async Task<List<WorkItemProcessorStateCache>> GetAllWorkItemProcessorStateCachesAsync(string? revisionName = null)
     {
+        if (string.IsNullOrEmpty(revisionName))
+        {
+            revisionName = _containerApp.Data.Configuration.Ingress.Traffic
+                .Single(traffic => traffic.Weight == 100)
+                .RevisionName;
+
+            if (string.IsNullOrEmpty(revisionName))
+            {
+                throw new Exception("Current active revision has no revision name");
+            }
+        }
+
         // Always fetch the latest container app information, in case there was a deployment or something like that
         // in between calls
         _containerApp = await _containerApp.GetAsync();
