ca9c599fe380f3f8428692a0d69c7f09639c82a1
Přemek Vysoký <premek.vysoky@microsoft.com>
2025-03-10 11:36:27 +0100

    Add a flat-flow migration tool (#4537)
    - The tool migrates all repositories at once as that is what we will
    want to do in the end.
    - Moves `CompactConsoleLoggerFormatter` so that it can be reused from
    other CLI projects.
    - The tool runs in a dry-run by default where it only logs which actions
    would be taken (which subscriptions disabled, created, ...).
    - The log of actions it creates looks like this:
    ```json
    {
      "https://github.com/dotnet/wpf - https://github.com/dotnet/windowsdesktop": {
        "action": "Disable",
        "id": "1e4fa27f-25cc-4275-f3f1-08d8e97c775d"
      },
      "https://github.com/dotnet/arcade - https://github.com/dotnet/windowsdesktop": {
        "action": "Disable",
        "id": "2f20696b-414c-44f2-a5a4-08d8ed868088"
      },
      "VMR -\u003E https://github.com/dotnet/windowsdesktop": {
        "action": "Create",
        "parameters": {
          "codeflow": true,
          "branch": "main",
          "excludedAssets": ""
        }
      },
    }
    ```
    
    https://github.com/dotnet/arcade-services/issues/4507

---
+++ tools/FlatFlowMigrationCli/MigrationLogger.cs
@@ -0,0 +1,137 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.DotNet.ProductConstructionService.Client.Models;
+using Microsoft.Extensions.Logging;
+
+namespace FlatFlowMigrationCli;
+
+/// <summary>
+/// Class that logs the operations in console and into a file instead of performing them.
+/// </summary>
+internal class MigrationLogger : ISubscriptionMigrator
+{
+    private readonly ILogger<MigrationLogger> _logger;
+    private readonly string _outputPath;
+
+    public MigrationLogger(ILogger<MigrationLogger> logger, string outputPath)
+    {
+        _logger = logger;
+        _outputPath = outputPath;
+    }
+
+    public async Task DisableSubscriptionAsync(Subscription subscription)
+    {
+        _logger.LogInformation("Would disable a subscription {sourceRepository} -> {targetRepository} / {subscriptionId}",
+            RemoveUrlPrefix(subscription.SourceRepository),
+            RemoveUrlPrefix(subscription.TargetRepository),
+            subscription.Id);
+
+        await LogActionAsync(GetActionKey(subscription), Action.Disable, subscription.Id.ToString());
+    }
+
+    public async Task DeleteSubscriptionAsync(Subscription subscription)
+    {
+        _logger.LogInformation("Would delete an existing subscription {sourceRepository} -> {targetRepository} / {subscriptionId}...",
+            RemoveUrlPrefix(subscription.SourceRepository),
+            RemoveUrlPrefix(subscription.TargetRepository),
+            subscription.Id);
+        await LogActionAsync(GetActionKey(subscription), Action.Delete, subscription.Id.ToString());
+    }
+
+    public async Task CreateVmrSubscriptionAsync(Subscription subscription)
+    {
+        _logger.LogInformation("Would create subscription {vmrUri} -> {repoUri}",
+            RemoveUrlPrefix(Constants.VmrUri),
+            RemoveUrlPrefix(subscription.TargetRepository));
+        await LogActionAsync($"{Constants.VmrUri} - {subscription.TargetRepository}", Action.Create, null, new()
+        {
+            { "codeflow", false },
+            { "branch", subscription.TargetBranch },
+        });
+    }
+
+    public async Task CreateBackflowSubscriptionAsync(string mappingName, string repoUri, string branch, HashSet<string> excludedAssets)
+    {
+        _logger.LogInformation("Would create a backflow subscription for {repoUri}", RemoveUrlPrefix(repoUri));
+        await LogActionAsync($"{Constants.VmrUri} - {repoUri}", Action.Create, null, new()
+        {
+            { "codeflow", true },
+            { "branch", branch },
+            { "excludedAssets", string.Join(", ", excludedAssets) },
+        });
+    }
+
+    public async Task CreateForwardFlowSubscriptionAsync(string mappingName, string repoUri, string channelName)
+    {
+        _logger.LogInformation("Would create a forward flow subscription for {repoUri}", RemoveUrlPrefix(repoUri));
+        await LogActionAsync($"{repoUri} - VMR", Action.Create, null, new()
+        {
+            { "codeflow", true },
+            { "channel", channelName },
+        });
+    }
+
+    private async Task LogActionAsync(string repoUri, Action action, string? id, Dictionary<string, object?>? Parameters = null)
+    {
+        var log = await ReadLog();
+        log[repoUri] = new RepoActionLog(action, id, Parameters);
+        await WriteLog(log);
+    }
+
+    private async Task<ActionLog> ReadLog()
+    {
+        if (!File.Exists(_outputPath))
+        {
+            return [];
+        }
+
+        using var file = File.Open(_outputPath, FileMode.Open);
+
+        try
+        {
+            var log = await JsonSerializer.DeserializeAsync<ActionLog>(file, SerializerOptions);
+            file.Close();
+            return log ?? [];
+        }
+        catch
+        {
+            file.Close();
+            return [];
+        }
+    }
+
+    private async Task WriteLog(ActionLog log)
+    {
+        using var file = File.Open(_outputPath, FileMode.Create);
+        await JsonSerializer.SerializeAsync(file, log, SerializerOptions);
+        file.Close();
+    }
+
+    private static string GetActionKey(Subscription subscription)
+        => $"{subscription.SourceRepository} - {subscription.TargetRepository}";
+
+    private static readonly JsonSerializerOptions SerializerOptions = new(JsonSerializerDefaults.Web)
+    {
+        WriteIndented = true,
+        AllowTrailingCommas = true,
+        Converters = { new JsonStringEnumConverter() },
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+    };
+
+    private static string RemoveUrlPrefix(string url) => url.Replace("https://github.com/", null);
+}
+
+internal class ActionLog : Dictionary<string, RepoActionLog>{}
+
+internal record RepoActionLog(Action Action, string? Id, Dictionary<string, object?>? Parameters);
+
+internal enum Action
+{
+    Unknown,
+    Create,
+    Disable,
+    Delete,
+}
---
+++ tools/FlatFlowMigrationCli/SubscriptionMigrator.cs
@@ -0,0 +1,135 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using FlatFlowMigrationCli.Operations;
+using Microsoft.DotNet.ProductConstructionService.Client;
+using Microsoft.DotNet.ProductConstructionService.Client.Models;
+using Microsoft.Extensions.Logging;
+
+namespace FlatFlowMigrationCli;
+
+/// <summary>
+/// Class that performs actual write changes in subscriptions.
+/// </summary>
+internal class SubscriptionMigrator : ISubscriptionMigrator
+{
+    private readonly IProductConstructionServiceApi _pcsClient;
+    private readonly ILogger<MigrateOperation> _logger;
+
+    public SubscriptionMigrator(IProductConstructionServiceApi pcsClient, ILogger<MigrateOperation> logger)
+    {
+        _pcsClient = pcsClient;
+        _logger = logger;
+    }
+
+    public async Task DisableSubscriptionAsync(Subscription incoming)
+    {
+        var disabledSubscription = new SubscriptionUpdate
+        {
+            ChannelName = incoming.Channel.Name,
+            SourceRepository = incoming.SourceRepository,
+            Enabled = false,
+            Policy = incoming.Policy,
+            PullRequestFailureNotificationTags = incoming.PullRequestFailureNotificationTags,
+        };
+
+        await _pcsClient.Subscriptions.UpdateSubscriptionAsync(incoming.Id, disabledSubscription);
+        _logger.LogInformation("Disabled subscription {subscriptionId} {sourceRepository} -> {targetRepository}",
+            incoming.Id,
+            incoming.SourceRepository,
+            incoming.TargetRepository);
+    }
+
+    public async Task DeleteSubscriptionAsync(Subscription incoming)
+    {
+        _logger.LogInformation("Deleting an existing subscription to VMR {subscriptionId}...", incoming.Id);
+        await _pcsClient.Subscriptions.DeleteSubscriptionAsync(incoming.Id);
+    }
+
+    public async Task CreateVmrSubscriptionAsync(Subscription outgoing)
+    {
+        _logger.LogInformation("Creating a new VMR subscription for {repoUri}...", outgoing.TargetRepository);
+
+        var newVmrSubscription = new SubscriptionData(
+            Constants.VmrChannelName,
+            Constants.VmrUri,
+            outgoing.TargetRepository,
+            outgoing.TargetBranch,
+            outgoing.Policy,
+            outgoing.PullRequestFailureNotificationTags);
+
+        var newSub = await _pcsClient.Subscriptions.CreateAsync(newVmrSubscription);
+
+        _logger.LogInformation("Created subscription {subscriptionId} for {repoUri} from the VMR", newSub.Id, outgoing.TargetRepository);
+    }
+
+    public async Task CreateBackflowSubscriptionAsync(string mappingName, string repoUri, string branch, HashSet<string> excludedAssets)
+    {
+        var existingSubscriptions = await _pcsClient.Subscriptions.ListSubscriptionsAsync(
+            sourceRepository: Constants.VmrUri,
+            targetRepository: repoUri,
+            sourceEnabled: true,
+            sourceDirectory: mappingName);
+
+        if (existingSubscriptions.Any(s => s.Channel.Name == Constants.VmrChannelName))
+        {
+            _logger.LogInformation("Backflow subscription already exists for {repoUri}", repoUri);
+            return;
+        }
+
+        _logger.LogInformation("Creating a backflow subscription for {repoUri}", repoUri);
+
+        var newBackflowSubscription = new SubscriptionData(
+            Constants.VmrChannelName,
+            Constants.VmrUri,
+            repoUri,
+            branch,
+            new SubscriptionPolicy(batchable: false, UpdateFrequency.EveryBuild)
+            {
+                MergePolicies = [new MergePolicy() { Name = "Standard" }]
+            },
+            null)
+        {
+            SourceEnabled = true,
+            SourceDirectory = mappingName,
+            ExcludedAssets = [.. excludedAssets],
+        };
+
+        var subscription = await _pcsClient.Subscriptions.CreateAsync(newBackflowSubscription);
+
+        _logger.LogInformation("Created a backflow subscription {subscriptionId}", subscription.Id);
+    }
+
+    public async Task CreateForwardFlowSubscriptionAsync(string mappingName, string repoUri, string channelName)
+    {
+        var existingSubscriptions = await _pcsClient.Subscriptions.ListSubscriptionsAsync(
+            sourceRepository: repoUri,
+            targetRepository: Constants.VmrUri,
+            sourceDirectory: mappingName);
+
+        if (existingSubscriptions.Any(s => s.Channel.Name == Constants.VmrChannelName))
+        {
+            _logger.LogInformation("Forward flow subscription already exists for {repoUri}", repoUri);
+            return;
+        }
+
+        _logger.LogInformation("Creating a forward flow subscription for {repoUri}", repoUri);
+        var newForwardFlowSubscription = new SubscriptionData(
+            channelName,
+            repoUri,
+            Constants.VmrUri,
+            "main",
+            new SubscriptionPolicy(batchable: false, UpdateFrequency.EveryBuild)
+            {
+                MergePolicies = [new MergePolicy() { Name = "Standard" }]
+            },
+            null)
+        {
+            SourceEnabled = true,
+            TargetDirectory = mappingName,
+        };
+
+        var subscription = await _pcsClient.Subscriptions.CreateAsync(newForwardFlowSubscription);
+        _logger.LogInformation("Created a forward flow subscription {subscriptionId}", subscription.Id);
+    }
+}
