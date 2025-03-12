1aaf0a4417ec68fa151ab8aedafb03eda3a4cb58
Přemek Vysoký <premek.vysoky@microsoft.com>
2025-03-12 09:59:53 +0100

    Allow quoting search terms on the Subscription page (#4553)
    - Adds a support for "quoting search terms" when filtering in the
    Subscription page
    - Resolves https://github.com/dotnet/arcade-services/issues/4494

---
+++ src/ProductConstructionService/ProductConstructionService.BarViz/Code/Helpers/StringExtensions.cs
@@ -0,0 +1,48 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Text;
+
+namespace ProductConstructionService.BarViz.Code.Helpers;
+
+internal static class StringExtensions
+{
+    public static string[] ParseSearchTerms(string searchFilter)
+    {
+        var terms = new List<string>();
+        var currentTerm = new StringBuilder();
+        bool inQuotes = false;
+
+        foreach (var c in searchFilter)
+        {
+            if (c == '"')
+            {
+                inQuotes = !inQuotes;
+                if (!inQuotes && currentTerm.Length > 0)
+                {
+                    terms.Add(currentTerm.ToString());
+                    currentTerm.Clear();
+                }
+            }
+            else if (char.IsWhiteSpace(c) && !inQuotes)
+            {
+                if (currentTerm.Length > 0)
+                {
+                    terms.Add(currentTerm.ToString());
+                    currentTerm.Clear();
+                }
+            }
+            else
+            {
+                currentTerm.Append(c);
+            }
+        }
+
+        if (currentTerm.Length > 0)
+        {
+            terms.Add(currentTerm.ToString());
+        }
+
+        return terms.ToArray();
+    }
+}
