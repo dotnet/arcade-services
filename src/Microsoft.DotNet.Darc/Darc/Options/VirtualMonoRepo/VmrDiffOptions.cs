a473f2134201004903aa8b044711c01c7e5cac9c
dkurepa <dkurepa@microsoft.com>
2025-03-31 12:14:13 +0200

    delete more useless stuff

--- src/Microsoft.DotNet.Darc/Darc/Options/VirtualMonoRepo/VmrDiffOptions.cs
+++ src/Microsoft.DotNet.Darc/Darc/Options/VirtualMonoRepo/VmrDiffOptions.cs
@@ -3,7 +3,6 @@
 
 using CommandLine;
 using Microsoft.DotNet.Darc.Operations.VirtualMonoRepo;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.DotNet.Darc.Options.VirtualMonoRepo;
 [Verb("diff", HelpText = "Diff the contents of the virtual repo with the contents of the source repos.")]
@@ -16,9 +15,4 @@ internal class VmrDiffOptions : VmrCommandLineOptions<VmrDiffOperation>
         "Repository names in the form of NAME or NAME:REVISION where REVISION is a commit SHA or other git reference (branch, tag). " +
         "Omitting REVISION will synchronize the repo to current HEAD.")]
     public string Input { get; set; }
-
-    public override IServiceCollection RegisterServices(IServiceCollection services)
-    {
-        return base.RegisterServices(services);
-    }
 }
