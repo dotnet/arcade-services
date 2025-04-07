471f5d0f7c152918e5dda0ef539f4b830d8ed19b
dkurepa <dkurepa@microsoft.com>
2025-04-07 18:48:51 +0200

    Move to correct folder, add test

--- tools/VersionPropsFormatter/VersionPropsFormatter.cs
+++ tools/VersionPropsFormatter/VersionPropsFormatter.cs
@@ -2,20 +2,24 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Xml;
+using Maestro.Common;
+using Microsoft.DotNet.DarcLib;
 using Microsoft.DotNet.DarcLib.Helpers;
 using Microsoft.DotNet.DarcLib.Models;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace VersionPropsFormatter;
-internal class VersionPropsFormatter(
+
+public class VersionPropsFormatter(
     IVersionDetailsParser versionDetailsParser,
     IDependencyFileManager dependencyFileManager,
     IProcessManager processManager,
     ILogger<VersionPropsFormatter> logger)
 {
-    public async Task RunAsync()
+    public async Task RunAsync(string path)
     {
-        NativePath repoPath = new(processManager.FindGitRoot(Directory.GetCurrentDirectory()));
+        NativePath repoPath = new(processManager.FindGitRoot(path));
         VersionDetails versionDetails = versionDetailsParser.ParseVersionDetailsFile(repoPath / VersionFiles.VersionDetailsXml, includePinned: true);
         var versionDetailsLookup = versionDetails.Dependencies.ToLookup(dep => dep.RepoUri, dep => dep);
 
@@ -83,4 +87,20 @@ internal class VersionPropsFormatter(
 
         logger.LogInformation(stringWriter.ToString());
     }
+
+    public static IServiceCollection RegisterServices(IServiceCollection services)
+    {
+        services.AddLogging(options => options.AddConsole());
+        services.AddSingleton<ILogger>(sp => sp.GetRequiredService<ILogger<IProcessManager>>());
+        services.AddSingleton<ITelemetryRecorder, NoTelemetryRecorder>();
+        services.AddSingleton<IRemoteTokenProvider>(new RemoteTokenProvider());
+        services.AddSingleton<IFileSystem, FileSystem>();
+        services.AddSingleton<IGitRepo, LocalLibGit2Client>();
+        services.AddSingleton<IDependencyFileManager, DependencyFileManager>();
+
+        services.AddSingleton<IProcessManager>(sp => ActivatorUtilities.CreateInstance<ProcessManager>(sp, "git"));
+        services.AddSingleton<IVersionDetailsParser, VersionDetailsParser>();
+        return services;
+    }
+
 }
