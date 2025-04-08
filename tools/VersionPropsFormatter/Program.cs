95e2aacb70c96d3d201aa6a7b0c84b89c30a7f85
dkurepa <dkurepa@microsoft.com>
2025-04-08 12:58:24 +0200

    use using for the service provider

--- tools/VersionPropsFormatter/Program.cs
+++ tools/VersionPropsFormatter/Program.cs
@@ -1,28 +1,13 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Microsoft.DotNet.DarcLib.Helpers;
 using Microsoft.Extensions.DependencyInjection;
 
 IServiceCollection serviceCollection = new ServiceCollection();
 
 VersionPropsFormatter.VersionPropsFormatter.RegisterServices(serviceCollection);
 
-IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+using var serviceProvider = serviceCollection.BuildServiceProvider();
 
-try
-{
-    await ActivatorUtilities.CreateInstance<VersionPropsFormatter.VersionPropsFormatter>(serviceProvider).RunAsync(Directory.GetCurrentDirectory());
-}
-finally
-{
-    // Ensure all logs are flushed by properly disposing the service provider
-    if (serviceProvider is IDisposable disposable)
-    {
-        disposable.Dispose();
-    }
-    else if (serviceProvider is IAsyncDisposable asyncDisposable)
-    {
-        await asyncDisposable.DisposeAsync();
-    }
-}
+await ActivatorUtilities.CreateInstance<VersionPropsFormatter.VersionPropsFormatter>(serviceProvider).RunAsync(Directory.GetCurrentDirectory());
+
