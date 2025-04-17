e0e6e557b7696c03305d4e3112f65952b19cfbf7
Premek Vysoky <premek.vysoky@microsoft.com>
2025-04-17 17:34:54 +0200

    Move Forward and Backward MergePolicy into one class

--- src/Maestro/Maestro.MergePolicies/CodeFlowMergePolicy.cs
+++ src/Maestro/Maestro.MergePolicies/CodeFlowMergePolicy.cs
@@ -1,32 +1,42 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Maestro.MergePolicyEvaluation;
+using Microsoft.DotNet.DarcLib;
 using Microsoft.DotNet.DarcLib.Helpers;
 using Microsoft.DotNet.DarcLib.VirtualMonoRepo;
+using static Maestro.MergePolicies.CodeFlowMergePolicyInterpreter;
 
 namespace Maestro.MergePolicies;
-internal abstract class CodeFlowMergePolicy : MergePolicy
+internal class CodeFlowMergePolicy : MergePolicy
 {
     public override string DisplayName => "Code flow verification";
 
-    protected static readonly string configurationErrorsHeader = """
-         ### :x: Check Failed
+    public override async Task<MergePolicyEvaluationResult> EvaluateAsync(PullRequestUpdateSummary pr, IRemote darc)
+    {
+        CodeFlowMergePolicyInterpreter interpreter = pr.CodeFlowDirection switch
+        {
+            CodeFlowDirection.BackFlow => new BackFlowMergePolicyInterpreter(),
+            CodeFlowDirection.ForwardFlow => new ForwardFlowMergePolicyInterpreter(),
+            _ => throw new ArgumentOutOfRangeException("PR is not a codeflow PR, can't evaluate CodeFlow Merge Policy"),
+        };
 
-         The following error(s) were encountered:
+        CodeFlowMergePolicyInterpreterResult result = await interpreter.InterpretAsync(pr, darc);
+        return result.IsSuccessful ?
+            Succeed(result.Title) :
+            Fail(result.Title, result.Message);
+    }
+}
 
+public class CodeFlowMergePolicyBuilder : IMergePolicyBuilder
+{
+    public string Name => MergePolicyConstants.CodeflowMergePolicyName;
 
-        """;
-
-    protected static readonly string SeekHelpMsg = $"""
-
-
-        ### :exclamation: IMPORTANT
-
-        The `{VmrInfo.DefaultRelativeSourceManifestPath}` and `{VersionFiles.VersionDetailsXml}` files are managed by Maestro/darc. Outside of exceptional circumstances, these files should not be modified manually.
-        **Unless you are sure that you know what you are doing, we recommend reaching out for help**. You can receive assistance by:
-        - tagging the **@dotnet/product-construction** team in a PR comment
-        - using the [First Responder channel](https://teams.microsoft.com/l/channel/19%3Aafba3d1545dd45d7b79f34c1821f6055%40thread.skype/First%20Responders?groupId=4d73664c-9f2f-450d-82a5-c2f02756606dhttps://teams.microsoft.com/l/channel/19%3Aafba3d1545dd45d7b79f34c1821f6055%40thread.skype/First%20Responders?groupId=4d73664c-9f2f-450d-82a5-c2f02756606d),
-        - [opening an issue](https://github.com/dotnet/arcade-services/issues/new?template=BLANK_ISSUE) in the dotnet/arcade-services repo
-        - contacting the [.NET Product Construction Services team via e-mail](mailto:dotnetprodconsvcs@microsoft.com).
-        """;
+    public Task<IReadOnlyList<IMergePolicy>> BuildMergePoliciesAsync(MergePolicyProperties properties, PullRequestUpdateSummary pr)
+    {
+        return Task.FromResult<IReadOnlyList<IMergePolicy>>([new CodeFlowMergePolicy()]);
+    }
 }
--- src/Maestro/Maestro.MergePolicies/MergePolicyServiceCollectionExtensions.cs
+++ src/Maestro/Maestro.MergePolicies/MergePolicyServiceCollectionExtensions.cs
@@ -14,8 +14,7 @@ public static class MergePolicyServiceCollectionExtensions
         services.AddTransient<IMergePolicyBuilder, DontAutomergeDowngradesMergePolicyBuilder>();
         services.AddTransient<IMergePolicyBuilder, StandardMergePolicyBuilder>();
         services.AddTransient<IMergePolicyBuilder, ValidateCoherencyMergePolicyBuilder>();
-        services.AddTransient<IMergePolicyBuilder, BackFlowMergePolicyBuilder>();
-        services.AddTransient<IMergePolicyBuilder, ForwardFlowMergePolicyBuilder>();
+        services.AddTransient<IMergePolicyBuilder, CodeFlowMergePolicyBuilder>();
         return services;
     }
 }
