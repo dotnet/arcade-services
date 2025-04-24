24ed8f166c673d9a3047a964fcf0d313c19762ca
Přemek Vysoký <premek.vysoky@microsoft.com>
2025-04-24 18:03:31 +0200

    Prefer the `az cli` credential in CI deployments (#4734)
    We started seeing failures in the deployments because some other
    identity is being picked up suddenly.
    
    Test build:
    https://dev.azure.com/dnceng/internal/_build/results?buildId=2694710&view=results

--- tools/ProductConstructionService.Cli/Options/DeploymentOptions.cs
+++ tools/ProductConstructionService.Cli/Options/DeploymentOptions.cs
@@ -59,9 +59,11 @@ internal class DeploymentOptions : Options
     {
         services.AddTransient<IProcessManager>(sp => new ProcessManager(sp.GetRequiredService<ILogger<ProcessManager>>(), "git"));
 
-        DefaultAzureCredential credential = new();
+        var credential = new ChainedTokenCredential(
+            new AzureCliCredential(),
+            new DefaultAzureCredential());
         services.AddSingleton(credential);
-        services.AddTransient<ArmClient>(sp => new(sp.GetRequiredService<DefaultAzureCredential>()));
+        services.AddTransient<ArmClient>(_ => new(credential));
         services.AddTransient<ResourceGroupResource>(sp =>
         {
             return new ArmClient(credential)
