dotnet/arcade-services

commit fbc466ca3d28f065905e53f3dd88bd61be965ea6

Inject services instead of passing them as method parameters (#4756)
This repository was ridden with places where we were passing
dependencies as arguments. We have mostly removed that but yesterday
during the bug squashing I found few more places where it's then harder
to use the classes so improving that.

src/Microsoft.DotNet.Darc/Darc/Options/CommandLineOptions.cs
@@ -150,6 +150,7 @@ public abstract class CommandLineOptions : ICommandLineOptions
             disableInteractiveAuth: IsCi,
             BuildAssetRegistryBaseUri));
         services.TryAddSingleton<IBasicBarClient>(sp => sp.GetRequiredService<IBarApiClient>());
+        services.TryAddTransient<ICoherencyUpdateResolver, CoherencyUpdateResolver>();
         services.TryAddTransient<ILogger>(sp => sp.GetRequiredService<ILogger<Operation>>());
         services.TryAddTransient<ITelemetryRecorder, NoTelemetryRecorder>();
         services.TryAddTransient<IGitRepoFactory>(sp => ActivatorUtilities.CreateInstance<GitRepoFactory>(sp, Path.GetTempPath()));

src/Maestro/Maestro.DataProviders/RemoteFactory.cs
@@ -7,16 +7,15 @@ using Maestro.Common.AzureDevOpsTokens;
 using Maestro.Data;
 using Microsoft.DotNet.DarcLib;
 using Microsoft.DotNet.DarcLib.Helpers;
-using Microsoft.DotNet.DarcLib.VirtualMonoRepo;
 using Microsoft.DotNet.GitHub.Authentication;
 using Microsoft.DotNet.Internal.Logging;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Maestro.DataProviders;
 
 public class RemoteFactory : IRemoteFactory
 {
-    private readonly IVersionDetailsParser _versionDetailsParser;
     private readonly OperationManager _operations;
     private readonly IProcessManager _processManager;
     private readonly ILoggerFactory _loggerFactory;
@@ -24,7 +23,7 @@ public class RemoteFactory : IRemoteFactory
     private readonly DarcRemoteMemoryCache _cache;
     private readonly IGitHubTokenProvider _gitHubTokenProvider;
     private readonly IAzureDevOpsTokenProvider _azdoTokenProvider;
-    private readonly ISourceMappingParser _sourceMappingParser;
+    private readonly IServiceProvider _serviceProvider;
 
     public RemoteFactory(
         BuildAssetRegistryContext context,
@@ -35,17 +34,16 @@ public class RemoteFactory : IRemoteFactory
         OperationManager operations,
         IProcessManager processManager,
         ILoggerFactory loggerFactory,
-        ISourceMappingParser sourceMappingParser)
+        IServiceProvider serviceProvider)
     {
         _operations = operations;
         _processManager = processManager;
         _loggerFactory = loggerFactory;
-        _versionDetailsParser = versionDetailsParser;
         _context = context;
         _gitHubTokenProvider = gitHubTokenProvider;
         _azdoTokenProvider = azdoTokenProvider;
         _cache = memoryCache;
-        _sourceMappingParser = sourceMappingParser;
+        _serviceProvider = serviceProvider;
     }
 
     public async Task<IRemote> CreateRemoteAsync(string repoUrl)
@@ -53,7 +51,7 @@ public class RemoteFactory : IRemoteFactory
         using (_operations.BeginOperation($"Getting remote for repo {repoUrl}."))
         {
             IRemoteGitRepo remoteGitClient = await GetRemoteGitClient(repoUrl);
-            return new Remote(remoteGitClient, _versionDetailsParser, _sourceMappingParser, _loggerFactory.CreateLogger<IRemote>());
+            return ActivatorUtilities.CreateInstance<Remote>(_serviceProvider, remoteGitClient);
         }
     }
 
@@ -62,7 +60,7 @@ public class RemoteFactory : IRemoteFactory
         using (_operations.BeginOperation($"Getting remote file manager for repo {repoUrl}."))
         {
             IRemoteGitRepo remoteGitClient = await GetRemoteGitClient(repoUrl);
-            return new DependencyFileManager(remoteGitClient, _versionDetailsParser, _loggerFactory.CreateLogger<IRemote>());
+            return ActivatorUtilities.CreateInstance<DependencyFileManager>(_serviceProvider, remoteGitClient);
         }
     }
 

src/Microsoft.DotNet.Darc/Darc/Operations/UpdateDependenciesOperation.cs
@@ -26,12 +26,14 @@ internal class UpdateDependenciesOperation : Operation
     private readonly IBarApiClient _barClient;
     private readonly IRemoteFactory _remoteFactory;
     private readonly IGitRepoFactory _gitRepoFactory;
+    private readonly ICoherencyUpdateResolver _coherencyUpdateResolver;
 
     public UpdateDependenciesOperation(
         UpdateDependenciesCommandLineOptions options,
         IBarApiClient barClient,
         IRemoteFactory remoteFactory,
         IGitRepoFactory gitRepoFactory,
+        ICoherencyUpdateResolver coherencyUpdateResolver,
         ILogger<UpdateDependenciesOperation> logger)
     {
         _options = options;
@@ -39,6 +41,7 @@ internal class UpdateDependenciesOperation : Operation
         _barClient = barClient;
         _remoteFactory = remoteFactory;
         _gitRepoFactory = gitRepoFactory;
+        _coherencyUpdateResolver = coherencyUpdateResolver;
     }
 
     /// <summary>
@@ -50,8 +53,6 @@ internal class UpdateDependenciesOperation : Operation
     {
         try
         {
-            var coherencyUpdateResolver = new CoherencyUpdateResolver(_barClient, _logger);
-
             var local = new Local(_options.GetRemoteTokenProvider(), _logger);
             List<DependencyDetail> dependenciesToUpdate = [];
             bool someUpToDate = false;
@@ -113,7 +114,7 @@ internal class UpdateDependenciesOperation : Operation
                         Console.WriteLine($"Looking up build with BAR id {_options.BARBuildId}");
                         var specificBuild = await _barClient.GetBuildAsync(_options.BARBuildId);
 
-                        int nonCoherencyResult = NonCoherencyUpdatesForBuild(specificBuild, coherencyUpdateResolver, currentDependencies, candidateDependenciesForUpdate, dependenciesToUpdate);
+                        int nonCoherencyResult = NonCoherencyUpdatesForBuild(specificBuild, currentDependencies, candidateDependenciesForUpdate, dependenciesToUpdate);
                         if (nonCoherencyResult != Constants.SuccessCode)
                         {
                             _logger.LogError("Failed to update non-coherent parent tied dependencies.");
@@ -187,7 +188,7 @@ internal class UpdateDependenciesOperation : Operation
                         continue;
                     }
 
-                    int nonCoherencyResult = NonCoherencyUpdatesForBuild(build, coherencyUpdateResolver, currentDependencies, candidateDependenciesForUpdate, dependenciesToUpdate);
+                    int nonCoherencyResult = NonCoherencyUpdatesForBuild(build, currentDependencies, candidateDependenciesForUpdate, dependenciesToUpdate);
                     if (nonCoherencyResult != Constants.SuccessCode)
                     {
                         _logger.LogError("Failed to update non-coherent parent tied dependencies.");
@@ -196,7 +197,7 @@ internal class UpdateDependenciesOperation : Operation
                 }
             }
 
-            int coherencyResult = await CoherencyUpdatesAsync(coherencyUpdateResolver, _remoteFactory, currentDependencies, dependenciesToUpdate)
+            int coherencyResult = await CoherencyUpdatesAsync(currentDependencies, dependenciesToUpdate)
                         .ConfigureAwait(false);
             if (coherencyResult != Constants.SuccessCode)
             {
@@ -250,9 +251,8 @@ internal class UpdateDependenciesOperation : Operation
         }
     }
 
-    private static int NonCoherencyUpdatesForBuild(
+    private int NonCoherencyUpdatesForBuild(
         Build build,
-        ICoherencyUpdateResolver updateResolver,
         List<DependencyDetail> currentDependencies,
         List<DependencyDetail> candidateDependenciesForUpdate,
         List<DependencyDetail> dependenciesToUpdate)
@@ -265,7 +265,7 @@ internal class UpdateDependenciesOperation : Operation
             });
 
         // Now determine what needs to be updated.
-        List<DependencyUpdate> updates = updateResolver.GetRequiredNonCoherencyUpdates(
+        List<DependencyUpdate> updates = _coherencyUpdateResolver.GetRequiredNonCoherencyUpdates(
             build.GetRepository(),
             build.Commit,
             assetData,
@@ -292,8 +292,6 @@ internal class UpdateDependenciesOperation : Operation
     }
 
     private async Task<int> CoherencyUpdatesAsync(
-        ICoherencyUpdateResolver updateResolver,
-        IRemoteFactory remoteFactory,
         List<DependencyDetail> currentDependencies,
         List<DependencyDetail> dependenciesToUpdate)
     {
@@ -303,7 +301,7 @@ internal class UpdateDependenciesOperation : Operation
         try
         {
             // Now run a coherency update based on the current set of dependencies updated from the previous pass.
-            coherencyUpdates = await updateResolver.GetRequiredCoherencyUpdatesAsync(currentDependencies, remoteFactory);
+            coherencyUpdates = await _coherencyUpdateResolver.GetRequiredCoherencyUpdatesAsync(currentDependencies);
         }
         catch (DarcCoherencyException e)
         {

src/Microsoft.DotNet.Darc/DarcLib/CoherencyUpdateResolver.cs
@@ -14,20 +14,21 @@ namespace Microsoft.DotNet.DarcLib;
 public class CoherencyUpdateResolver : ICoherencyUpdateResolver
 {
     private readonly IBasicBarClient _barClient;
+    private readonly IRemoteFactory _remoteFactory;
     private readonly ILogger _logger;
 
     public CoherencyUpdateResolver(
         IBasicBarClient barClient,
+        IRemoteFactory remoteFactory,
         ILogger logger)
     {
         _barClient = barClient;
+        _remoteFactory = remoteFactory;
         _logger = logger;
     }
 
-    public async Task<List<DependencyUpdate>> GetRequiredCoherencyUpdatesAsync(
-        IEnumerable<DependencyDetail> dependencies,
-        IRemoteFactory remoteFactory)
-        => await GetRequiredStrictCoherencyUpdatesAsync(dependencies, remoteFactory);
+    public async Task<List<DependencyUpdate>> GetRequiredCoherencyUpdatesAsync(IEnumerable<DependencyDetail> dependencies)
+        => await GetRequiredStrictCoherencyUpdatesAsync(dependencies);
 
     public List<DependencyUpdate> GetRequiredNonCoherencyUpdates(
         string sourceRepoUri,
@@ -97,7 +98,6 @@ public class CoherencyUpdateResolver : ICoherencyUpdateResolver
     ///     Get updates required by coherency constraints using the "strict" algorithm
     /// </summary>
     /// <param name="dependencies">Current set of dependencies.</param>
-    /// <param name="remoteFactory">Remote factory for remote queries.</param>
     /// <returns>Dependencies with updates.</returns>
     /// <remarks>
     ///     'Strict' coherency is a version of coherency that does not **require** any build information,
@@ -119,9 +119,7 @@ public class CoherencyUpdateResolver : ICoherencyUpdateResolver
     ///     but this is fairly minimal and generally covered by the need to have dependencies explicit in the
     ///     version details files anyway.
     /// </remarks>
-    private async Task<List<DependencyUpdate>> GetRequiredStrictCoherencyUpdatesAsync(
-        IEnumerable<DependencyDetail> dependencies,
-        IRemoteFactory remoteFactory)
+    private async Task<List<DependencyUpdate>> GetRequiredStrictCoherencyUpdatesAsync(IEnumerable<DependencyDetail> dependencies)
     {
         List<DependencyUpdate> toUpdate = [];
         IEnumerable<DependencyDetail> leavesOfCoherencyTrees = CalculateLeavesOfCoherencyTrees(dependencies);
@@ -189,7 +187,7 @@ public class CoherencyUpdateResolver : ICoherencyUpdateResolver
                 if (!dependenciesCache.TryGetValue(parentCoherentDependencyCacheKey,
                         out IEnumerable<DependencyDetail> coherentParentsDependencies))
                 {
-                    IRemote remoteClient = await remoteFactory.CreateRemoteAsync(parentCoherentDependency.RepoUri);
+                    IRemote remoteClient = await _remoteFactory.CreateRemoteAsync(parentCoherentDependency.RepoUri);
                     coherentParentsDependencies = await remoteClient.GetDependenciesAsync(
                         parentCoherentDependency.RepoUri,
                         parentCoherentDependency.Commit);
@@ -235,7 +233,7 @@ public class CoherencyUpdateResolver : ICoherencyUpdateResolver
                 _logger.LogInformation($"Dependency {dependencyToUpdate.Name} will be updated to " +
                                        $"{cpdDependency.Version} from {cpdDependency.RepoUri}@{cpdDependency.Commit}.");
 
-                Asset coherentAsset = await DisambiguateAssetsAsync(remoteFactory, buildCache, nugetConfigCache,
+                Asset coherentAsset = await DisambiguateAssetsAsync(buildCache, nugetConfigCache,
                     parentCoherentDependency, cpdDependency);
 
                 var updatedDependency = new DependencyDetail(dependencyToUpdate)
@@ -314,15 +312,16 @@ public class CoherencyUpdateResolver : ICoherencyUpdateResolver
     /// Disambiguate a set of potential assets based the nuget config
     /// file in a repo. The asset's locations are returned if a match is found.
     /// </summary>
-    /// <param name="remoteFactory">Remote factory for looking up the nuget config.</param>
     /// <param name="buildCache">Cache of builds</param>
     /// <param name="nugetConfigCache">Cache of nuget config files</param>
     /// <param name="parentCoherentDependency">Parent dependency of <paramref name="cpdDependency"/></param>
     /// <param name="cpdDependency">Dependency to disambiguate on.</param>
     /// <returns>Asset if a match to nuget.config is found. Asset from newest build is returned </returns>
-    private async Task<Asset> DisambiguateAssetsAsync(IRemoteFactory remoteFactory,
-        Dictionary<string, List<Build>> buildCache, Dictionary<string, IEnumerable<string>> nugetConfigCache,
-        DependencyDetail parentCoherentDependency, DependencyDetail cpdDependency)
+    private async Task<Asset> DisambiguateAssetsAsync(
+        Dictionary<string, List<Build>> buildCache,
+        Dictionary<string, IEnumerable<string>> nugetConfigCache,
+        DependencyDetail parentCoherentDependency,
+        DependencyDetail cpdDependency)
     {
         var parentCoherentDependencyCacheKey = $"{parentCoherentDependency.RepoUri}@{parentCoherentDependency.Commit}";
 
@@ -389,7 +388,7 @@ public class CoherencyUpdateResolver : ICoherencyUpdateResolver
             // coherent asset itself.
             if (!nugetConfigCache.TryGetValue(parentCoherentDependencyCacheKey, out IEnumerable<string> nugetFeeds))
             {
-                IRemote remoteClient = await remoteFactory.CreateRemoteAsync(parentCoherentDependency.RepoUri);
+                IRemote remoteClient = await _remoteFactory.CreateRemoteAsync(parentCoherentDependency.RepoUri);
                 nugetFeeds = await remoteClient.GetPackageSourcesAsync(parentCoherentDependency.RepoUri, parentCoherentDependency.Commit);
             }
 

src/Microsoft.DotNet.Darc/DarcLib/ICoherencyUpdateResolver.cs
@@ -14,11 +14,8 @@ public interface ICoherencyUpdateResolver
     ///     Get updates required by coherency constraints.
     /// </summary>
     /// <param name="dependencies">Current set of dependencies.</param>
-    /// <param name="remoteFactory">Remote factory for remote queries.</param>
     /// <returns>List of dependency updates.</returns>
-    Task<List<DependencyUpdate>> GetRequiredCoherencyUpdatesAsync(
-        IEnumerable<DependencyDetail> dependencies,
-        IRemoteFactory remoteFactory);
+    Task<List<DependencyUpdate>> GetRequiredCoherencyUpdatesAsync(IEnumerable<DependencyDetail> dependencies);
 
     /// <summary>
     ///     Given a current set of dependencies, determine what non-coherency updates

src/Microsoft.DotNet.Darc/DarcLib/IRemote.cs
@@ -127,15 +127,11 @@ public interface IRemote
     /// </summary>
     /// <param name="repoUri">Repository to update</param>
     /// <param name="branch">Branch of <paramref name="repoUri"/> to update.</param>
-    /// <param name="remoteFactory">Remote factory for obtaining common script files from arcade</param>
-    /// <param name="barClientFactory">Factory for getting clients that can query build and asset information</param>
     /// <param name="itemsToUpdate">Dependencies that need updating.</param>
     /// <param name="message">Commit message.</param>
     Task<List<GitFile>> CommitUpdatesAsync(
         string repoUri,
         string branch,
-        IRemoteFactory remoteFactory,
-        IBasicBarClient barClient,
         List<DependencyDetail> itemsToUpdate,
         string message);
 

src/Microsoft.DotNet.Darc/DarcLib/Remote.cs
@@ -24,6 +24,8 @@ public sealed class Remote : IRemote
     private readonly DependencyFileManager _fileManager;
     private readonly IRemoteGitRepo _remoteGitClient;
     private readonly ISourceMappingParser _sourceMappingParser;
+    private readonly IRemoteFactory _remoteFactory;
+    private readonly IAssetLocationResolver _locationResolver;
     private readonly ILogger _logger;
 
     //[DependencyUpdate]: <> (Begin)
@@ -38,12 +40,16 @@ public sealed class Remote : IRemote
         IRemoteGitRepo remoteGitClient,
         IVersionDetailsParser versionDetailsParser,
         ISourceMappingParser sourceMappingParser,
+        IRemoteFactory remoteFactory,
+        IAssetLocationResolver locationResolver,
         ILogger logger)
     {
         _logger = logger;
         _remoteGitClient = remoteGitClient;
         _versionDetailsParser = versionDetailsParser;
         _sourceMappingParser = sourceMappingParser;
+        _remoteFactory = remoteFactory;
+        _locationResolver = locationResolver;
         _fileManager = new DependencyFileManager(remoteGitClient, _versionDetailsParser, _logger);
     }
 
@@ -157,23 +163,19 @@ public sealed class Remote : IRemote
     /// </summary>
     /// <param name="repoUri">Repository to update</param>
     /// <param name="branch">Branch of <paramref name="repoUri"/> to update.</param>
-    /// <param name="remoteFactory">Remote factory for obtaining common script files from arcade</param>
     /// <param name="itemsToUpdate">Dependencies that need updating.</param>
     /// <param name="message">Commit message.</param>
     /// <returns>Async task.</returns>
     public async Task<List<GitFile>> CommitUpdatesAsync(
         string repoUri,
         string branch,
-        IRemoteFactory remoteFactory,
-        IBasicBarClient barClient,
         List<DependencyDetail> itemsToUpdate,
         string message)
     {
         CheckForValidGitClient();
 
         List<DependencyDetail> oldDependencies = [.. await GetDependenciesAsync(repoUri, branch)];
-        var locationResolver = new AssetLocationResolver(barClient);
-        await locationResolver.AddAssetLocationToDependenciesAsync(oldDependencies);
+        await _locationResolver.AddAssetLocationToDependenciesAsync(oldDependencies);
 
         // If we are updating the arcade sdk we need to update the eng/common files
         // and the sdk versions in global.json
@@ -186,7 +188,7 @@ public sealed class Remote : IRemote
 
         if (mayNeedArcadeUpdate)
         {
-            IDependencyFileManager arcadeFileManager = await remoteFactory.CreateDependencyFileManagerAsync(arcadeItem.RepoUri);
+            IDependencyFileManager arcadeFileManager = await _remoteFactory.CreateDependencyFileManagerAsync(arcadeItem.RepoUri);
             try
             {
                 targetDotNetVersion = await arcadeFileManager.ReadToolsDotnetVersionAsync(arcadeItem.RepoUri, arcadeItem.Commit, sourceRepoIsVmr);
@@ -213,7 +215,7 @@ public sealed class Remote : IRemote
         {
             // Files in the source arcade repo. We use the remote factory because the
             // arcade repo may be in github while this remote is targeted at AzDO.
-            IRemote arcadeRemote = await remoteFactory.CreateRemoteAsync(arcadeItem.RepoUri);
+            IRemote arcadeRemote = await _remoteFactory.CreateRemoteAsync(arcadeItem.RepoUri);
             List<GitFile> engCommonFiles = await arcadeRemote.GetCommonScriptFilesAsync(arcadeItem.RepoUri, arcadeItem.Commit, repoIsVmr: sourceRepoIsVmr);
             // If the engCommon files are coming from the VMR, we have to remove 'src/arcade/' from the file paths
             if (sourceRepoIsVmr)

src/ProductConstructionService/ProductConstructionService.DependencyFlow/PullRequestBuilder.cs
@@ -26,7 +26,6 @@ internal interface IPullRequestBuilder
     ///     A string writer that the PR description should be written to. If this an update
     ///     to an existing PR, this will contain the existing PR description.
     /// </param>
-    /// <param name="remoteFactory">Remote factory for generating remotes based on repo uri</param>
     /// <param name="targetRepository">Target repository that the updates should be applied to</param>
     /// <param name="newBranchName">Target branch the updates should be to</param>
     Task<string> CalculatePRDescriptionAndCommitUpdatesAsync(
@@ -38,7 +37,6 @@ internal interface IPullRequestBuilder
     /// <summary>
     ///     Compute the title for a pull request.
     /// </summary>
-    /// <param name="inProgressPr">Current in progress pull request information</param>
     /// <returns>Pull request title</returns>
     Task<string> GeneratePRTitleAsync(
         List<SubscriptionPullRequestUpdate> subscriptions,
@@ -173,8 +171,6 @@ internal class PullRequestBuilder : IPullRequestBuilder
             List<GitFile> committedFiles = await remote.CommitUpdatesAsync(
                 targetRepository,
                 newBranchName,
-                _remoteFactory,
-                _barClient,
                 itemsToUpdate,
                 message.ToString());
 
@@ -198,8 +194,6 @@ internal class PullRequestBuilder : IPullRequestBuilder
             await remote.CommitUpdatesAsync(
                 targetRepository,
                 newBranchName,
-                _remoteFactory,
-                _barClient,
                 itemsToUpdate,
                 message.ToString());
         }
@@ -209,7 +203,7 @@ internal class PullRequestBuilder : IPullRequestBuilder
         if (requiredUpdates.Count == 0)
         {
             var message = "Failed to perform coherency update for one or more dependencies.";
-            await remote.CommitUpdatesAsync(targetRepository, newBranchName, _remoteFactory, _barClient, [], message);
+            await remote.CommitUpdatesAsync(targetRepository, newBranchName, [], message);
             return $"Coherency update: {message} Please review the GitHub checks or run `darc update-dependencies --coherency-only` locally against {newBranchName} for more information.";
         }
 

commit 7c3ffc379b72c0061031ee3dfbff8c01aa19a92f

Add missing service registration for remote (#4780)
<!-- Link the GitHub or AzDO issue this pull request is associated with.
Please copy and paste the full URL rather than using the
dotnet/arcade-services# syntax -->

Fixes an issue with missing services for `IRemote` resolution in Darc
introduced in https://github.com/dotnet/arcade-services/pull/4756

<!-- https://github.com/dotnet/arcade-services/issues/4756 -->

src/Microsoft.DotNet.Darc/Darc/Options/CommandLineOptions.cs
@@ -143,6 +143,8 @@ public abstract class CommandLineOptions : ICommandLineOptions
 
         services.TryAddSingleton<IFileSystem, FileSystem>();
         services.TryAddSingleton<IRemoteFactory, RemoteFactory>();
+        services.TryAddSingleton<IVersionDetailsParser, VersionDetailsParser>();
+        services.TryAddSingleton<IAssetLocationResolver, AssetLocationResolver>();
         services.TryAddTransient<IProcessManager>(sp => new ProcessManager(sp.GetRequiredService<ILogger<ProcessManager>>(), GitLocation));
         services.TryAddSingleton<IBarApiClient>(sp => new BarApiClient(
             BuildAssetRegistryToken,