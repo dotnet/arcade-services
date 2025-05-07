7af7ce5a545b7d2b5d2e67b6e60380b2d6cf4422
Přemek Vysoký <premek.vysoky@microsoft.com>
2025-05-07 18:14:03 +0200

    Use `maestro-test-arcade` instead of `arcade` for Scenario tests (#4816)
    Fixes problems where we cannot use `maestro-auth-test/arcade` with
    `ReproTool`

--- test/ProductConstructionService.ScenarioTests/TestRepository.cs
+++ test/ProductConstructionService.ScenarioTests/TestRepository.cs
@@ -11,10 +11,11 @@ internal class TestRepository
     internal const string TestRepo3Name = "maestro-test3";
     internal const string VmrTestRepoName = "maestro-test-vmr";
     internal const string SourceBranch = "master";
+    internal const string TestArcadeName = "maestro-test-arcade";
 
     // This branch and commit data is special for the coherency test
     // It's required to make sure that the dependency tree is set up correctly in the repo without conflicting with other test cases
     internal const string CoherencyTestRepo1Commit = "cc1a27107a1f4c4bc5e2f796c5ef346f60abb404";
     internal const string CoherencyTestRepo2Commit = "8460158878d4b7568f55d27960d4453877523ea6";
-    internal const string ArcadeTestRepoCommit = "f3d51d2c9af2a3eb046fa54c5acdef9fb37db172";
+    internal const string ArcadeTestRepoCommit = "a702b2239b4dd238dd52c26a02d055cf4baff3a1";
 }
