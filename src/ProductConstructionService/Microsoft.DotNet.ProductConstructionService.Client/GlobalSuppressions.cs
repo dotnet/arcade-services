215f5b33f349087fdb15f3ca147f5bb05bca9bde
Premek Vysoky <premek.vysoky@microsoft.com>
2025-05-23 00:11:44 +0200

    Revert copilot stuff

--- src/ProductConstructionService/Microsoft.DotNet.ProductConstructionService.Client/GlobalSuppressions.cs
+++ src/ProductConstructionService/Microsoft.DotNet.ProductConstructionService.Client/GlobalSuppressions.cs
@@ -5,32 +5,3 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Style", "IDE0130:Namespace does not match folder structure", Justification = "Auto-generate code", Scope = "namespaceanddescendants", Target = "~N:Microsoft.DotNet.ProductConstructionService.Client")]
 [assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Auto-generate code", Scope = "namespaceanddescendants", Target = "~N:Microsoft.DotNet.ProductConstructionService.Client")]
-[assembly: SuppressMessage("Usage", "SYSLIB0051:Type or member is obsolete", 
-    Justification = "Suppressing Serialization obsolescence warnings which come from generated code", 
-    Scope = "member", 
-    Target = "~M:Microsoft.DotNet.ProductConstructionService.Client.RestApiException.#ctor(System.Runtime.Serialization.SerializationInfo,System.Runtime.Serialization.StreamingContext)")]
-    
-[assembly: SuppressMessage("Usage", "SYSLIB0051:Type or member is obsolete", 
-    Justification = "Suppressing Serialization obsolescence warnings which come from generated code", 
-    Scope = "member", 
-    Target = "~M:Microsoft.DotNet.ProductConstructionService.Client.RestApiException.GetObjectData(System.Runtime.Serialization.SerializationInfo,System.Runtime.Serialization.StreamingContext)")]
-    
-[assembly: SuppressMessage("Usage", "SYSLIB0051:Type or member is obsolete", 
-    Justification = "Suppressing Serialization obsolescence warnings which come from generated code", 
-    Scope = "member", 
-    Target = "~M:Microsoft.DotNet.ProductConstructionService.Client.RestApiException`1.#ctor(System.Runtime.Serialization.SerializationInfo,System.Runtime.Serialization.StreamingContext)")]
-    
-[assembly: SuppressMessage("Usage", "SYSLIB0051:Type or member is obsolete", 
-    Justification = "Suppressing Serialization obsolescence warnings which come from generated code", 
-    Scope = "member", 
-    Target = "~M:Microsoft.DotNet.ProductConstructionService.Client.RestApiException`1.GetObjectData(System.Runtime.Serialization.SerializationInfo,System.Runtime.Serialization.StreamingContext)")]
-
-[assembly: SuppressMessage("Usage", "CS0672:Member overrides obsolete member",
-    Justification = "Suppressing obsolete member warnings which come from generated code",
-    Scope = "member",
-    Target = "~M:Microsoft.DotNet.ProductConstructionService.Client.RestApiException.GetObjectData(System.Runtime.Serialization.SerializationInfo,System.Runtime.Serialization.StreamingContext)")]
-
-[assembly: SuppressMessage("Usage", "CS0672:Member overrides obsolete member",
-    Justification = "Suppressing obsolete member warnings which come from generated code",
-    Scope = "member",
-    Target = "~M:Microsoft.DotNet.ProductConstructionService.Client.RestApiException`1.GetObjectData(System.Runtime.Serialization.SerializationInfo,System.Runtime.Serialization.StreamingContext)")]
