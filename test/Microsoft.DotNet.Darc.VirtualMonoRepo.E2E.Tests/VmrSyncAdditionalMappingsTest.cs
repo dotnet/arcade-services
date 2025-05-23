24e569bf3b3ae3edb6d39fb6ea24317fef3dbb6e
Přemek Vysoký <premek.vysoky@microsoft.com>
2025-05-23 12:56:04 +0200

    Update test/Microsoft.DotNet.Darc.VirtualMonoRepo.E2E.Tests/VmrSyncAdditionalMappingsTest.cs

--- test/Microsoft.DotNet.Darc.VirtualMonoRepo.E2E.Tests/VmrSyncAdditionalMappingsTest.cs
+++ test/Microsoft.DotNet.Darc.VirtualMonoRepo.E2E.Tests/VmrSyncAdditionalMappingsTest.cs
@@ -38,6 +38,7 @@ internal class VmrSyncAdditionalMappingsTest : VmrTestsBase
             expectedFilesFromRepos
         );
 
+        // The git-info files are not created in this test so we should not expect them
         expectedFiles = [..expectedFiles.Where(f => !f.Path.Contains(new NativePath(VmrInfo.GitInfoSourcesDir)))];
 
         CheckDirectoryContents(VmrPath, expectedFiles);
