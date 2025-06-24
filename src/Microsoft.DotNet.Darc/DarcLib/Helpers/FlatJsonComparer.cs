2b23a473c72ff380ec02ee599f234f27d69f6757
Your Name <dkurepa@microsoft.com>
2025-06-24 11:26:05 +0200

    Add the converter

---
+++ src/Microsoft.DotNet.Darc/DarcLib/Helpers/FlatJsonComparer.cs
@@ -0,0 +1,61 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+#nullable enable
+namespace Microsoft.DotNet.DarcLib.Helpers;
+
+public static class FlatJsonComparer
+{
+    public static void CompareFlatJsons(
+        Dictionary<string, object> oldJson,
+        Dictionary<string, object> newJson)
+    {
+        List<FlatJsonComparerResultNode> changes = [];
+
+        foreach (var kvp in oldJson)
+        {
+            if (!newJson.TryGetValue(kvp.Key, out var newValue))
+            {
+                changes.Add(new FlatJsonComparerResultNode(kvp.Key, NodeComparisonResult.Removed, null));
+            }
+            else if (kvp.Value.GetType() != newValue.GetType())
+            {
+                throw new ArgumentException($"Key {kvp.Key} value has different types in old and new json");
+            }
+            else if (kvp.Value.GetType() == typeof(List<string>))
+            {
+                var oldList = (List<string>)kvp.Value;
+                var newList = (List<string>)newValue;
+                if (!oldList.SequenceEqual(newList))
+                {
+                    changes.Add(new FlatJsonComparerResultNode(kvp.Key, NodeComparisonResult.Updated, newValue));
+                }
+            }
+            else if (!kvp.Value.Equals(newValue))
+            {
+                changes.Add(new FlatJsonComparerResultNode(kvp.Key, NodeComparisonResult.Updated, newValue));
+            }
+        }
+
+        foreach (var kvp in newJson)
+        {
+            if (!oldJson.ContainsKey(kvp.Key))
+            {
+                changes.Add(new FlatJsonComparerResultNode(kvp.Key, NodeComparisonResult.Added, kvp.Value));
+            }
+        }
+    }
+}
+
+public record FlatJsonComparerResultNode(string Key, NodeComparisonResult Result, object? NewValue);
+
+public enum NodeComparisonResult
+{
+    Added,
+    Removed,
+    Updated
+}
---
+++ src/Microsoft.DotNet.Darc/DarcLib/Helpers/JsonFlattener.cs
@@ -0,0 +1,62 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+
+#nullable enable
+namespace Microsoft.DotNet.DarcLib.Helpers;
+public static class JsonFlattener
+{
+    public static Dictionary<string, object> FlattenJsonToDictionary(string json)
+    {
+        using var document = JsonDocument.Parse(json);
+        Dictionary<string, object> dictionary = [];
+        Stack<(JsonElement element, string path)> pathsToProcess = [];
+        pathsToProcess.Push((document.RootElement, string.Empty));
+
+        while (pathsToProcess.Count > 0)
+        {
+            (JsonElement currentElement, string currentPath) = pathsToProcess.Pop();
+
+            FlattenJsonElement(currentElement, currentPath, dictionary, pathsToProcess);
+        }
+
+        return dictionary;
+    }
+
+    /// <summary>
+    /// This method is used to flatten global.json and dotnet-tools.json files into a dictionary, which don't have a complex structure.
+    /// For example, we can expect that arrays won't have other arrays inside of them
+    /// </summary>
+    private static void FlattenJsonElement(JsonElement element, string path, Dictionary<string, object> dictionary, Stack<(JsonElement, string)> pathsToProcess)
+    {
+        switch (element.ValueKind)
+        {
+            case JsonValueKind.Object:
+                foreach(JsonProperty property in element.EnumerateObject())
+                {
+                    string newPath = string.IsNullOrEmpty(path) ? property.Name : $"{path}:{property.Name}";
+                    pathsToProcess.Push((property.Value, newPath));
+                }
+                break;
+
+            case JsonValueKind.Array:
+                dictionary[path] = element.EnumerateArray().Select(item => item.ToString()).ToList();
+                break;
+
+            case JsonValueKind.True:
+                dictionary[path] = true;
+                break;
+
+            case JsonValueKind.False:
+                dictionary[path] = false;
+                break;
+
+            default:
+                dictionary[path] = element.ToString();
+                break;
+        }
+    }
+}
