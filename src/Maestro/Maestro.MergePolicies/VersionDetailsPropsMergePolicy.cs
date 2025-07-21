f1c240e31bda96db0a2c57c8f40f69d88c58c8b1
Djuradj Kurepa <dkurepa@microsoft.com>
2025-07-21 09:17:38 +0200

    Create Merge Policy

---
+++ src/Maestro/Maestro.MergePolicies/VersionDetailsPropsMergePolicy.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Threading.Tasks;
+using Maestro.MergePolicyEvaluation;
+using Microsoft.DotNet.DarcLib;
+
+namespace Maestro.MergePolicies;
+internal class VersionDetailsPropsMergePolicy : MergePolicy
+{
+    public override string Name => "VersionDetailsProps";
+
+    public override string DisplayName => "Version Details Properties Merge Policy";
+
+    public override Task<MergePolicyEvaluationResult> EvaluateAsync(PullRequestUpdateSummary pr, IRemote darc) => throw new NotImplementedException();
+}
