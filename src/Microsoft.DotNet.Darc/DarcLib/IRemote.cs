4637540c10a0e53e3046b3b01218a227cd04dba8
Djuradj Kurepa <91743470+dkurepa@users.noreply.github.com>
2025-08-11 17:03:59 +0200

    Fix scenario tests (#5133)
    <!-- Link the GitHub or AzDO issue this pull request is associated with.
    Please copy and paste the full URL rather than using the
    dotnet/arcade-services# syntax -->
    https://github.com/dotnet/arcade-services/issues/4792

--- src/Microsoft.DotNet.Darc/DarcLib/IRemote.cs
+++ src/Microsoft.DotNet.Darc/DarcLib/IRemote.cs
@@ -105,7 +105,7 @@ public interface IRemote
     /// <param name="repoUri">URI of repo containing script files.</param>
     /// <param name="commit">Common to get script files at.</param>
     /// <returns>Script files.</returns>
-    Task<List<GitFile>> GetCommonScriptFilesAsync(string repoUri, string commit, string mapping = null);
+    Task<List<GitFile>> GetCommonScriptFilesAsync(string repoUri, string commit, LocalPath relativeBasePath = null);
 
     /// <summary>
     ///     Create a new branch in the specified repository.
