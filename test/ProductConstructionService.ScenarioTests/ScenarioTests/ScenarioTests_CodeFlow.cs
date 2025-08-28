16b6f1ad1d3af9135cf5aa6911a17757185cc5f6
Djuradj Kurepa <dkurepa@microsoft.com>
2025-08-28 12:29:11 +0200

    Remove unused using

--- test/ProductConstructionService.ScenarioTests/ScenarioTests/ScenarioTests_CodeFlow.cs
+++ test/ProductConstructionService.ScenarioTests/ScenarioTests/ScenarioTests_CodeFlow.cs
@@ -6,7 +6,6 @@ using Microsoft.DotNet.DarcLib.Models.Darc;
 using NUnit.Framework;
 using Microsoft.DotNet.DarcLib.Helpers;
 using Microsoft.DotNet.DarcLib.VirtualMonoRepo;
-using FluentAssertions;
 
 #nullable enable
 namespace ProductConstructionService.ScenarioTests;
