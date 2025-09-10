33885109b9e6a8c251f4bd6fce913045893b4e49
Djuradj Kurepa <dkurepa@microsoft.com>
2025-09-10 15:05:14 +0200

    Remove asset matcher changes since they're not needed anymore

--- src/Microsoft.DotNet.Darc/DarcLib/Models/Darc/AssetMatcher.cs
+++ src/Microsoft.DotNet.Darc/DarcLib/Models/Darc/AssetMatcher.cs
@@ -1,9 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using System.Collections.Generic;
-using Microsoft.DotNet.DarcLib.Helpers;
 using Microsoft.Extensions.FileSystemGlobbing;
 
 namespace Microsoft.DotNet.DarcLib.Models.Darc;
@@ -25,78 +23,6 @@ public static class AssetFilterExtensions
         var matcher = new Matcher();
         matcher.AddIncludePatterns(filters);
         return new AssetMatcher(matcher);
-    }
-
-    /// <summary>
-    /// Creates asset matchers for each target directory based on applicable filters.
-    /// </summary>
-    public static Dictionary<UnixPath, IAssetMatcher> GetAssetMatchersPerDirectory(this IReadOnlyCollection<string> filters, List<UnixPath> targetDirectories)
-    {
-        Dictionary<UnixPath, IAssetMatcher> assetMatcherDictionary = [];
-        foreach (var dir in targetDirectories)
-        {
-            var directoryFilters = GetFiltersForDirectory(filters, dir);
-
-            if (directoryFilters == null || directoryFilters.Count == 0)
-            {
-                assetMatcherDictionary[dir] = new AssetMatcher(null);
-            }
-            else
-            {
-                var matcher = new Matcher();
-                matcher.AddIncludePatterns(directoryFilters);
-                assetMatcherDictionary[dir] = new AssetMatcher(matcher);
-            }
-        }
-
-        return assetMatcherDictionary;
-    }
-
-    private static IReadOnlyCollection<string> GetFiltersForDirectory(this IReadOnlyCollection<string> filters, string directory)
-    {
-        List<string> dirFilters = [];
-        
-        // Normalize the directory - treat "." as root directory
-        string normalizedDirectory = directory == "." ? string.Empty : directory;
-        
-        foreach (var filter in filters)
-        {
-            var lastSlash = filter.LastIndexOf('/');
-
-            if (lastSlash == -1)
-            {
-                // No slash in filter - this is a root-level pattern
-                if (string.IsNullOrEmpty(normalizedDirectory))
-                {
-                    dirFilters.Add(filter);
-                }
-                continue;
-            }
-
-            var filterPath = filter.Substring(0, lastSlash);
-            var assetPattern = filter.Substring(lastSlash + 1);
-            // If filterPath is "*" or "**", it applies to all directories
-            if (filterPath == "*" || filterPath == "**")
-            {
-                dirFilters.Add(assetPattern);
-                continue;
-            }
-            if (string.IsNullOrEmpty(normalizedDirectory))
-            {
-                continue;
-            }
-            // Use Matcher to check if the directory matches the filter path pattern
-            var pathMatcher = new Matcher();
-            pathMatcher.AddInclude(filterPath);
-
-            // Check if this filter applies to the current directory using glob matching
-            if (pathMatcher.Match(normalizedDirectory).HasMatches)
-            {
-                dirFilters.Add(assetPattern);
-            }
-        }
-        
-        return dirFilters;
     }
 }
 
