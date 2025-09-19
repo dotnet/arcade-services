9789f1bee53a7dc1f04488b5b9384aec7d66463d
Premek Vysoky <premek.vysoky@microsoft.com>
2025-09-19 14:48:31 +0200

    Do not serialize empty classification

--- src/Microsoft.DotNet.Darc/DarcLib/Models/Darc/Yaml/ChannelYamlData.cs
+++ src/Microsoft.DotNet.Darc/DarcLib/Models/Darc/Yaml/ChannelYamlData.cs
@@ -1,10 +1,14 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using YamlDotNet.Serialization;
+
 namespace Microsoft.DotNet.DarcLib.Models.Darc.Yaml;
 
 public class ChannelYamlData
 {
     public string Name { get; set; }
+
+    [YamlMember(DefaultValuesHandling = DefaultValuesHandling.OmitNull | DefaultValuesHandling.OmitDefaults)]
     public string Classification { get; set; }
 }
