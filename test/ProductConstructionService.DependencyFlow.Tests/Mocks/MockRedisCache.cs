b08fd4f117f5b9704273354f0e7929d628380d64
Premek Vysoky <premek.vysoky@microsoft.com>
2025-10-01 13:17:50 +0200

    Move codeflow test mocks to a namespace

--- test/ProductConstructionService.DependencyFlow.Tests/Mocks/MockRedisCache.cs
+++ test/ProductConstructionService.DependencyFlow.Tests/Mocks/MockRedisCache.cs
@@ -1,9 +1,13 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 using ProductConstructionService.Common;
 
-namespace ProductConstructionService.DependencyFlow.Tests;
+namespace ProductConstructionService.DependencyFlow.Tests.Mocks;
 
 internal class MockRedisCache : IRedisCache
 {
