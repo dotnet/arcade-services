4ea040e8cbe8941fdc6069d382bd4a743083f387
Premek Vysoky <premek.vysoky@microsoft.com>
2025-10-02 12:43:39 +0200

    Fix branch preparation

--- src/Microsoft.DotNet.Darc/DarcLib/VirtualMonoRepo/VmrForwardFlower.cs
+++ src/Microsoft.DotNet.Darc/DarcLib/VirtualMonoRepo/VmrForwardFlower.cs
@@ -326,13 +326,14 @@ public class VmrForwardFlower : VmrCodeFlower, IVmrForwardFlower
         bool rebase,
         CancellationToken cancellationToken)
     {
-        // When updating an existing PR, we create a work branch to make the changes on
         var vmr = _localGitRepoFactory.Create(_vmrInfo.VmrPath);
 
         // If a PR branch exists, check out the last flow's commit in the PR branch
-        // If a PR branch does not exist, check out the last flow's commit in the target branch
-        await vmr.CheckoutAsync(headBranchExisted ? lastFlows.LastForwardFlow.VmrSha : lastFlows.LastFlow.VmrSha);
-        await _dependencyTracker.RefreshMetadataAsync();
+        if (headBranchExisted)
+        {
+            await vmr.CheckoutAsync(lastFlows.LastForwardFlow.VmrSha);
+            await _dependencyTracker.RefreshMetadataAsync();
+        }
 
         IWorkBranch? workBranch = headBranchExisted || rebase
             ? await _workBranchFactory.CreateWorkBranchAsync(vmr, currentFlow.GetBranchName(), headBranch)
