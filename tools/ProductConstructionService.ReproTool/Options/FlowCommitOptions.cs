0f1c878868ea3660b00a22bfe556debfd39b2785
Adam <adamzippor@microsoft.com>
2025-10-10 13:36:48 +0200

    fix source/target directory + add source-commit option (#5334)
    https://github.com/dotnet/arcade-services/issues/5335

--- tools/ProductConstructionService.ReproTool/Options/FlowCommitOptions.cs
+++ tools/ProductConstructionService.ReproTool/Options/FlowCommitOptions.cs
@@ -16,8 +16,11 @@ internal class FlowCommitOptions : Options
     [Option("source-repo", HelpText = "Repo (full URI) to flow the commit from (must be under maestro-auth-test/)", Required = true)]
     public required string SourceRepository { get; init; }
 
-    [Option("source-branch", HelpText = "Branch whose commit will be flown", Required = true)]
-    public required string SourceBranch { get; init; }
+    [Option("source-branch", HelpText = "Either --source-branch or --source-commit is required. Branch whose commit will be flown. Ignored if source-commit is provided.", Required = false)]
+    public string? SourceBranch { get; init; }
+
+    [Option("source-commit", HelpText = "Either --source-branch or --source-commit is required. Source commit that will be flown.", Required = false)]
+    public string? SourceCommit { get; init; }
 
     [Option("target-repo", HelpText = "Repo (full URI) to flow the commit to (must be under maestro-auth-test/)", Required = true)]
     public required string TargetRepository { get; init; }
@@ -28,7 +31,7 @@ internal class FlowCommitOptions : Options
     [Option("packages", HelpText = "Name(s) of package(s) to include in the flown build", Required = false)]
     public IEnumerable<string> Packages { get; init; } = [];
 
-    [Option("realBuildId", HelpText = "Build to take assets from. Shouldn't be combined with packages", Required = false)]
+    [Option("assets-from-build", HelpText = "A real build id from which to take assets. Shouldn't be provided if using the --packages option", Required = false)]
     public int RealBuildId { get; init; }
 
     internal override Operation GetOperation(IServiceProvider sp)
