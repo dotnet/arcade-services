34d205150f623dfb915e7f8346c1071c309be5ac
Djuradj Kurepa <dkurepa@microsoft.com>
2025-10-10 16:58:13 +0200

    pls let this work

--- src/ProductConstructionService/ProductConstructionService.DependencyFlow/PullRequestBuilder.cs
+++ src/ProductConstructionService/ProductConstructionService.DependencyFlow/PullRequestBuilder.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Text;
@@ -1020,3 +1020,4 @@ internal class PullRequestBuilder : IPullRequestBuilder
         return null;
     }
 }
+
