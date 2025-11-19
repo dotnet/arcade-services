cd0326c176070f2888f7b31304bbddeb7f8a1aaa
Premek Vysoky <premek.vysoky@microsoft.com>
2025-11-19 12:22:21 +0100

    Add backflow tests

--- test/Microsoft.DotNet.DarcLib.Codeflow.Tests/BackflowTests.cs
+++ test/Microsoft.DotNet.DarcLib.Codeflow.Tests/BackflowTests.cs
@@ -480,6 +480,20 @@ internal class BackflowTests : CodeFlowTests
         // Now we make several changes in the VMR and try to locally flow them via darc
         await File.WriteAllTextAsync(_productRepoVmrFilePath, "New content in the VMR again");
         await File.WriteAllTextAsync(_productRepoVmrFilePath + "-added-in-repo", "New file from the VMR");
+
+        // Update version files
+        var newDependency = new DependencyDetail
+        {
+            Name = "Package.New",
+            Version = "2.0.0",
+            RepoUri = "https://github.com/some/repo",
+            Commit = "commit-sha",
+            Type = DependencyType.Toolset,
+        };
+        await GetLocal(VmrPath).AddDependencyAsync(
+            newDependency,
+            relativeBasePath: VmrInfo.GetRelativeRepoSourcesPath(Constants.ProductRepoName));
+
         await GitOperations.CommitAll(VmrPath, "New content in the VMR again");
 
         string[] stagedFiles = await CallDarcBackflow();
@@ -490,6 +504,7 @@ internal class BackflowTests : CodeFlowTests
             _productRepoFileName,
             _productRepoFileName + "-added-in-repo",
             VersionFiles.VersionDetailsXml,
+            VersionFiles.VersionDetailsProps,
         ];
 
         // We check if everything got staged properly
@@ -511,9 +526,10 @@ internal class BackflowTests : CodeFlowTests
         File.Copy(
             ProductRepoPath / DarcLib.Constants.CommonScriptFilesPath / "build.ps1",
             VmrPath / VmrInfo.GetRelativeRepoSourcesPath("arcade") / DarcLib.Constants.CommonScriptFilesPath / "build.ps2");
+
         await GitOperations.CommitAll(VmrPath, "Changing VMR's eng/common");
 
-        ProductConstructionService.Client.Models.Build build = await CreateNewVmrBuild(
+        Build build = await CreateNewVmrBuild(
         [
             ("Package.A1", "1.0.1"),
             ("Package.B1", "1.0.1"),
@@ -524,7 +540,6 @@ internal class BackflowTests : CodeFlowTests
 
         expectedFiles = [
             ..expectedFiles,
-            VersionFiles.VersionDetailsProps,
             VersionFiles.GlobalJson,
             DarcLib.Constants.CommonScriptFilesPath + "/build.ps2",
         ];
@@ -540,6 +555,7 @@ internal class BackflowTests : CodeFlowTests
         await GitOperations.ExecuteGitCommand(ProductRepoPath, ["add", _productRepoFilePath]);
         await GitOperations.ExecuteGitCommand(ProductRepoPath, ["commit", "-m", "Committing the backflow"]);
         await GitOperations.CheckAllIsCommitted(ProductRepoPath);
+        (await GetLocal(ProductRepoPath).GetDependenciesAsync("Package.New")).Should().ContainEquivalentOf(newDependency);
 
         // Now we make another set of changes in the VMR and try again
         // This time it will be same direction flow as the previous one (before it was opposite)
