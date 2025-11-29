af1e2b8efac1b694d4356c122065f2082421c66c
Adam Zippor <adamzippor@microsoft.com>
2025-11-29 14:29:51 +0100

    Add yaml model for repository branch policies

---
+++ src/Microsoft.DotNet.Darc/DarcLib/Models/Yaml/RepositoryPoliciesYaml.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using YamlDotNet.Serialization;
+
+namespace Microsoft.DotNet.DarcLib.Models.Yaml;
+
+public class RepositoryPoliciesYaml
+{
+    public const string RepoElement = "Repository URL";
+    public const string BranchElement = "Branch";
+    public const string MergePolicyElement = "Merge Policies";
+
+    [YamlMember(Alias = BranchElement, ApplyNamingConventions = false)]
+    public string Branch { get; set; }
+
+    [YamlMember(Alias = RepoElement, ApplyNamingConventions = false)]
+    public string Repository { get; set; }
+
+    [YamlMember(Alias = MergePolicyElement, ApplyNamingConventions = false)]
+    public List<MergePolicyYaml> MergePolicies { get; set; }
+}
