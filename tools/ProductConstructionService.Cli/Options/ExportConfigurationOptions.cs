140f2de9ea62d8b8a2dddacbbe437cc58b4aae79
Djuradj Kurepa <dkurepa@microsoft.com>
2025-12-04 12:34:22 +0100

    didn't safe this file

--- tools/ProductConstructionService.Cli/Options/ExportConfigurationOptions.cs
+++ tools/ProductConstructionService.Cli/Options/ExportConfigurationOptions.cs
@@ -14,5 +14,11 @@ internal class ExportConfigurationOptions : PcsApiOptions
     [Option("export-path", Required = false, HelpText = "The output path for the exported configuration files.")]
     public required string ExportPath { get; set; } = Directory.GetCurrentDirectory();
 
+    public override Task<IServiceCollection> RegisterServices(IServiceCollection services)
+    {
+        services.AddSingleton<IFileSystem, FileSystem>();
+        return base.RegisterServices(services);
+    }
+
     public override IOperation GetOperation(IServiceProvider sp) => ActivatorUtilities.CreateInstance<ExportConfigurationOperation>(sp, this);
 }
