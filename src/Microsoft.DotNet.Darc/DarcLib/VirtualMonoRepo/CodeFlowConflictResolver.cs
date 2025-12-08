87cfbfb6541613ed23315c36a5c96cd9439897e8
Premek Vysoky <premek.vysoky@microsoft.com>
2025-12-08 17:56:22 +0100

    Clean up conflict resolver too

--- src/Microsoft.DotNet.Darc/DarcLib/VirtualMonoRepo/CodeFlowConflictResolver.cs
+++ src/Microsoft.DotNet.Darc/DarcLib/VirtualMonoRepo/CodeFlowConflictResolver.cs
@@ -249,22 +249,33 @@ public abstract class CodeFlowConflictResolver
             return false;
         }
 
+        // Create patch for the file represent only the most current flow
+        string fromSha, toSha;
+        if (codeflowOptions.CurrentFlow.IsForwardFlow)
+        {
+            (fromSha, toSha) = (crossingFlow.RepoSha, codeflowOptions.CurrentFlow.RepoSha);
+        }
+        else
+        {
+            (fromSha, toSha) = (crossingFlow.VmrSha, codeflowOptions.CurrentFlow.VmrSha);
+        }
+
         var patchName = _vmrInfo.TmpPath / $"{codeflowOptions.Mapping.Name}-{Guid.NewGuid()}.patch";
         List<VmrIngestionPatch> patches = await _patchHandler.CreatePatches(
             patchName,
-            sha1: codeflowOptions.CurrentFlow.IsForwardFlow
-                ? crossingFlow.RepoSha
-                : crossingFlow.VmrSha,
-            sha2: codeflowOptions.CurrentFlow.IsForwardFlow
-                ? codeflowOptions.CurrentFlow.RepoSha
-                : codeflowOptions.CurrentFlow.VmrSha,
+            fromSha,
+            toSha,
             path: codeflowOptions.CurrentFlow.IsForwardFlow
                 ? new UnixPath(conflictedFile.Path.Substring(vmrSourcesPath.Length + 1))
                 : conflictedFile,
             filters: null,
             relativePaths: true,
-            workingDir: codeflowOptions.CurrentFlow.IsForwardFlow ? repo.Path : vmr.Path / vmrSourcesPath,
-            applicationPath: codeflowOptions.CurrentFlow.IsForwardFlow ? vmrSourcesPath : null,
+            workingDir: codeflowOptions.CurrentFlow.IsForwardFlow
+                ? repo.Path
+                : vmr.Path / vmrSourcesPath,
+            applicationPath: codeflowOptions.CurrentFlow.IsForwardFlow
+                ? vmrSourcesPath
+                : null,
             ignoreLineEndings: true,
             cancellationToken);
 
