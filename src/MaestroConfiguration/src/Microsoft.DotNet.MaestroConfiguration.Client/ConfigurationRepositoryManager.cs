f7b1601cd07d79e4cfb02aa584eb19b9e22f305e
Djuradj Kurepa <dkurepa@microsoft.com>
2025-12-17 10:00:23 +0100

    Update comment and add the tests I never added

--- src/MaestroConfiguration/src/Microsoft.DotNet.MaestroConfiguration.Client/ConfigurationRepositoryManager.cs
+++ src/MaestroConfiguration/src/Microsoft.DotNet.MaestroConfiguration.Client/ConfigurationRepositoryManager.cs
@@ -179,7 +179,7 @@ public class ConfigurationRepositoryManager : IConfigurationRepositoryManager
                 subscriptionFilePath);
         }
 
-        // delete the old subscription (with the same id) and add the updated one
+        // replace the old subscription (with the same id) with the the updated one
         var existingSubscription = subscriptionsInFile.FirstOrDefault(s => s.Id == updatedSubscription.Id);
         if (existingSubscription == null)
         {
