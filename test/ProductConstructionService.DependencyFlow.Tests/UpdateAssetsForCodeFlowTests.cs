bf7d67c12d8920fa15675a847fd32803b8b646d7
Premek Vysoky <premek.vysoky@microsoft.com>
2026-01-07 11:16:44 +0100

    Add tests

--- test/ProductConstructionService.DependencyFlow.Tests/UpdateAssetsForCodeFlowTests.cs
+++ test/ProductConstructionService.DependencyFlow.Tests/UpdateAssetsForCodeFlowTests.cs
@@ -1,10 +1,13 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using AwesomeAssertions;
 using Maestro.Data.Models;
 using Maestro.MergePolicies;
 using Microsoft.DotNet.DarcLib.Helpers;
 using Microsoft.DotNet.DarcLib.Models;
+using Microsoft.DotNet.DarcLib.Models.VirtualMonoRepo;
+using Moq;
 using NUnit.Framework;
 using ProductConstructionService.DependencyFlow.Model;
 
@@ -240,6 +243,90 @@ internal class UpdateAssetsForCodeFlowTests : UpdateAssetsPullRequestUpdaterTest
                         BuildId = build2.Id,
                         SourceRepo = build.GetRepository(),
                         CommitSha = build2.Commit
+                    }
+                ],
+                RequiredUpdates = [],
+                CodeFlowDirection = CodeFlowDirection.ForwardFlow,
+            };
+
+            AndShouldHavePullRequestCheckReminder();
+            AndShouldHaveInProgressPullRequestState(build2, expectedState: expectedState);
+        }
+    }
+
+    // Tests a case when user decides to merge the PR manually instead of calling `darc resolve-conflicts`.
+    // We must then make sure that the latest applied build for the subscriptions is updated accordingly.
+    [Test]
+    public async Task UpdateWithManuallyMergedPrAndLatestFlowNotApplied()
+    {
+        GivenATestChannel();
+        GivenACodeFlowSubscription(
+            new SubscriptionPolicy
+            {
+                Batchable = false,
+                UpdateFrequency = UpdateFrequency.EveryBuild,
+            });
+
+        // The situation is following:
+        // 1. build1 is flowed and PR is opened
+        // 2. build2 is flowed but conflicts so it's not applied
+        // 3. User manually merges the PR without resolving conflicts in the PR.
+        //    This means build1 was applied but build2 was not.
+        // 4. build2 is flowed again and a new PR should open (with a conflict inside)
+        Build build1 = GivenANewBuild(true);
+        build1.Id = 100;
+        Build build2 = GivenANewBuild(true);
+        build2.Id = 101;
+
+        using (WithExistingCodeFlowPullRequest(build2, PrStatus.Merged, null, willFlowNewBuild: true))
+        {
+            // Original PR is merged, we should try to delete the branch
+            DarcRemotes[VmrUri]
+                .Setup(x => x.DeletePullRequestBranchAsync(VmrPullRequestUrl))
+                .Returns(Task.CompletedTask)
+                .Verifiable();
+
+            // When the build2 was not applied, we must find build1 metadata in the merged source manifest
+            DarcRemotes[VmrUri]
+                .Setup(x => x.GetSourceManifestAsync(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(new SourceManifest(
+                    [
+                        new RepositoryRecord(
+                            Subscription.TargetDirectory,
+                            build1.GetRepository(),
+                            build1.Commit,
+                            build1.Id),
+                        new RepositoryRecord(
+                            "another-repo",
+                            "https://github.com/another/repo",
+                            "abcdef01234234423",
+                            build1.Id + 100),
+                    ], []))
+                .Verifiable();
+
+            // URI of the new PR that should get created
+            VmrPullRequestUrl = $"{VmrUri}/pulls/2";
+            CreatePullRequestShouldReturnAValidValue();
+
+            await WhenUpdateAssetsAsyncIsCalled(build2);
+
+            Subscription.LastAppliedBuild.Id.Should().Be(build1.Id);
+
+            var expectedState = new InProgressPullRequest()
+            {
+                UpdaterId = GetPullRequestUpdaterId(Subscription).Id,
+                Url = VmrPullRequestUrl,
+                HeadBranch = InProgressPrHeadBranch,
+                HeadBranchSha = InProgressPrHeadBranchSha,
+                SourceSha = build2.Commit,
+                ContainedSubscriptions =
+                [
+                    new()
+                    {
+                        SubscriptionId = Subscription.Id,
+                        BuildId = build2.Id,
+                        SourceRepo = build1.GetRepository(),
+                        CommitSha = build2.Commit,
                     }
                 ],
                 RequiredUpdates = [],
