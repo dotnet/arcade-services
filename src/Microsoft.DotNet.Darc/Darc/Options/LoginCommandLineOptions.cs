549b021ce707f2a59ea7642eaf6569be69681912
Přemek Vysoký <premek.vysoky@microsoft.com>
2026-02-19 13:14:09 +0100

    Update login command help text for clarity

--- src/Microsoft.DotNet.Darc/Darc/Options/LoginCommandLineOptions.cs
+++ src/Microsoft.DotNet.Darc/Darc/Options/LoginCommandLineOptions.cs
@@ -6,9 +6,9 @@ using Microsoft.DotNet.Darc.Operations;
 
 namespace Microsoft.DotNet.Darc.Options;
 
-[Verb("login", HelpText = "Authenticate with Maestro and store credentials for use by automation tools.")]
+[Verb("login", HelpText = "Authenticate with Maestro (credentials are persisted for a time)")]
 internal class LoginCommandLineOptions : CommandLineOptions<LoginOperation>
 {
-    [Option("bar-uri", HelpText = "URI of the Build Asset Registry service to authenticate with. Defaults to production Maestro.")]
+    [Option("bar-uri", HelpText = "URI of the Build Asset Registry service to authenticate with. Defaults to production Maestro")]
     public string BarUri { get; set; }
 }
