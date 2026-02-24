e8c411daa9aa141df3c8d4804b0f67186cc19f5b
Premek Vysoky <premek.vysoky@microsoft.com>
2026-02-24 16:34:18 +0100

    Change how we define nullability

--- src/Microsoft.DotNet.Darc/Darc/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Microsoft.DotNet.Darc.Tests")]
-[assembly: InternalsVisibleTo("Microsoft.DotNet.DarcLib.Codeflow.Tests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
