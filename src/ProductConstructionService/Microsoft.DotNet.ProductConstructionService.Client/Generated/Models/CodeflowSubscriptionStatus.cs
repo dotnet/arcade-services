c00c4d2102aaefe6502a4a23de36c0b001b891ac
Adam <adamzippor@microsoft.com>
2026-03-04 15:47:41 +0100

    Codeflow statuses endpoint (#6034)
    #5932

---
+++ src/ProductConstructionService/Microsoft.DotNet.ProductConstructionService.Client/Generated/Models/CodeflowSubscriptionStatus.cs
@@ -0,0 +1,23 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Newtonsoft.Json;
+
+namespace Microsoft.DotNet.ProductConstructionService.Client.Models
+{
+    public partial class CodeflowSubscriptionStatus
+    {
+        public CodeflowSubscriptionStatus()
+        {
+        }
+
+        [JsonProperty("subscription")]
+        public Subscription Subscription { get; set; }
+
+        [JsonProperty("activePullRequest")]
+        public TrackedPullRequest ActivePullRequest { get; set; }
+
+        [JsonProperty("newerBuildsAvailable")]
+        public int? NewerBuildsAvailable { get; set; }
+    }
+}
---
+++ src/ProductConstructionService/ProductConstructionService.Api/Api/v2020_02_20/Models/CodeflowStatus.cs
@@ -0,0 +1,38 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Newtonsoft.Json;
+using static ProductConstructionService.Api.Api.v2020_02_20.Controllers.PullRequestController;
+
+#nullable disable
+namespace ProductConstructionService.Api.v2020_02_20.Models;
+
+public class CodeflowStatus
+{
+    [JsonProperty("mappingName")]
+    public string MappingName { get; set; }
+
+    [JsonProperty("repositoryUrl")]
+    public string RepositoryUrl { get; set; }
+
+    [JsonProperty("repositoryBranch")]
+    public string RepositoryBranch { get; set; }
+
+    [JsonProperty("forwardFlow")]
+    public CodeflowSubscriptionStatus ForwardFlow { get; set; }
+
+    [JsonProperty("backflow")]
+    public CodeflowSubscriptionStatus Backflow { get; set; }
+}
+
+public class CodeflowSubscriptionStatus
+{
+    [JsonProperty("subscription")]
+    public Subscription Subscription { get; set; }
+
+    [JsonProperty("activePullRequest")]
+    public TrackedPullRequest ActivePullRequest { get; set; }
+
+    [JsonProperty("newerBuildsAvailable")]
+    public int? NewerBuildsAvailable { get; set; }
+}
