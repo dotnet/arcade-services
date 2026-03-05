b3492d9416d75298e763fa65a35dee8b0065e6c0
Pavel Purma <pavelpurma@microsoft.com>
2026-03-05 13:43:14 +0100

    DummyApp build

--- eng/service-templates/BuildInsights/FullCreate/set-aca-image.ps1
+++ eng/service-templates/BuildInsights/FullCreate/set-aca-image.ps1
@@ -1,3 +1,2 @@
 az containerapp update --name build-insights-stage --resource-group build-insights-stage-rg `
-    --image buildinsightsstage.azurecr.io/build-insights-api:202603043-1-630a680517-dev `
-    --command "/bin/sh, -c, cd /app/BuildInsights.DummyApp && dotnet BuildInsights.DummyApp.dll"
+    --image buildinsightsstage.azurecr.io/dummy-app:202603043-1-630a680517-dev `
