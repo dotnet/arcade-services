5aa89f0d75606934c59e0190e38b9cde37fde858
Premek Vysoky <premek.vysoky@microsoft.com>
2026-03-18 13:57:54 +0100

    Revert BI changes

--- src/BuildInsights/BuildInsights.QueueInsights/Models/QueueInsightsBetaSettings.cs
+++
@@ -1,9 +0,0 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-namespace BuildInsights.QueueInsights.Models;
-
-public class QueueInsightsBetaSettings
-{
-    public string[] AllowedRepos { get; set; } = Array.Empty<string>();
-}
