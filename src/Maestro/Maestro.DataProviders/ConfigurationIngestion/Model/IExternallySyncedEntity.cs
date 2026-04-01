2ac072bffe4ff80ae14de70bb20dbbe7974882f5
Adam Zippor <adamzippor@microsoft.com>
2026-04-01 10:07:15 +0200

    move enable

--- src/Maestro/Maestro.DataProviders/ConfigurationIngestion/Model/IExternallySyncedEntity.cs
+++ src/Maestro/Maestro.DataProviders/ConfigurationIngestion/Model/IExternallySyncedEntity.cs
@@ -1,10 +1,9 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#nullable enable
-
 using System.Collections.Generic;
 
+#nullable enable
 namespace Maestro.DataProviders.ConfigurationIngestion.Model;
 
 public abstract class IExternallySyncedEntity
