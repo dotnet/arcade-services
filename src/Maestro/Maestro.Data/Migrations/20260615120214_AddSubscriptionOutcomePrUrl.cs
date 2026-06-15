6eeb84845b7e831d7e6f937e246e91d9ffe838a8
Adam Zippor <adamzippor@microsoft.com>
2026-06-15 14:10:32 +0200

    try fix migration

--- src/Maestro/Maestro.Data/Migrations/20260615120214_AddSubscriptionOutcomePrUrl.cs
+++ src/Maestro/Maestro.Data/Migrations/20260615120214_AddSubscriptionOutcomePrUrl.cs
@@ -1,14 +1,11 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace Maestro.Data.Migrations
 {
     /// <inheritdoc />
-    public partial class AddSubscriptionOutcomeRepoAndPrUrls : Migration
+    public partial class AddSubscriptionOutcomePrUrl : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
