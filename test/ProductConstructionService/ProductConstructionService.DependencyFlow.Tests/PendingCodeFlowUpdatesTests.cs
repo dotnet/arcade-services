91a213ec46f2af74d53517cafb56e58920d2acb7
Adam Zippor <adamzippor@microsoft.com>
2026-06-23 10:51:57 +0200

    CR

--- test/ProductConstructionService/ProductConstructionService.DependencyFlow.Tests/PendingCodeFlowUpdatesTests.cs
+++ test/ProductConstructionService/ProductConstructionService.DependencyFlow.Tests/PendingCodeFlowUpdatesTests.cs
@@ -101,6 +101,7 @@ internal class PendingCodeFlowUpdatesTests : PendingUpdatePullRequestUpdaterTest
             var res = await WhenProcessPendingUpdatesAsyncIsCalled(newBuild, isCodeFlow: true);
             Assert.That(res.OutcomeType, Is.EqualTo(SubscriptionOutcomeType.NotUpdatable));
 
+            AndPullRequestShouldHaveComment("opposite codeflow merged");
             AndShouldHaveNoPendingUpdateState();
             AndShouldHavePullRequestCheckReminder();
         }
