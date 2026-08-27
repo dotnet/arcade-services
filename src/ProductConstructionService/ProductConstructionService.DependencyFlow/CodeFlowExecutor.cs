2935ddfe4ece5e203efa84390b6b2b63ca0eb2bb
Adam Zippor <adamzippor@microsoft.com>
2026-08-27 17:35:02 +0200

    remove unused dependencies

--- src/ProductConstructionService/ProductConstructionService.DependencyFlow/CodeFlowExecutor.cs
+++ src/ProductConstructionService/ProductConstructionService.DependencyFlow/CodeFlowExecutor.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Maestro.Common.Telemetry;
-using Maestro.Data.Models;
 using Maestro.WorkItems;
 using Microsoft.DotNet.DarcLib;
 using Microsoft.DotNet.DarcLib.Helpers;
